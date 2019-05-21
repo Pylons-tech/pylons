a83d10607db2514a37d93b6f2bb85f9363acde64
Girish Ramnani <girishramnani95@gmail.com>
2019-05-21 16:26:50 +0530

    Add codec

---
+++ x/pylons/codec.go
@@ -0,0 +1,10 @@
+package pylons
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+// RegisterCodec registers concrete types on wire codec
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgGetPylons{}, "pylons/GetPylons", nil)
+}
