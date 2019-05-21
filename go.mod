ab0356b00a4b6ce2dd9989756f7c11f7b1a02a2e
Girish Ramnani <girishramnani95@gmail.com>
2019-05-21 19:39:29 +0530

    added cli client

--- go.mod
+++ go.mod
@@ -34,7 +34,7 @@ require (
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/spf13/viper v1.0.3 // indirect
+	github.com/spf13/viper v1.0.3
 	github.com/syndtr/goleveldb v1.0.0 // indirect
 	github.com/tendermint/btcd v0.1.1 // indirect
 	github.com/tendermint/go-amino v0.14.1
---
+++ x/pylons/client/cli/tx.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/MikeSofaer/pylons/x/pylons"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/utils"
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtxb "github.com/cosmos/cosmos-sdk/x/auth/client/txbuilder"
+)
+
+const (
+	DefaultCoinPerRequest = 500
+)
+
+// GetCmdBuyName is the CLI command for sending a BuyName transaction
+func GetCmdBuyName(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "get-pylons",
+		Short: "ask for pylons. 500 pylons per request",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc).WithAccountDecoder(cdc)
+
+			txBldr := authtxb.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			if err := cliCtx.EnsureAccountExists(); err != nil {
+				return err
+			}
+
+			msg := pylons.NewMsgGetPylons(pylons.NewPylon(DefaultCoinPerRequest), cliCtx.GetFromAddress())
+			err := msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			cliCtx.PrintResponse = true
+
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg}, false)
+		},
+	}
+}
