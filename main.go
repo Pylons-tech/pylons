8b0ff7fc9f09b62ae2a961f18955ca0247822326
Girish Ramnani <girishramnani95@gmail.com>
2019-06-24 04:16:21 +0530

    added way to provide a hex encoded priv key

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 
 	"encoding/hex"
+	"encoding/json"
 
 	clkeys "github.com/cosmos/cosmos-sdk/client/keys"
 	"github.com/cosmos/cosmos-sdk/crypto/keys"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/mintkey"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	crypto "github.com/tendermint/tendermint/crypto/secp256k1"
 )
 
-func ShittySignature (cleartextKey string, hexData string) {
+func ShittySignature(cleartextKey string, hexData string) {
 	// generate private key from cleartext
 	hexPrivKey := cleartextKey
 	privKeyBytes, err := hex.DecodeString(hexPrivKey)
@@ -50,11 +52,11 @@ func GenerateCoinKey() (keys.Info, string, error) {
 func main() {
 	// Apache signature
 	ShittySignature(
-		"c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4", 
+		"c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4",
 		"5468697320697320616E206578616D706C65206F662061207369676E6564206D6573736167652E")
 	// Interop bullshit
 	ShittySignature(
-		"a96e62ed3955e65be32703f12d87b6b5cf26039ecfa948dc5107a495418e5330", 
+		"a96e62ed3955e65be32703f12d87b6b5cf26039ecfa948dc5107a495418e5330",
 		"7b22416d6f756e74223a5b7b22616d6f756e74223a22353030222c2264656e6f6d223a2270796c6f6e227d5d2c22526571756573746572223a22636f736d6f733133647a6d39306679763335706671707434326735657672637634736c3673397968736e397668227d")
 	info, secret, _ := GenerateCoinKey()
 	fmt.Printf("Secret: %+v\n", secret)
@@ -80,4 +82,23 @@ func main() {
 
 	bech32PubKey := sdk.MustBech32ifyAccPub(pubKey)
 	fmt.Println("bech32PubKey:", bech32PubKey)
+
+	keybase, err := clkeys.NewKeyBaseFromDir("/home/girish/.plncli")
+	if err != nil {
+		return
+	}
+	infott, err := keybase.Get("jack")
+	ttt, _ := json.Marshal(infott)
+	tmp := make(map[string]interface{})
+	json.Unmarshal(ttt, &tmp)
+	fmt.Println("HEHEHEHEH", tmp["privkey.armor"])
+	// lif := info.
+	privKey, err := mintkey.UnarmorDecryptPrivKey(tmp["privkey.armor"].(string), "jack1234")
+	fmt.Println("PrivKey", hex.EncodeToString(privKey.Bytes()), sdk.AccAddress(privKey.PubKey().Bytes()).String())
+}
+
+// localInfo is the public information about a locally stored key
+type localInfo struct {
+	Name         string `json:"name"`
+	PrivKeyArmor string `json:"privkey.armor"`
 }
