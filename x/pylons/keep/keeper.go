c1fff4d6d4a8fd9a69fc4b9b6171ff5c804126af
Girish Ramnani <girishramnani95@gmail.com>
2019-06-28 15:43:59 +0530

    keeper refactor

--- x/pylons/keep/keeper.go
+++ x/pylons/keep/keeper.go
@@ -1,96 +1,27 @@
 package keep
 
 import (
-	"errors"
-
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 
-	"fmt"
-
-	"github.com/MikeSofaer/pylons/x/pylons/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // Keeper maintains the link to data storage and exposes getter/setter methods for the various parts of the state machine
 type Keeper struct {
 	CoinKeeper  bank.Keeper
-	CookbookKey sdk.StoreKey // Unexposed key to access store from sdk.Context
-	Cdc         *codec.Codec // The wire codec for binary encoding/decoding.
+	CookbookKey sdk.StoreKey // Unexposed key to access cookbook store from sdk.Context
+	RecipeKey   sdk.StoreKey
+	Cdc         *codec.Codec // The wire codec for binary encoding/decoding
+
 }
 
 // NewKeeper creates a new Keeper
-func NewKeeper(coinKeeper bank.Keeper, storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
+func NewKeeper(coinKeeper bank.Keeper, cookbookKey, recipeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
 	return Keeper{
 		CoinKeeper:  coinKeeper,
-		CookbookKey: storeKey,
+		CookbookKey: cookbookKey,
+		RecipeKey:   recipeKey,
 		Cdc:         cdc,
 	}
-}
-
-// SetCookbook sets the cookbook with the name as the key
-func (k Keeper) SetCookbook(ctx sdk.Context, cookbook types.Cookbook) error {
-
-	if cookbook.Sender.Empty() {
-		return errors.New("the sender cannot be empty")
-
-	}
-	mCB, err := k.Cdc.MarshalBinaryBare(cookbook)
-	if err != nil {
-		return err
-	}
-	store := ctx.KVStore(k.CookbookKey)
-	store.Set([]byte(cookbook.KeyGen()), mCB)
-	return nil
-}
-
-// GetCookbook returns cookbook based on UUID
-func (k Keeper) GetCookbook(ctx sdk.Context, id string) (types.Cookbook, error) {
-	store := ctx.KVStore(k.CookbookKey)
-
-	if !store.Has([]byte(id)) {
-		return types.Cookbook{}, errors.New("The cookbook doesn't exist")
-	}
-
-	uCB := store.Get([]byte(id))
-	var cookbook types.Cookbook
-
-	k.Cdc.MustUnmarshalBinaryBare(uCB, &cookbook)
-	return cookbook, nil
-}
-
-// UpdateCookbook is used to update the cookbook using the id
-func (k Keeper) UpdateCookbook(ctx sdk.Context, id string, cookbook types.Cookbook) error {
-	if cookbook.Sender.Empty() {
-		return errors.New("the sender cannot be empty")
-
-	}
-	store := ctx.KVStore(k.CookbookKey)
-
-	if !store.Has([]byte(id)) {
-		return fmt.Errorf("the cookbook with gid %s does not exist", id)
-	}
-	mCB, err := k.Cdc.MarshalBinaryBare(cookbook)
-	if err != nil {
-		return err
-	}
-	store.Set([]byte(id), mCB)
-	return nil
-}
-
-// GetCookbooksIterator returns an iterator for all the cookbooks
-func (k Keeper) GetCookbooksIterator(ctx sdk.Context, sender sdk.AccAddress) sdk.Iterator {
-	store := ctx.KVStore(k.CookbookKey)
-	return sdk.KVStorePrefixIterator(store, []byte(sender.String()))
-}
-
-// DeleteCookbook is usec to delete a cookbook based on the id
-func (k Keeper) DeleteCookbook(ctx sdk.Context, id string) error {
-	store := ctx.KVStore(k.CookbookKey)
-
-	if !store.Has([]byte(id)) {
-		return fmt.Errorf("The cookbook with the id %s doesn't exist", id)
-	}
-	store.Delete([]byte(id))
-	return nil
 }
