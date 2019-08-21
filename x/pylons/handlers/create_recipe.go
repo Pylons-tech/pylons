8a44f80d4d482fb769bcd87b9b5ad04f0d1a2fd8
Girish Ramnani <girishramnani95@gmail.com>
2019-08-22 02:25:39 +0530

    added todo portion as a comment for now so I dont forget

--- x/pylons/handlers/create_recipe.go
+++ x/pylons/handlers/create_recipe.go
@@ -14,7 +14,7 @@ func HandlerMsgCreateRecipe(ctx sdk.Context, keeper keep.Keeper, msg msgs.MsgCre
 	if err != nil {
 		return err.Result()
 	}
-
+	// TODO: check the recipe has a parent cookbook and is being created from the owner of the cookbook
 	recipe := types.NewRecipe(msg.CookbookName, msg.ID, msg.Inputs, msg.Outputs, 0)
 	if err := keeper.SetRecipe(ctx, recipe); err != nil {
 		return sdk.ErrInternal(err.Error()).Result()
