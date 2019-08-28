4f9795e6a7b39d991cd88d843d49140409c70021
Girish Ramnani <girishramnani95@gmail.com>
2019-08-28 21:28:39 +0530

    added tx builder for execute recipe

--- x/pylons/client/rest/rest.go
+++ x/pylons/client/rest/rest.go
@@ -31,6 +31,8 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec, 
 		txbuilder.UpdateCookbookTxBuilder(cdc, cliCtx, storeName)).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/%s/create_recipe/tx_build/", storeName),
 		txbuilder.CreateRecipeTxBuilder(cdc, cliCtx, storeName)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%s/execute_recipe/tx_build/", storeName),
+		txbuilder.ExecuteRecipeTxBuilder(cdc, cliCtx, storeName)).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/%s/update_recipe/tx_build/", storeName),
 		txbuilder.UpdateRecipeTxBuilder(cdc, cliCtx, storeName)).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/%s/get_pylons", storeName),
