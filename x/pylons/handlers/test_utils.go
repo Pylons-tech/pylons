32340c329543c7287142a6332ba4167e68099ab2
Eugen Steffen <stalepresh121@outlook.com>
2019-09-18 00:35:58 +1000

    create_cookbook_test

--- x/pylons/handlers/test_utils.go
+++ x/pylons/handlers/test_utils.go
@@ -9,14 +9,20 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/cosmos/cosmos-sdk/x/params"
+
+	"github.com/MikeSofaer/pylons/x/pylons/keep"
 )
 
 type testCoinInput struct {
-	cdc *codec.Codec
-	ctx sdk.Context
-	ak  auth.AccountKeeper
-	pk  params.Keeper
+	cdc  *codec.Codec
+	ctx  sdk.Context
+	ak   auth.AccountKeeper
+	pk   params.Keeper
+	bk   bank.Keeper
+	fcK  auth.FeeCollectionKeeper
+	plnK keep.Keeper
 }
 
 func setupTestCoinInput() testCoinInput {
@@ -30,12 +36,25 @@ func setupTestCoinInput() testCoinInput {
 	keyParams := sdk.NewKVStoreKey("params")
 	tkeyParams := sdk.NewTransientStoreKey("transient_params")
 
+	fcKey := sdk.NewKVStoreKey("fee_collection")
+	cbKey := sdk.NewKVStoreKey("pylons")
+	rcKey := sdk.NewKVStoreKey("pylons_recipe")
+	itKey := sdk.NewKVStoreKey("pylons_item")
+	execKey := sdk.NewKVStoreKey("pylons_execution")
+
 	ms := store.NewCommitMultiStore(db)
 	ms.MountStoreWithDB(authCapKey, sdk.StoreTypeIAVL, db)
 	ms.MountStoreWithDB(fckCapKey, sdk.StoreTypeIAVL, db)
 	ms.MountStoreWithDB(keyParams, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(fcKey, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(cbKey, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(rcKey, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(itKey, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(execKey, sdk.StoreTypeIAVL, db)
 	ms.MountStoreWithDB(tkeyParams, sdk.StoreTypeTransient, db)
 	ms.LoadLatestVersion()
+
+	ms.GetKVStore(cbKey)
 
 	pk := params.NewKeeper(cdc, keyParams, tkeyParams)
 	ak := auth.NewAccountKeeper(
@@ -45,5 +64,22 @@ func setupTestCoinInput() testCoinInput {
 
 	ak.SetParams(ctx, auth.DefaultParams())
 
-	return testCoinInput{cdc: cdc, ctx: ctx, ak: ak, pk: pk}
+	bk := bank.NewBaseKeeper(
+		ak,
+		pk.Subspace(bank.DefaultParamspace),
+		bank.DefaultCodespace,
+	)
+
+	fcK := auth.NewFeeCollectionKeeper(cdc, fcKey)
+
+	plnK := keep.NewKeeper(
+		bk,
+		cbKey, // cookbook
+		rcKey, // recipe
+		itKey, // item
+		execKey, // exec
+		cdc,
+	)
+
+	return testCoinInput{cdc: cdc, ctx: ctx, ak: ak, pk: pk, bk: bk, fcK: fcK, plnK: plnK}
 }
