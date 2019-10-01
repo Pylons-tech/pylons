ebd3bd953deaa7cac210f6dc0e5a54f14562d823
Eugen Steffen <stalepresh121@outlook.com>
2019-10-02 00:37:49 +1000

    remove commented weighted param test

--- x/pylons/types/weighted_param_test.go
+++ x/pylons/types/weighted_param_test.go
@@ -19,23 +19,11 @@ func TestWeightedParamListSerialize(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			data, err := json.Marshal(entries)
 			// t.Errorf("TestWeightedParamListSerialize1 LOG_entries:: %+v", entries)
-			// t.Errorf("TestWeightedParamListSerialize1 LOG_err:: %+v", err)
 			require.True(t, err == nil)
 			var serializedEntries WeightedParamList
 			err2 := json.Unmarshal(data, &serializedEntries)
 			require.True(t, err2 == nil)
-			// t.Errorf("TestWeightedParamListSerialize1 LOG_serializedEntries:: %+v", serializedEntries)
-			// t.Errorf("TestWeightedParamListSerialize1 LOG_err2:: %+v", err2)
 			require.True(t, reflect.DeepEqual(entries, serializedEntries))
-
-			// data, err := entries.MarshalJSON()
-			// t.Errorf("TestWeightedParamListSerialize LOG_entries:: %+v", entries)
-			// t.Errorf("TestWeightedParamListSerialize LOG_err:: %+v", err)
-			// require.True(t, err == nil)
-			// var serializedEntries WeightedParamList
-			// err2 := serializedEntries.UnmarshalJSON(data)
-			// t.Errorf("TestWeightedParamListSerialize LOG_serializedEntries:: %+v", serializedEntries)
-			// t.Errorf("TestWeightedParamListSerialize LOG_err2:: %+v", err2)
 		})
 	}
 }
