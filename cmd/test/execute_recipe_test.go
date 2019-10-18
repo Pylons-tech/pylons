b30cb4c6b6d409f25b65418455f1de47a9657252
Eugen Steffen <stalepresh121@outlook.com>
2019-10-18 10:47:06 +1000

    fix execute_recipe test

--- cmd/test/execute_recipe_test.go
+++ cmd/test/execute_recipe_test.go
@@ -1,62 +1,61 @@
 package main
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/MikeSofaer/pylons/x/pylons/msgs"
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/MikeSofaer/pylons/x/pylons/msgs"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestExecuteRecipeViaCLI(t *testing.T) {
-// 	// TODO if we find a way to sign using sequence number between same blocks, this wait can be removed
-// 	WaitForNextBlock()
+func TestExecuteRecipeViaCLI(t *testing.T) {
+	// TODO if we find a way to sign using sequence number between same blocks, this wait can be removed
+	WaitForNextBlock()
 
-// 	tests := []struct {
-// 		name            string
-// 		rcpName         string
-// 		itemIDs         []string
-// 		desiredItemName string
-// 	}{
-// 		{
-// 			"basic flow test",
-// 			"TESTRCP_ExecuteRecipe_002",
-// 			[]string{},
-// 			"TESTITEM_ExecuteRecipe_002",
-// 		},
-// 	}
+	tests := []struct {
+		name            string
+		rcpName         string
+		itemIDs         []string
+		desiredItemName string
+	}{
+		{
+			"basic flow test",
+			"TESTRCP_ExecuteRecipe_002",
+			[]string{},
+			"TESTITEM_ExecuteRecipe_002",
+		},
+	}
 
-// 	for _, tc := range tests {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			err := MockRecipeWithName(tc.rcpName, tc.desiredItemName, t)
-// 			ErrValidation(t, "error mocking recipe %+v", err)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := MockRecipeWithName(tc.rcpName, tc.desiredItemName, t)
+			ErrValidation(t, "error mocking recipe %+v", err)
 
-// 			recipes, err := TestQueryListRecipe(t)
-// 			ErrValidation(t, "error listing recipes %+v", err)
+			recipes, err := TestQueryListRecipe(t)
+			ErrValidation(t, "error listing recipes %+v", err)
 
-// 			require.True(t, err == nil)
-// 			require.True(t, len(recipes) > 0)
+			require.True(t, err == nil)
+			require.True(t, len(recipes) > 0)
 
-// 			rcp, ok := FindRecipeFromArrayByName(recipes, tc.rcpName)
-// 			if !ok {
-// 				t.Errorf("error getting recipe with name %+v", tc.rcpName)
-// 				t.Fatal()
-// 			}
+			rcp, ok := FindRecipeFromArrayByName(recipes, tc.rcpName)
+			if !ok {
+				t.Errorf("error getting recipe with name %+v", tc.rcpName)
+				t.Fatal()
+			}
 
-// 			eugenAddr := GetAccountAddr("eugen", t)
-// 			sdkAddr, err := sdk.AccAddressFromBech32(eugenAddr)
-// 			require.True(t, err == nil)
-// 			TestTxWithMsg(
-// 				t,
-// 				msgs.NewMsgExecuteRecipe(rcp.ID, sdkAddr, tc.itemIDs),
-// 				"pylons/ExecuteRecipe")
+			eugenAddr := GetAccountAddr("eugen", t)
+			sdkAddr, err := sdk.AccAddressFromBech32(eugenAddr)
+			require.True(t, err == nil)
+			TestTxWithMsg(
+				t,
+				msgs.NewMsgExecuteRecipe(rcp.ID, sdkAddr, tc.itemIDs))
 
-// 			WaitForNextBlock()
-// 			items, err := ListItemsViaCLI(t)
-// 			ErrValidation(t, "error listing items via cli ::: %+v", err)
+			WaitForNextBlock()
+			items, err := ListItemsViaCLI(t)
+			ErrValidation(t, "error listing items via cli ::: %+v", err)
 
-// 			_, ok = FindItemFromArrayByName(items, tc.desiredItemName)
-// 			require.True(t, ok)
-// 		})
-// 	}
-// }
+			_, ok = FindItemFromArrayByName(items, tc.desiredItemName)
+			require.True(t, ok)
+		})
+	}
+}
