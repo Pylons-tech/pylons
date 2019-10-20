68cf728d8f3d46bea3520d15fb9eada0e9e48c20
Girish Ramnani <girishramnani95@gmail.com>
2019-10-20 16:26:42 +0530

    added more complex test cases for create trade handler

--- x/pylons/handlers/create_trade_test.go
+++ x/pylons/handlers/create_trade_test.go
@@ -19,8 +19,8 @@ func TestHandlerMsgCreateTrade(t *testing.T) {
 	sender, _ := sdk.AccAddressFromBech32("cosmos1y8vysg9hmvavkdxpvccv2ve3nssv5avm0kt337")
 	sender2, _ := sdk.AccAddressFromBech32("cosmos16wfryel63g7axeamw68630wglalcnk3l0zuadc")
 	cbData := CreateCBResponse{}
-	mockedCoinInput.Bk.AddCoins(mockedCoinInput.Ctx, sender, types.NewPylon(1000000))
-	mockedCoinInput.Bk.AddCoins(mockedCoinInput.Ctx, sender2, types.NewPylon(1000000))
+	mockedCoinInput.Bk.AddCoins(mockedCoinInput.Ctx, sender, types.NewPylon(100000))
+	mockedCoinInput.Bk.AddCoins(mockedCoinInput.Ctx, sender2, types.NewPylon(100000))
 
 	cookbookMsg := msgs.NewMsgCreateCookbook("cookbook-0001", "this has to meet character limits", "SketchyCo", "1.0.0", "example@example.com", 1, msgs.DefaultCostPerBlock, sender)
 	cookbookResult := HandlerMsgCreateCookbook(mockedCoinInput.Ctx, mockedCoinInput.PlnK, cookbookMsg)
@@ -52,6 +52,34 @@ func TestHandlerMsgCreateTrade(t *testing.T) {
 				*item,
 			},
 			showError: false,
+		},
+		"trade with item and coins": {
+			sender:        sender,
+			inputItemList: types.GenItemInputList("Pikachu"),
+			outputItemList: types.ItemList{
+				*item,
+			},
+			outputCoinList: types.NewPylon(10000),
+			showError:      false,
+		},
+		"trade with only items failure due to sender not being owner": {
+			sender:        sender,
+			inputItemList: types.GenItemInputList("Pikachu"),
+			outputItemList: types.ItemList{
+				*item2,
+			},
+			desiredError: "is not owned by sender",
+			showError:    true,
+		},
+		"trade with coin and item failure due to low balance": {
+			sender:        sender,
+			inputItemList: types.GenItemInputList("Pikachu"),
+			outputItemList: types.ItemList{
+				*item,
+			},
+			outputCoinList: types.NewPylon(1000000),
+			desiredError:   "sender doesn't have enough coins for the trade",
+			showError:      true,
 		},
 	}
 	for testName, tc := range cases {
