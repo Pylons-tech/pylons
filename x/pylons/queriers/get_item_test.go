6bc2fa5d1e622958f69cb07fa40565fe6f0a264b
Eugen Steffen <stalepresh121@outlook.com>
2019-10-22 13:23:05 +1000

    add queriers.get_item_test

--- x/pylons/queriers/get_item_test.go
+++ x/pylons/queriers/get_item_test.go
@@ -1,3 +1,90 @@
 package queriers
 
 // TODO should write queriers.get_item test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/MikeSofaer/pylons/x/pylons/handlers"
+	"github.com/MikeSofaer/pylons/x/pylons/keep"
+	"github.com/MikeSofaer/pylons/x/pylons/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func TestGetItem(t *testing.T) {
+	mockedCoinInput := keep.SetupTestCoinInput()
+
+	sender := "cosmos1y8vysg9hmvavkdxpvccv2ve3nssv5avm0kt337"
+	senderAccAddress, _ := sdk.AccAddressFromBech32(sender)
+
+	mockedCoinInput.Bk.AddCoins(mockedCoinInput.Ctx, senderAccAddress, types.PremiumTier.Fee)
+
+	// mock cookbook
+	cbData := handlers.MockCookbook(mockedCoinInput, senderAccAddress)
+
+	// mock item
+	mockItemName := "GET_ITEM_MOCK_TEST_NAME"
+	mockedItem := keep.GenItem(cbData.CookbookID, senderAccAddress, mockItemName)
+	mockedCoinInput.PlnK.SetItem(mockedCoinInput.Ctx, *mockedItem)
+
+	cases := map[string]struct {
+		path          []string
+		desiredError  string
+		showError     bool
+		desiredRcpCnt int
+		itemName      string
+	}{
+		"error check when providing invalid item ID": {
+			path:          []string{"invalid itemID"},
+			showError:     true,
+			desiredError:  "The item doesn't exist",
+			desiredRcpCnt: 0,
+		},
+		"error check when not providing itemID": {
+			path:          []string{},
+			showError:     true,
+			desiredError:  "no item id is provided in path",
+			desiredRcpCnt: 0,
+		},
+		"get item successful check": {
+			path:          []string{mockedItem.ID},
+			showError:     false,
+			desiredError:  "",
+			desiredRcpCnt: 1,
+			itemName:      mockItemName,
+		},
+	}
+	for testName, tc := range cases {
+		t.Run(testName, func(t *testing.T) {
+			result, err := GetItem(
+				mockedCoinInput.Ctx,
+				tc.path,
+				abci.RequestQuery{
+					Path: "",
+					Data: []byte{},
+				},
+				mockedCoinInput.PlnK,
+			)
+			// t.Errorf("GetItemTEST LOG:: %+v", err)
+			if tc.showError {
+				require.True(t, strings.Contains(err.Error(), tc.desiredError))
+			} else {
+				require.True(t, err == nil)
+				readItem := types.Item{}
+				readItemErr := mockedCoinInput.PlnK.Cdc.UnmarshalJSON(result, &readItem)
+
+				require.True(t, readItemErr == nil)
+
+				itmName, ok := readItem.FindString("Name")
+				require.True(t, ok)
+				require.True(t, itmName == tc.itemName)
+			}
+		})
+	}
+}
