1bdaefde056547a26e41338e691cc6a885c0b8c4
Eugen Steffen <stalepresh121@outlook.com>
2019-12-03 19:08:26 +1000

    exact coin amount checker

--- cmd/fixtures_test/fixture_test.go
+++ cmd/fixtures_test/fixture_test.go
@@ -79,7 +79,8 @@ func PropertyExistCheck(step FixtureStep, t *testing.T) {
 	if len(pCheck.Coins) > 0 {
 		for _, coinCheck := range pCheck.Coins {
 			accInfo := intTest.GetAccountInfoFromName(pCheck.Owner, t)
-			require.True(t, accInfo.Coins.AmountOf(coinCheck.Coin).GTE(sdk.NewInt(coinCheck.Amount)))
+			// require.True(t, accInfo.Coins.AmountOf(coinCheck.Coin).GTE(sdk.NewInt(coinCheck.Amount)))
+			require.True(t, accInfo.Coins.AmountOf(coinCheck.Coin).Equal(sdk.NewInt(coinCheck.Amount)))
 		}
 	}
 }
