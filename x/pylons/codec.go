8a30689b6e41584af44f1ba08e0ccdf5c781da18
Girish Ramnani <girishramnani95@gmail.com>
2020-04-17 06:55:56 +0530

    add the new module struct and some changes

--- x/pylons/codec.go
+++ x/pylons/codec.go
@@ -6,6 +6,13 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
+// ModuleCdc is the codec for the module
+var ModuleCdc = codec.New()
+
+func init() {
+	RegisterCodec(ModuleCdc)
+}
+
 // RegisterCodec registers concrete types on wire codec
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(msgs.MsgGetPylons{}, "pylons/GetPylons", nil)
