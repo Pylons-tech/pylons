5d6ea25ddea12c932701a3c63772ec220d867ed5
Girish Ramnani <girishramnani95@gmail.com>
2020-04-28 23:28:42 +0530

    added account creation decorator

--- x/pylons/handlers/custom_ante_handler.go
+++ x/pylons/handlers/custom_ante_handler.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
-	"github.com/cosmos/cosmos-sdk/x/auth/exported"
 	"github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 )
@@ -46,45 +48,29 @@ func (svd AccountCreationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simul
 	if ctx.IsReCheckTx() {
 		return next(ctx, tx, simulate)
 	}
-	sigTx, ok := tx.(ante.SigVerifiableTx)
+	sigTx, ok := tx.(types.StdTx)
 	if !ok {
 		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "invalid transaction type")
 	}
+	messages := sigTx.GetMsgs()
+	if len(messages) > 0 && messages[0].Type() == "get_pylons" {
+		pubkey := sigTx.Signatures[0].PubKey
+		address := sdk.AccAddress(pubkey.Address().Bytes())
 
-	// stdSigs contains the sequence number, account number, and signatures.
-	// When simulating, this would just be a 0-length slice.
-	sigs := sigTx.GetSignatures()
+		fmt.Println(address.String() + " received")
+		// if the account doesnt exist we set it
+		if svd.ak.GetAccount(ctx, address) == nil {
+			fmt.Println("Account not found")
+			acc := &auth.BaseAccount{
+				Sequence:      0,
+				Coins:         sdk.Coins{},
+				AccountNumber: svd.ak.GetNextAccountNumber(ctx),
+				PubKey:        pubkey,
+				Address:       address,
+			}
+			svd.ak.SetAccount(ctx, acc)
+		}
 
-	// stdSigs contains the sequence number, account number, and signatures.
-	// When simulating, this would just be a 0-length slice.
-	signerAddrs := sigTx.GetSigners()
-	signerAccs := make([]exported.Account, len(signerAddrs))
-
-	// check that signer length and signature length are the same
-	if len(sigs) != len(signerAddrs) {
-		return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "invalid number of signer;  expected: %d, got %d", len(signerAddrs), len(sigs))
 	}
-
-	for i, sig := range sigs {
-		signerAccs[i], err = ante.GetSignerAcc(ctx, svd.ak, signerAddrs[i])
-		if err != nil {
-			return ctx, err
-		}
-
-		// retrieve signBytes of tx
-		signBytes := sigTx.GetSignBytes(ctx, signerAccs[i])
-
-		// retrieve pubkey
-		pubKey := signerAccs[i].GetPubKey()
-		if !simulate && pubKey == nil {
-			return ctx, sdkerrors.Wrap(sdkerrors.ErrInvalidPubKey, "pubkey on account is not set")
-		}
-
-		// verify signature
-		if !simulate && !pubKey.VerifyBytes(signBytes, sig) {
-			return ctx, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "signature verification failed; verify correct account sequence and chain-id")
-		}
-	}
-
 	return next(ctx, tx, simulate)
 }
