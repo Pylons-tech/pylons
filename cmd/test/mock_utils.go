819f2425aff565f845d99fb6bdd76cfadfd4d515
Andreas Kolovos <andreas.kolovos@gmx.com>
2020-05-11 17:53:37 +0200

    update latest integration test error fix

--- cmd/test/mock_utils.go
+++ cmd/test/mock_utils.go
@@ -151,7 +151,7 @@ func MockDetailedRecipeGUID(
 	err = WaitForNextBlock()
 	ErrValidation(t, "error waiting for creating recipe %+v", err)
 
-	txHandleResBytes, err := GetTxData(txhash, t)
+	txHandleResBytes, err := WaitAndGetTxData(txhash, 3, t)
 	t.MustNil(err)
 	resp := handlers.CreateRecipeResponse{}
 	err = GetAminoCdc().UnmarshalJSON(txHandleResBytes, &resp)
