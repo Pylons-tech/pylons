1853ed973dc60c942d0608b7472d612026336def
ahmedaly113 <ahmedaly113@outlook.com>
2020-05-13 19:18:52 +0100

    initial cloudbuild 3node test setup

---
+++ init_accounts_3n_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+pylonscli keys add michael <<< 11111111
+pylonscli keys add iain <<< 11111111
+pylonscli keys add afti <<< 11111111
+pylonscli keys add girish <<< 11111111
+pylonscli keys add eugen <<< 11111111
