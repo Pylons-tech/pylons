00e66a6c4227cde7ec384dcb01ffe0f9de54076d
Michael Sofaer <msofaer@catalist.us>
2020-05-17 01:07:02 -0400

    test keychain

--- init_accounts.sh
+++ init_accounts.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-pylonscli keys add michael <<< 11111111
-pylonscli keys add iain <<< 11111111
-pylonscli keys add afti <<< 11111111
-pylonscli keys add girish <<< 11111111
-pylonscli keys add eugen <<< 11111111
-pylonsd add-genesis-account $(pylonscli keys show michael -a) 10000000pylon,10000000michaelcoin,10000000loudcoin,100000000stake
-pylonsd add-genesis-account $(pylonscli keys show iain -a) 10000000pylon,10000000iaincoin,10000000loudcoin,100000000stake
-pylonsd add-genesis-account $(pylonscli keys show afti -a) 10000000pylon,10000000afticoin,10000000loudcoin,100000000stake
-pylonsd add-genesis-account $(pylonscli keys show girish -a) 10000000pylon,10000000girishcoin,10000000loudcoin,100000000stake
-pylonsd add-genesis-account $(pylonscli keys show eugen -a) 10000000pylon,10000000eugencoin,10000000loudcoin,100000000stake
+pylonscli keys add michael <<< 11111111 --keyring-backend=test
+pylonscli keys add iain <<< 11111111 --keyring-backend=test
+pylonscli keys add afti <<< 11111111 --keyring-backend=test
+pylonscli keys add girish <<< 11111111 --keyring-backend=test
+pylonscli keys add eugen <<< 11111111 --keyring-backend=test
+pylonsd add-genesis-account $(pylonscli keys show michael -a --keyring-backend=test) 10000000pylon,10000000michaelcoin,10000000loudcoin,100000000stake
+pylonsd add-genesis-account $(pylonscli keys show iain -a --keyring-backend=test) 10000000pylon,10000000iaincoin,10000000loudcoin,100000000stake
+pylonsd add-genesis-account $(pylonscli keys show afti -a --keyring-backend=test) 10000000pylon,10000000afticoin,10000000loudcoin,100000000stake
+pylonsd add-genesis-account $(pylonscli keys show girish -a --keyring-backend=test) 10000000pylon,10000000girishcoin,10000000loudcoin,100000000stake
+pylonsd add-genesis-account $(pylonscli keys show eugen -a --keyring-backend=test) 10000000pylon,10000000eugencoin,10000000loudcoin,100000000stake
