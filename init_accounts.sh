3e42a2df9ff2827f4bdc6c5198e31ced1da52e58
ahmedaly113 <ahmedaly113@outlook.com>
2020-05-18 14:03:35 +0100

    fixed init account sh file

--- init_accounts.sh
+++ init_accounts.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-pylonscli keys add michael <<< 11111111 --keyring-backend=test
-pylonscli keys add iain <<< 11111111 --keyring-backend=test
-pylonscli keys add afti <<< 11111111 --keyring-backend=test
-pylonscli keys add girish <<< 11111111 --keyring-backend=test
-pylonscli keys add eugen <<< 11111111 --keyring-backend=test
+pylonscli keys add michael --keyring-backend=test <<< y
+pylonscli keys add iain --keyring-backend=test <<< y
+pylonscli keys add afti --keyring-backend=test <<< y
+pylonscli keys add girish --keyring-backend=test <<< y
+pylonscli keys add eugen --keyring-backend=test <<< y
 pylonsd add-genesis-account $(pylonscli keys show michael -a --keyring-backend=test) 10000000pylon,10000000michaelcoin,10000000loudcoin,100000000stake
 pylonsd add-genesis-account $(pylonscli keys show iain -a --keyring-backend=test) 10000000pylon,10000000iaincoin,10000000loudcoin,100000000stake
 pylonsd add-genesis-account $(pylonscli keys show afti -a --keyring-backend=test) 10000000pylon,10000000afticoin,10000000loudcoin,100000000stake
