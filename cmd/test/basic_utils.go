810a4bf6ac892dd48c0cbf321a0dc8269de77d4b
ahmedaly113 <ahmedaly113@outlook.com>
2020-05-19 17:48:23 +0100

    Moved node url parser on test from create_cookbook to utils to make it global on all test files

--- cmd/test/basic_utils.go
+++ cmd/test/basic_utils.go
@@ -27,6 +27,12 @@ type CLIOptions struct {
 var CLIOpts CLIOptions
 var cliMux sync.Mutex
 
+var customNode = flag.String("node", "tcp://localhost:26657", "custom node url")
+
+func init() {
+	CLIOpts.CustomNode = customNode
+}
+
 func ReadFile(fileURL string, t *testing.T) []byte {
 	jsonFile, err := os.Open(fileURL)
 	if err != nil {
