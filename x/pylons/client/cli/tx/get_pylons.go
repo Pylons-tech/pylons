97488e87a260a956094d1953bf709dd328641886
miguel1770 <mike1770@outlook.com>
2020-07-01 14:50:40 +0300

    custom balance per get-pylons request

--- x/pylons/client/cli/tx/get_pylons.go
+++ x/pylons/client/cli/tx/get_pylons.go
@@ -29,6 +29,7 @@ const (
 
 // GetPylons implements GetPylons msg transaction
 func GetPylons(cdc *codec.Codec) *cobra.Command {
+	var customBalance int64
 	ccb := &cobra.Command{
 		Use:   "get-pylons",
 		Short: "ask for pylons. 500 pylons per request",
@@ -38,7 +39,7 @@ func GetPylons(cdc *codec.Codec) *cobra.Command {
 			inBuf := bufio.NewReader(cmd.InOrStdin())
 			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(utils.GetTxEncoder(cdc))
 
-			msg := msgs.NewMsgGetPylons(types.NewPylon(DefaultCoinPerRequest), cliCtx.GetFromAddress())
+			msg := msgs.NewMsgGetPylons(types.NewPylon(customBalance), cliCtx.GetFromAddress())
 			err := msg.ValidateBasic()
 			if err != nil {
 				return err
@@ -48,6 +49,7 @@ func GetPylons(cdc *codec.Codec) *cobra.Command {
 			return GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
 		},
 	}
+	ccb.PersistentFlags().Int64Var(&customBalance, "amount", DefaultCoinPerRequest, "The request pylon amount")
 	return ccb
 }
 
