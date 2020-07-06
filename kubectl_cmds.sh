f959bf6744fbc4c6dc1780250c57d62bd4fc9135
ahmedaly113 <ahmedaly113@outlook.com>
2020-07-06 18:12:27 +0100

    kubectl local setup manual complete

--- kubectl_cmds.sh
+++ kubectl_cmds.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 
+kubectl delete configmap genesis
 kubectl create configmap genesis --from-file=./production_config/genesis.json
 
-kubectl delete secret node0-val-key
-kubectl delete secret node0-config
-kubectl delete secret node0-key
+
+kubectl delete secret node0-all-keys
+kubectl delete secret node1-all-keys
+kubectl delete secret node2-all-keys
 
 kubectl create secret generic node0-all-keys --from-file=./production_config/node0/pylonsd/config/priv_validator_key.json --from-file=./production_config/node0/pylonsd/config/config.toml --from-file=./production_config/node0/pylonsd/config/node_key.json
-kubectl create secret generic node0-val-key --from-file=./production_config/node0/pylonsd/config/priv_validator_key.json 
-kubectl create secret generic node0-config --from-file=./production_config/node0/pylonsd/config/config.toml
-kubectl create secret generic node0-key --from-file=./production_config/node0/pylonsd/config/node_key.json
 kubectl apply -f node0-deployment.yaml
 kubectl get pods
 
 kubectl create secret generic node1-all-keys --from-file=./production_config/node1/pylonsd/config/priv_validator_key.json --from-file=./production_config/node1/pylonsd/config/config.toml --from-file=./production_config/node1/pylonsd/config/node_key.json
-kubectl create secret generic node1-val-key --from-file=./production_config/node1/pylonsd/config/priv_validator_key.json 
-kubectl create secret generic node1-config --from-file=./production_config/node1/pylonsd/config/config.toml
-kubectl create secret generic node1-key --from-file=./production_config/node1/pylonsd/config/node_key.json
 kubectl apply -f node1-deployment.yaml
 kubectl get pods
 
 kubectl create secret generic node2-all-keys --from-file=./production_config/node2/pylonsd/config/priv_validator_key.json --from-file=./production_config/node2/pylonsd/config/config.toml --from-file=./production_config/node2/pylonsd/config/node_key.json
-kubectl create secret generic node2-val-key --from-file=./production_config/node2/pylonsd/config/priv_validator_key.json 
-kubectl create secret generic node2-config --from-file=./production_config/node2/pylonsd/config/config.toml
-kubectl create secret generic node2-key --from-file=./production_config/node2/pylonsd/config/node_key.json
 kubectl apply -f node2-deployment.yaml
 kubectl get pods
--- tree_new_run.sh
+++ tree_new_run.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
 docker build -t pylons_node . --target test_server
+
+kubectl delete service/node0
 kubectl delete statefulset.apps/node0
 kubectl delete pod/node0-0
 kubectl apply -f node0-deployment.yaml
+
+kubectl delete service/node1
+kubectl delete statefulset.apps/node1
+kubectl delete pod/node1-0
+kubectl apply -f node1-deployment.yaml
+
+kubectl delete service/node2
+kubectl delete statefulset.apps/node2
+kubectl delete pod/node2-0
+kubectl apply -f node2-deployment.yaml
+
 kubectl get all
 kubectl logs pod/node0-0
