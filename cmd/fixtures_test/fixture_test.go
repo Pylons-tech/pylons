dfbb280bc905cad7cfc55316351e1024d9ff53e4
Eugen Steffen <stalepresh121@outlook.com>
2020-07-14 11:19:57 +1000

    add verify-only test and verify-only document for pylons

--- cmd/fixtures_test/fixture_test.go
+++ cmd/fixtures_test/fixture_test.go
@@ -12,6 +12,7 @@ import (
 var runSerialMode = false
 var useRest = false
 var useKnownCookbook = false
+var verifyOnly = false
 var scenarios = ""
 var accounts = ""
 
@@ -19,6 +20,7 @@ func init() {
 	flag.BoolVar(&runSerialMode, "runserial", false, "true/false value to check if test will be running in parallel")
 	flag.BoolVar(&useRest, "userest", false, "use rest endpoint for Tx send")
 	flag.BoolVar(&useKnownCookbook, "use-known-cookbook", false, "use existing cookbook or not")
+	flag.BoolVar(&verifyOnly, "verify-only", false, "use this flag to only verify")
 	flag.StringVar(&scenarios, "scenarios", "", "custom scenario file names")
 	flag.StringVar(&accounts, "accounts", "", "custom account names")
 }
@@ -27,6 +29,7 @@ func TestFixturesViaCLI(t *testing.T) {
 	flag.Parse()
 	fixturetestSDK.FixtureTestOpts.IsParallel = !runSerialMode
 	fixturetestSDK.FixtureTestOpts.CreateNewCookbook = !useKnownCookbook
+	fixturetestSDK.FixtureTestOpts.VerifyOnly = verifyOnly
 	if useRest {
 		inttestSDK.CLIOpts.RestEndpoint = "http://localhost:1317"
 	}
