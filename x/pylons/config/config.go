41b357c05b4b8172bd3c449389668c89b6f5b396
Eugen Steffen <stalepresh121@outlook.com>
2020-08-05 12:22:38 +1000

    set IsProduction false for default configuration

--- x/pylons/config/config.go
+++ x/pylons/config/config.go
@@ -91,6 +91,7 @@ func ReadConfig() error {
 			},
 		},
 		GoogleIAPPubKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwZsjhk6eN5Pve9pP3uqz2MwBFixvmCRtQJoDQLTEJo3zTd9VMZcXoerQX8cnDPclZWmMZWkO+BWcN1ikYdGHvU2gC7yBLi+TEkhsEkixMlbqOGRdmNptJJhqxuVmXK+drWTb6W0IgQ9g8CuCjZUiMTc0UjHb5mPOE/IhcuTZ0wCHdoqc5FS2spdQqrohvSEP7gR4ZgGzYNI1U+YZHskIEm2qC4ZtSaX9J/fDkAmmJFV2hzeDMcljCxY9+ZM1mdzIpZKwM7O6UdWRpwD1QJ7yXND8AQ9M46p16F0VQuZbbMKCs90NIcKkx6jDDGbVmJrFnUT1Oq1uYxNYtiZjTp+JowIDAQAB",
+		IsProduction:    false,
 	}
 	return nil
 }
