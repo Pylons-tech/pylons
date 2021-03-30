11b0c9389bc0479e7a1c200830e37d07c21cf0d1
ahmedaly113 <ahmedaly113@outlook.com>
2021-03-30 09:28:08 +0800

    golint

--- x/pylons/msgs/update_recipe_test.go
+++ x/pylons/msgs/update_recipe_test.go
@@ -4,9 +4,9 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"github.com/Pylons-tech/pylons/x/pylons/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 )
 
 func TestUpdateRecipeValidateBasic(t *testing.T) {
