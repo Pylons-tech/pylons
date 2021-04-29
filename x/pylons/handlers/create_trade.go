1a8392f9d685509b80a826cb5d53095fabd2dd23
ahmedaly113 <ahmedaly113@outlook.com>
2021-04-29 13:01:41 +1000

    sdk code standard

--- x/pylons/handlers/create_trade.go
+++ x/pylons/handlers/create_trade.go
@@ -4,15 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
-	"testing"
 
+	"github.com/Pylons-tech/pylons/x/pylons/config"
 	"github.com/Pylons-tech/pylons/x/pylons/keeper"
 	"github.com/Pylons-tech/pylons/x/pylons/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/require"
 )
 
 // CreateTrade is used to create a trade by a user
@@ -82,6 +79,239 @@ func (k msgServer) CreateTrade(ctx context.Context, msg *types.MsgCreateTrade) (
 	return &types.MsgCreateTradeResponse{
 		TradeID: trade.ID,
 		Message: "successfully created a trade",
+		Status:  "Success",
+	}, nil
+}
+
+// FulfillTrade is used to fulfill a trade
+func (k msgServer) FulfillTrade(ctx context.Context, msg *types.MsgFulfillTrade) (*types.MsgFulfillTradeResponse, error) {
+	err := msg.ValidateBasic()
+	if err != nil {
+		return nil, errInternal(err)
+	}
+
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	sender, _ := sdk.AccAddressFromBech32(msg.Sender)
+
+	trade, err := k.GetTrade(sdkCtx, msg.TradeID)
+	if err != nil {
+		return nil, errInternal(err)
+	}
+
+	if trade.Completed {
+		return nil, errInternal(errors.New("this trade is already completed"))
+	}
+
+	if len(msg.ItemIDs) != len(trade.ItemInputs) {
+		return nil, errInternal(errors.New("the item IDs count doesn't match the trade input"))
+	}
+
+	items, err := GetItemsFromIDs(sdkCtx, k.Keeper, msg.ItemIDs, sender)
+	if err != nil {
+		return nil, errInternal(err)
+	}
+
+	// get pylons LLC address
+	pylonsLLCAddress, err := sdk.AccAddressFromBech32(config.Config.Validators.PylonsLLC)
+	if err != nil {
+		return nil, errInternal(err)
+	}
+
+	// check if the sender has all condition met
+	totalItemTransferFee := int64(0)
+	matchedItems := types.ItemList{}
+	for i, itemInput := range trade.ItemInputs {
+		matchedItem := items[i]
+		ec, err := k.EnvCollection(sdkCtx, "", msg.TradeID, matchedItem)
+		if err != nil {
+			return nil, errInternal(fmt.Errorf("error creating env collection for %s item", matchedItem.String()))
+		}
+		matchErr := itemInput.MatchError(matchedItem, ec)
+		if matchErr != nil {
+			return nil, errInternal(fmt.Errorf("[%d]th item does not match: %s item_id=%s", i, matchErr.Error(), matchedItem.ID))
+		}
+		if err = matchedItem.NewTradeError(); err != nil {
+			return nil, errInternal(fmt.Errorf("[%d]th item is not tradable: %s item_id=%s", i, err.Error(), matchedItem.ID))
+		}
+		totalItemTransferFee += matchedItem.CalculateTransferFee()
+		matchedItems = append(matchedItems, matchedItem)
+	}
+	tradeSender, err := sdk.AccAddressFromBech32(trade.Sender)
+	if err != nil {
+		return nil, errInternal(err)
+	}
+	// Unlock trade creator's coins
+	err = k.UnlockCoin(sdkCtx, types.NewLockedCoin(tradeSender, trade.CoinOutputs))
+	if err != nil {
+		return nil, errInternal(err)
+	}
+
+	inputCoins := types.CoinInputList(trade.CoinInputs).ToCoins()
+	if !keeper.HasCoins(k.Keeper, sdkCtx, sender, inputCoins) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "the sender doesn't have sufficient coins")
+	}
+
+	if !keeper.HasCoins(k.Keeper, sdkCtx, tradeSender, trade.CoinOutputs) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "the trade creator doesn't have sufficient coins")
+	}
+
+	// -------------------- handle Item interactions ----------------
+	refreshedOutputItems := types.ItemList{}
+
+	// get the items from the trade initiator
+	for _, item := range trade.ItemOutputs {
+		// verify if its still owned by the initiator
+		storedItem, err := k.GetItem(sdkCtx, item.ID)
+		if err != nil {
+			return nil, errInternal(err)
+		}
+		// if it isn't then we error out as there hasn't been any state changes so far
+		if storedItem.Sender != trade.Sender {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, fmt.Sprintf("Item with id %s is not owned by the trade creator", storedItem.ID))
+		}
+
+		if err = storedItem.FulfillTradeError(trade.ID); err != nil {
+			return nil, errInternal(fmt.Errorf("%s item id is not tradable", storedItem.ID))
+		}
+
+		totalItemTransferFee += storedItem.CalculateTransferFee()
+
+		refreshedOutputItems = append(refreshedOutputItems, storedItem)
+	}
+
+	// ----------------- handle coin interaction ----------------------
+	inputPylonsAmount := inputCoins.AmountOf(types.Pylon)
+	outputPylonsAmount := trade.CoinOutputs.AmountOf(types.Pylon)
+	totalPylonsAmount := inputPylonsAmount.Int64() + outputPylonsAmount.Int64()
+
+	// Select bigger one between total additional fee and total pylons amount
+	tradePercent := config.Config.Fee.PylonsTradePercent
+	totalPylonsAmountFee := totalPylonsAmount * tradePercent / 100
+
+	totalFee := Max(totalItemTransferFee, totalPylonsAmountFee)
+	// if total fee exceeds the total pylons amount, it fails
+	if totalFee > totalPylonsAmount {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "total pylons amount is not enough to pay fees")
+	}
+
+	// divide total fee between the sender and fullfiller
+	if totalPylonsAmount == 0 {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "totalPylonsAmount is 0 unexpectedly")
+	}
+
+	// trade creator to trade acceptor the coin output
+	// Send output coin from sender to fullfiller
+
+	err = keeper.SendCoins(k.Keeper, sdkCtx, tradeSender, sender, trade.CoinOutputs)
+
+	if err != nil {
+		return nil, errInternal(err)
+	}
+	senderFee := totalFee * outputPylonsAmount.Int64() / totalPylonsAmount
+	if senderFee != 0 {
+		// sender process fee after receiving coins from trade.Sender
+		err = keeper.SendCoins(k.Keeper, sdkCtx, sender, pylonsLLCAddress, types.NewPylon(senderFee))
+		if err != nil {
+			return nil, errInternal(err)
+		}
+	}
+
+	// trade acceptor to trade creator the coin input
+	// Send input coin from fullfiller to sender (trade creator)
+	err = keeper.SendCoins(k.Keeper, sdkCtx, sender, tradeSender, inputCoins)
+	if err != nil {
+		return nil, errInternal(err)
+	}
+	fulfillerFee := totalFee - senderFee
+	if fulfillerFee != 0 {
+		// trade.Sender process fee after receiving coins from sender
+		err = keeper.SendCoins(k.Keeper, sdkCtx, tradeSender, pylonsLLCAddress, types.NewPylon(fulfillerFee))
+		if err != nil {
+			return nil, errInternal(err)
+		}
+	}
+
+	totalFeeForCBOwners := totalFee * config.Config.Fee.ItemTransferCookbookOwnerProfitPercent / 100
+
+	for _, item := range refreshedOutputItems {
+
+		if totalItemTransferFee == 0 {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "totalItemTransferFee is 0 unexpectedly")
+		}
+		feeForCB := totalFeeForCBOwners * item.CalculateTransferFee() / totalItemTransferFee
+
+		cookbook, err := k.GetCookbook(sdkCtx, item.CookbookID)
+		if err != nil {
+			return nil, errInternal(errors.New("Invalid cookbook id"))
+		}
+
+		cookbookSender, err := sdk.AccAddressFromBech32(cookbook.Sender)
+		if err != nil {
+			return nil, errInternal(err)
+		}
+
+		if feeForCB > 0 {
+			err = keeper.SendCoins(k.Keeper, sdkCtx, pylonsLLCAddress, cookbookSender, types.NewPylon(feeForCB))
+			if err != nil {
+				return nil, errInternal(err)
+			}
+		}
+
+		item.Sender = sender.String()
+
+		k.SetItemHistory(sdkCtx, types.ItemHistory{
+			ID:      types.KeyGen(sender),
+			Owner:   sender,
+			ItemID:  item.ID,
+			TradeID: item.OwnerTradeID,
+		})
+
+		item.OwnerTradeID = ""
+		err = k.SetItem(sdkCtx, item)
+		if err != nil {
+			return nil, errInternal(err)
+		}
+	}
+
+	for _, item := range matchedItems {
+		if totalItemTransferFee == 0 {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "totalItemTransferFee is 0 unexpectedly")
+		}
+		feeForCB := totalFeeForCBOwners * item.CalculateTransferFee() / totalItemTransferFee
+
+		cookbook, err := k.GetCookbook(sdkCtx, item.CookbookID)
+		if err != nil {
+			return nil, errInternal(errors.New("Invalid cookbook id"))
+		}
+
+		cookbookSender, err := sdk.AccAddressFromBech32(cookbook.Sender)
+		if err != nil {
+			return nil, errInternal(err)
+		}
+
+		if feeForCB > 0 {
+			err = keeper.SendCoins(k.Keeper, sdkCtx, pylonsLLCAddress, cookbookSender, types.NewPylon(feeForCB))
+			if err != nil {
+				return nil, errInternal(err)
+			}
+		}
+
+		item.Sender = trade.Sender
+		err = k.SetItem(sdkCtx, item)
+		if err != nil {
+			return nil, errInternal(err)
+		}
+	}
+
+	trade.FulFiller = sender.String()
+	trade.Completed = true
+	err = k.SetTrade(sdkCtx, trade)
+	if err != nil {
+		return nil, errInternal(err)
+	}
+
+	return &types.MsgFulfillTradeResponse{
+		Message: "successfully fulfilled the trade",
 		Status:  "Success",
 	}, nil
 }
@@ -213,228 +443,4 @@ func (k msgServer) DisableTrade(ctx context.Context, msg *types.MsgDisableTrade)
 		Message: "successfully disabled the trade",
 		Status:  "Success",
 	}, nil
-}
-
-func TestHandlerMsgDisableTrade(t *testing.T) {
-	tci := keeper.SetupTestCoinInput()
-	tci.PlnH = NewMsgServerImpl(tci.PlnK)
-	sender, sender2, _, _ := keeper.SetupTestAccounts(t, tci, types.NewPylon(100000), nil, nil, nil)
-
-	err := tci.Bk.AddCoins(tci.Ctx, sender2, types.NewPylon(100000))
-	require.NoError(t, err)
-
-	id := uuid.New()
-	id2 := uuid.New()
-	id3 := uuid.New()
-	id4 := uuid.New()
-
-	cookbookMsg := types.NewMsgCreateCookbook(
-		"cookbook-0001",
-		"cookbook-id-0001",
-		"this has to meet character limits",
-		"SketchyCo",
-		"1.0.0",
-		"example@example.com",
-		1,
-		types.DefaultCostPerBlock,
-		sender.String(),
-	)
-	cookbookResult, _ := tci.PlnH.CreateCookbook(sdk.WrapSDKContext(tci.Ctx), &cookbookMsg)
-	require.True(t, len(cookbookResult.CookbookID) > 0)
-
-	item := keeper.GenItem(cookbookResult.CookbookID, sender, "Raichu")
-	item.OwnerTradeID = id.String()
-	err = tci.PlnK.SetItem(tci.Ctx, item)
-	require.NoError(t, err)
-
-	// add trades for tests, one open trade by each sender and one closed trade
-	err = tci.PlnK.SetTrade(tci.Ctx, types.Trade{
-		ID:          id.String(),
-		ItemInputs:  types.GenTradeItemInputList("LOUD-CB-001", []string{"Pikachu"}),
-		ItemOutputs: types.ItemList{item},
-		CoinOutputs: types.NewPylon(10000),
-		Sender:      sender.String(),
-	})
-	require.NoError(t, err)
-
-	err = tci.PlnK.SetTrade(tci.Ctx, types.Trade{
-		ID:          id2.String(),
-		ItemInputs:  types.GenTradeItemInputList("LOUD-CB-001", []string{"Richu"}),
-		CoinOutputs: types.NewPylon(10000),
-		Sender:      sender2.String(),
-	})
-	require.NoError(t, err)
-
-	err = tci.PlnK.SetTrade(tci.Ctx, types.Trade{
-		ID:          id3.String(),
-		ItemInputs:  types.GenTradeItemInputList("LOUD-CB-001", []string{"Pichu"}),
-		CoinOutputs: types.NewPylon(1000),
-		Sender:      sender2.String(),
-		FulFiller:   sender.String(),
-		Completed:   true,
-	})
-	require.NoError(t, err)
-
-	err = tci.PlnK.SetTrade(tci.Ctx, types.Trade{
-		ID:          id4.String(),
-		ItemInputs:  types.GenTradeItemInputList("LOUD-CB-001", []string{"Pikachu"}),
-		ItemOutputs: types.ItemList{item},
-		CoinOutputs: types.NewPylon(10000),
-		Sender:      sender2.String(),
-	})
-	require.NoError(t, err)
-
-	cases := map[string]struct {
-		tradeID      string
-		sender       sdk.AccAddress
-		desiredError string
-		showError    bool
-	}{
-		"disable a trade successfully": {
-			tradeID: id.String(),
-			sender:  sender,
-		},
-		"disable a trade failure due to unauthorized sender": {
-			tradeID:      id2.String(),
-			showError:    true,
-			sender:       sender,
-			desiredError: "Trade initiator is not the same as sender",
-		},
-		"disable a completed trade with failure": {
-			tradeID:      id3.String(),
-			showError:    true,
-			sender:       sender2,
-			desiredError: "Cannot disable a completed trade",
-		},
-		"disable wrong item id owner trade with failure": {
-			tradeID:      id4.String(),
-			showError:    true,
-			sender:       sender2,
-			desiredError: "is not owned by the trade creator",
-		},
-	}
-
-	for name, tc := range cases {
-		t.Run(name, func(t *testing.T) {
-
-			delTrdMsg := types.NewMsgDisableTrade(tc.tradeID, tc.sender.String())
-			_, err := tci.PlnH.DisableTrade(sdk.WrapSDKContext(tci.Ctx), &delTrdMsg)
-			if tc.showError == false {
-				trd, _ := tci.PlnK.GetTrade(tci.Ctx, tc.tradeID)
-				require.True(t, trd.Disabled)
-				if trd.ItemOutputs != nil && len(trd.ItemOutputs) > 0 {
-					require.True(t, trd.ItemOutputs[0].OwnerTradeID == "")
-				}
-			} else {
-				require.True(t, err != nil)
-				require.True(t, strings.Contains(err.Error(), tc.desiredError), err.Error())
-			}
-		})
-	}
-
-}
-
-func TestHandlerMsgEnableTrade(t *testing.T) {
-	tci := keeper.SetupTestCoinInput()
-	tci.PlnH = NewMsgServerImpl(tci.PlnK)
-	sender, sender2, _, _ := keeper.SetupTestAccounts(t, tci, types.NewPylon(100000), nil, nil, nil)
-
-	err := tci.Bk.AddCoins(tci.Ctx, sender2, types.NewPylon(100000))
-	require.NoError(t, err)
-
-	id := uuid.New()
-	id2 := uuid.New()
-	id3 := uuid.New()
-
-	cookbookMsg := types.NewMsgCreateCookbook(
-		"cookbook-0001",
-		"cookbook-id-0001",
-		"this has to meet character limits",
-		"SketchyCo",
-		"1.0.0",
-		"example@example.com",
-		1,
-		types.DefaultCostPerBlock,
-		sender.String(),
-	)
-
-	cookbookResult, _ := tci.PlnH.CreateCookbook(sdk.WrapSDKContext(tci.Ctx), &cookbookMsg)
-	require.True(t, len(cookbookResult.CookbookID) > 0)
-
-	item := keeper.GenItem(cookbookResult.CookbookID, sender, "Raichu")
-	// item.OwnerTradeID = id.String()
-	err = tci.PlnK.SetItem(tci.Ctx, item)
-	require.NoError(t, err)
-
-	// add 3 trades. one open trade by each sender and one closed trade
-	err = tci.PlnK.SetTrade(tci.Ctx, types.Trade{
-		ID:          id.String(),
-		ItemInputs:  types.GenTradeItemInputList("LOUD-CB-001", []string{"Pikachu"}),
-		ItemOutputs: types.ItemList{item},
-		CoinOutputs: types.NewPylon(10000),
-		Sender:      sender.String(),
-		Disabled:    true, // we disable this trade initially
-	})
-	require.NoError(t, err)
-
-	err = tci.PlnK.SetTrade(tci.Ctx, types.Trade{
-		ID:          id2.String(),
-		ItemInputs:  types.GenTradeItemInputList("LOUD-CB-001", []string{"Richu"}),
-		CoinOutputs: types.NewPylon(10000),
-		Sender:      sender2.String(),
-	})
-	require.NoError(t, err)
-
-	err = tci.PlnK.SetTrade(tci.Ctx, types.Trade{
-		ID:          id3.String(),
-		ItemInputs:  types.GenTradeItemInputList("LOUD-CB-001", []string{"Pikachu"}),
-		ItemOutputs: types.ItemList{item},
-		CoinOutputs: types.NewPylon(10000),
-		Sender:      sender2.String(),
-		Disabled:    true, // we disable this trade initially
-	})
-	require.NoError(t, err)
-
-	cases := map[string]struct {
-		tradeID      string
-		sender       sdk.AccAddress
-		desiredError string
-		showError    bool
-	}{
-		"enable a trade successfully": {
-			tradeID: id.String(),
-			sender:  sender,
-		},
-		"enable a trade failure due to unauthorized sender": {
-			tradeID:      id2.String(),
-			showError:    true,
-			sender:       sender,
-			desiredError: "Trade initiator is not the same as sender",
-		},
-		"enable wrong item id owner trade with failure": {
-			tradeID:      id3.String(),
-			showError:    true,
-			sender:       sender2,
-			desiredError: "is not owned by the trade creator",
-		},
-	}
-
-	for name, tc := range cases {
-		t.Run(name, func(t *testing.T) {
-
-			delTrdMsg := types.NewMsgEnableTrade(tc.tradeID, tc.sender.String())
-			_, err := tci.PlnH.EnableTrade(sdk.WrapSDKContext(tci.Ctx), &delTrdMsg)
-			if tc.showError == false {
-				trd, _ := tci.PlnK.GetTrade(tci.Ctx, tc.tradeID)
-				require.True(t, !trd.Disabled)
-				if trd.ItemOutputs != nil && len(trd.ItemOutputs) > 0 {
-					require.True(t, trd.ItemOutputs[0].OwnerTradeID != "")
-				}
-			} else {
-				require.True(t, err != nil)
-				require.True(t, strings.Contains(err.Error(), tc.desiredError), err.Error())
-			}
-		})
-	}
-
 }
