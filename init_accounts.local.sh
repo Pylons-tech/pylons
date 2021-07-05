5e7cd242379cb99e93d4d06e74468de5ab45150e
andraderaf <rafael.andrade@tendermint.com>
2021-07-05 14:03:33 -0400

    Changing shebang in order to leverage bash

--- init_accounts.local.sh
+++ init_accounts.local.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -rf $HOME/.pylonsd
 
 pylonsd init masternode --chain-id=pylonschain --home=$HOME/.pylonsd
 
 pylonsd keys add node0 --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "cat indoor zoo vivid actress steak female fat shrug payment harvest sadness hazard frown alcohol mountain erode latin symbol peace repair inspire blade supply"
+
 pylonsd keys add michael --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "primary push only kiwi elephant give nut roast nature fury jaguar certain distance endorse earn reform fatal edge mother submit team neither gaze whip"
+
 pylonsd keys add eugen --keyring-backend=test --home=$HOME/.pylonsd --recover <<< "shiver pencil sauce original thank real stick armed inform cradle very elder drink planet scheme assault test science kite better chronic visa village order"
 
 pylonsd add-genesis-account cosmos105wr8t6y97rwv90xzhxd4juj4lsajtjaass6h7 10000000000pylon # Pylons LLC validator
