21392b0c67dd541311ef8cd62aecde5687c4af23
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2021-07-06 14:20:53 +0200

    Set correct paths in scripts/protoc_gen.sh

--- scripts/protoc_gen.sh
+++ scripts/protoc_gen.sh
@@ -4,22 +4,22 @@ COSMOS_VERSION=$(go list -m github.com/cosmos/cosmos-sdk | sed 's:.* ::')
 TEMPDIR="github.com/Pylons-tech/pylons/"
 GOPATH=$(go env GOPATH)
 
-PROTO_DIRS=$(find ./proto -path -prune -o -name '*.proto')
+PROTO_DIRS=$(find ../proto -path -prune -o -name '*.proto')
 for dir in $PROTO_DIRS; do
   protoc \
-    -I ./proto \
+    -I ../proto \
     -I ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${COSMOS_VERSION}/proto \
     -I ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${COSMOS_VERSION}/third_party/proto \
     --gocosmos_out=plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
     ${dir}
 
   protoc \
-    -I ./proto \
+    -I ../proto \
     -I ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${COSMOS_VERSION}/proto \
     -I ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${COSMOS_VERSION}/third_party/proto \
     --grpc-gateway_out=logtostderr=true:. \
     ${dir}
 done
 
-$(cp -R ${TEMPDIR}x/* ./x)
+$(cp -R ${TEMPDIR}x/* ../x)
 $(rm -r ./github.com)
