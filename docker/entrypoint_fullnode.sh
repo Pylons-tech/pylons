65f9a87b0a7fc1a11ae2a979bcf00fa35f87785f
andraderaf <rafael.andrade@tendermint.com>
2021-07-29 10:44:49 -0400

    Fixing entrypoint script, putting the correct ports to make the connections between peers

--- docker/entrypoint_fullnode.sh
+++ docker/entrypoint_fullnode.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Add the seeds to the fullnode to work.
-seedinfo="$(curl -s validator:26657/status |jq -r '.result.node_info.id')@validator:26657"
+seedinfo="$(curl -s validator:26657/status |jq -r '.result.node_info.id')@validator:26656"
 
-sed -c -i "s/\(seeds *= *\).*/\1$seedinfo/" $HOME/.pylonsd/config/config.toml
-sed -c -i "s/\(persistent_peers *= *\).*/\1$seedinfo/" $HOME/.pylonsd/config/config.toml
+sed -ie "s/seeds = \"\"/seeds = \"$seedinfo\"/" $HOME/.pylonsd/config/config.toml
+sed -ie "s/persistent\_peers = \"\"/persistent\_peers = \"$seedinfo\"/" $HOME/.pylonsd/config/config.toml
 
 pylonsd start --home=$HOME/.pylonsd
