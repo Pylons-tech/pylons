402ae540c11617b18254b12007a3e1cc374610ce
andraderaf <rafael.andrade@tendermint.com>
2021-07-29 11:51:36 -0400

    Using command line to connect peers

--- docker/entrypoint_fullnode.sh
+++ docker/entrypoint_fullnode.sh
@@ -3,7 +3,7 @@
 # Add the seeds to the fullnode to work.
 seedinfo="$(curl -s validator:26657/status |jq -r '.result.node_info.id')@validator:26656"
 
-sed -ie "s/seeds = \"\"/seeds = \"$seedinfo\"/" $HOME/.pylonsd/config/config.toml
-sed -ie "s/persistent\_peers = \"\"/persistent\_peers = \"$seedinfo\"/" $HOME/.pylonsd/config/config.toml
-
-pylonsd start --home=$HOME/.pylonsd
+pylonsd start \
+  --home=$HOME/.pylonsd \
+  --p2p.seeds $seedinfo
+  --p2p.persistent_peers $seedinfo
