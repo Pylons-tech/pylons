118005a42eb0a375cd8fb3964e3ea7b5640c0eaf
andraderaf <rafael.andrade@tendermint.com>
2021-08-04 09:21:57 -0400

    Generated testnet files

--- genesis/generate_genesis.sh
+++ genesis/generate_genesis.sh
@@ -5,13 +5,13 @@ rm -rf $HOME/.pylonsd
 pylonsd init pylonsvalidator --chain-id=pylonschain --home=$HOME/.pylonsd
 
 
-pylonsd keys add pylonsinc --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "achieve patch rabbit delay you stairs flag nation lady outside answer property olive man dwarf used chapter sunset bundle sniff between since review logic"
-pylonsd keys add tendermint --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "oil inflict cup nation circle palace cactus uphold clarify vibrant believe stem farm aerobic casino treat early amazing remain produce exhaust bachelor flame own"
+pylonsd keys add pylonsinc --nosort --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "achieve patch rabbit delay you stairs flag nation lady outside answer property olive man dwarf used chapter sunset bundle sniff between since review logic"
+pylonsd keys add tendermint --nosort --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "oil inflict cup nation circle palace cactus uphold clarify vibrant believe stem farm aerobic casino treat early amazing remain produce exhaust bachelor flame own"
 
 # The three initial accounts
-pylonsd keys add helder --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "stick tennis puppy mechanic pass basic scout number ice ticket cave license scale sugar scout bench correct media danger badge public gauge metal firm"
-pylonsd keys add jacob  --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "fox smart obtain similar west hub long envelope swallow pitch call joke cradle balcony general hint surprise segment total small body park pottery elite"
-pylonsd keys add jack   --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "please hurt play boost stadium pitch someone purity extra victory one story head setup learn trick feed infant monkey ring anxiety now upon piece"
+pylonsd keys add helder --nosort --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "stick tennis puppy mechanic pass basic scout number ice ticket cave license scale sugar scout bench correct media danger badge public gauge metal firm"
+pylonsd keys add jacob  --nosort --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "fox smart obtain similar west hub long envelope swallow pitch call joke cradle balcony general hint surprise segment total small body park pottery elite"
+pylonsd keys add jack   --nosort --keyring-backend=test  --home=$HOME/.pylonsd --recover <<< "please hurt play boost stadium pitch someone purity extra victory one story head setup learn trick feed infant monkey ring anxiety now upon piece"
 
 
 pylonsd add-genesis-account $(pylonsd keys show pylonsinc -a --keyring-backend=test --home=$HOME/.pylonsd) 9000000000pylon,900000000bedrock
@@ -23,17 +23,18 @@ pylonsd add-genesis-account $(pylonsd keys show jack -a --keyring-backend=test -
 
 mkdir -p $HOME/.pylonsd/config/gentx/
 
-pylonsd gentx helder --from $(pylonsd keys show helder -a --keyring-backend=test --home=$HOME/.pylonsd)  1bedrock --account-number 0 --chain-id=pylonschain --moniker="alpha" --min-self-delegation 1 --commission-max-change-rate=0.01 --commission-max-rate=1.0 --commission-rate=0.07 --output-document $HOME/.pylonsd/config/gentx/alpha.json
-pylonsd gentx jacob --from $(pylonsd keys show jacob -a --keyring-backend=test --home=$HOME/.pylonsd)  1bedrock --account-number 1 --chain-id=pylonschain --moniker="beta" --min-self-delegation 1 --commission-max-change-rate=0.01 --commission-max-rate=1.0 --commission-rate=0.07 --output-document $HOME/.pylonsd/config/gentx/beta.json
-pylonsd gentx jack  --from $(pylonsd keys show jack -a --keyring-backend=test --home=$HOME/.pylonsd)  1bedrock --account-number 2 --chain-id=pylonschain --moniker="gamma" --min-self-delegation 1 --commission-max-change-rate=0.01 --commission-max-rate=1.0 --commission-rate=0.07 --output-document $HOME/.pylonsd/config/gentx/gamma.json
+pylonsd gentx helder --offline --from $(pylonsd keys show helder -a --keyring-backend=test --home=$HOME/.pylonsd)  1bedrock --account-number 2 --chain-id=pylonschain --moniker="alpha" --min-self-delegation 1 --commission-max-change-rate=0.01 --commission-max-rate=1.0 --commission-rate=0.07 --output-document $HOME/.pylonsd/config/gentx/alpha.json
+pylonsd gentx jacob --offline --from $(pylonsd keys show jacob -a --keyring-backend=test --home=$HOME/.pylonsd)  1bedrock --account-number 3 --chain-id=pylonschain --moniker="beta" --min-self-delegation 1 --commission-max-change-rate=0.01 --commission-max-rate=1.0 --commission-rate=0.07 --output-document $HOME/.pylonsd/config/gentx/beta.json
+pylonsd gentx jack --offline  --from $(pylonsd keys show jack -a --keyring-backend=test --home=$HOME/.pylonsd)  1bedrock --account-number 4 --chain-id=pylonschain --moniker="gamma" --min-self-delegation 1 --commission-max-change-rate=0.01 --commission-max-rate=1.0 --commission-rate=0.07 --output-document $HOME/.pylonsd/config/gentx/gamma.json
 
+sed -i 's/stake/bedrock/g' $HOME/.pylonsd/config/genesis.json
 
 
 
 pylonsd collect-gentxs  --home=$HOME/.pylonsd
 
 # sed command for osx
-sed -i 's/stake/bedrock/g' $HOME/.pylonsd/config/genesis.json
+
 
 # # sed command for ubuntu
 # sed -i 's/enable = false/enable = true/g' $HOME/.pylonsd/config/app.toml
