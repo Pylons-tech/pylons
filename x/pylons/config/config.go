4c32cddab5c4c7f302c5efefaece7628dc9685c0
aljo242 <alex@shmeeload.xyz>
2021-08-06 11:58:56 -0400

    hard-coded stripe config

--- x/pylons/config/config.go
+++ x/pylons/config/config.go
@@ -106,6 +106,8 @@ func ReadConfig() error {
 			return cfg
 		}
 	}
+
+
 	Config = Configuration{
 		Fee: FeeConfiguration{
 			RecipePercent:                          10,
@@ -147,9 +149,9 @@ func ReadConfig() error {
 			},
 		},
 		StripeConfig: StripeConfiguration{
-			StripeSecretKey:   "StripeSecretKey",
-			StripePubKey:      "StripePubKey",
-			StripeClientID:    "StripeClientID",
+			StripeSecretKey:   "sk_test_51Iy5pOEdpQgutKvr1vkMGl6z4KoqNOkHCtfSskbJBYhpfwuemglQtcNn4XqgiqooiStO9P1k4rxUd83CUqV3kWqM00zG97ZVD8",
+			StripePubKey:      "pk_test_51Iy5pOEdpQgutKvrWuZVvM0fPB4LRm0tzP1y17hWDCxBcISFb6X07BtIpOQArSfQgApOceoAwUX0qJXPC07OBLCn005w7KGtWE",
+			StripeClientID:    "ca_Jd44I5yQ4pKBpePVtiROVcTFRzQVU6cT",
 			StripeRedirectURI: "https://wallet.pylons.tech",
 			StripeCancelURI:   "https://wallet.pylons.tech/cancel",
 			StripeCountry:     "us",
@@ -160,5 +162,7 @@ func ReadConfig() error {
 		},
 		IsProduction: false,
 	}
+
 	return nil
+	
 }
