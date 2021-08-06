3a347c00eb690b3c1faf6f1e0501a593f2960810
andraderaf <rafael.andrade@tendermint.com>
2021-08-06 15:33:28 -0400

    removing unused files

--- docker/entrypoint_fullnode.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Add the seeds to the fullnode to work.
-seedinfo="$(curl -s validator:26657/status |jq -r '.result.node_info.id')@validator:26656"
-
-pylonsd start \
-  --home=$HOME/.pylonsd \
-  --p2p.seeds $seedinfo
-  --p2p.persistent_peers $seedinfo
