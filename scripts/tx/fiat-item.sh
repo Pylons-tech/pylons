ca10be2df91969c06b9695a01d13450c1d9bb3af
Alex Johnson <alex.johnson@tendermint.com>
2021-08-06 11:30:56 -0400

    Delete fiat-item.sh

--- scripts/tx/fiat-item.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pylonsd tx pylons fiat-item ./sample_txs/pylons_module_cli/fiat_item.json --cookbookID=cosmos19vlpdf25cxh0w2s80z44r9ktrgzncf7zsaqey280460012-a00d-4932-9561-5ddc222265d0 --from=eugen --keyring-backend=test --home=$HOME/.pylonsd --chain-id=pylonschain --yes
