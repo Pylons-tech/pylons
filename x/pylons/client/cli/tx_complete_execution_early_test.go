9f4c8191e073cafa168e2d9a89a0207d74e46780
Rafael de Andrade <rafael.andrade@tendermint.com>
2021-09-08 20:34:03 -0400

    Created initial part of the test that creates the executions

--- x/pylons/client/cli/tx_complete_execution_early_test.go
+++ x/pylons/client/cli/tx_complete_execution_early_test.go
@@ -1,11 +1,11 @@
 package cli_test
 
 import (
+	"encoding/json"
 	"fmt"
-	tmcli "github.com/tendermint/tendermint/libs/cli"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"reflect"
+	"github.com/Pylons-tech/pylons/testutil/network"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"testing"
 
 	"github.com/Pylons-tech/pylons/x/pylons/types"
@@ -16,87 +16,94 @@ import (
 	"github.com/Pylons-tech/pylons/x/pylons/client/cli"
 )
 
-package cli_test
-
-
-
-import (
-"fmt"
-"reflect"
-"testing"
-
-"google.golang.org/grpc/codes"
-
-clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
-"github.com/stretchr/testify/require"
-tmcli "github.com/tendermint/tendermint/libs/cli"
-"google.golang.org/grpc/status"
-
-"github.com/Pylons-tech/pylons/x/pylons/client/cli"
-"github.com/Pylons-tech/pylons/x/pylons/types"
-)
-
 func TestCmdCompleteExecutionEarly(t *testing.T) {
-	net, executions := networkWithExecutionObjects(t,2)
 
-	ctx := net.Validators[0].ClientCtx
+	net := network.New(t)
+	val := net.Validators[0]
+	ctx := val.ClientCtx
+	cookbookID := "testCookbookID"
+	recipeID := "testRecipeID"
+
+	// Create a cookbook
+
+	cbFields := []string{
+		"testCookbookName",
+		"DescriptionDescriptionDescription",
+		"testDeveloper",
+		"v0.0.1",
+		"test@email.com",
+		"{\"denom\": \"pylons\", \"amount\": \"1\"}",
+		"true",
+	}
+
+	//Common arguments
 	common := []string{
-		fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(net.Config.BondDenom, sdk.NewInt(10))).String()),
 	}
-	for _, tc := range [] struct {
-		desc       string
-		recipeId   string
-		cookbookId string
-		args []string
-		err  error
-		obj  types.QueryListExecutionsByRecipeResponse
-	}{
-		{
-			desc:        "found1",
-			recipeId:    executions[0].ID,
-			cookbookId : executions[0].CookbookID,
-			args:        common,
-			obj:         types.QueryListExecutionsByRecipeResponse{},
+	args := []string{cookbookID}
+	args = append(args, cbFields...)
+	args = append(args, common...)
+	_, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateCookbook(), args)
+	require.NoError(t, err)
+
+
+	coinInputsJson, _ := json.Marshal([] types.CoinInput{
+		types.CoinInput{
+			Coins: sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(1))),
 		},
-		{
-			desc:        "found2",
-			recipeId:    executions[1].ID,
-			cookbookId : executions[1].CookbookID,
-			args:        common,
-			obj:         types.QueryListExecutionsByRecipeResponse{},
+		types.CoinInput{
+			Coins: sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(1))),
 		},
-		{
-			desc:       "not found",
-			recipeId:   types.GenTestBech32FromString("not_found"),
-			cookbookId: types.GenTestBech32FromString("not_found"),
-			args:       common,
-			obj:        types.QueryListExecutionsByRecipeResponse{},
-		},
-		{
-			desc:       "invalid",
-			recipeId:   "not_found",
-			cookbookId: "not_found",
-			args:       common,
-			err:        status.Error(codes.InvalidArgument, "invalid address"),
-		},
-	} {
-		tc := tc
-		t.Run(tc.desc, func(t *testing.T) {
-			args := []string{tc.cookbookId,tc.recipeId}
-			args = append(args, tc.args...)
-			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListExecutionsByRecipe(), args)
-			if tc.err != nil {
-				stat, ok := status.FromError(tc.err)
-				require.True(t, ok)
-				require.ErrorIs(t, stat.Err(), tc.err)
-			} else {
-				require.NoError(t, err)
-				var resp types.QueryListExecutionsByRecipeResponse
-				error := net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp)
-				require.NoError(t, error)
-				require.NotNil(t, resp)
-				require.Equal(t, reflect.ValueOf(resp).Kind(), reflect.ValueOf(tc.obj).Kind())
-			}
-		})
+	},
+	)
+
+	// Create a recipe
+	recipeFields := []string{
+		"testRecipeName",
+		"DescriptionDescriptionDescriptionDescription",
+		"v0.0.1",
+		string(coinInputsJson),
+		"[]",
+		"{}",
+		"[]",
+		"1",
+		"true",
+		"extraInfo",
 	}
+	args = []string{cookbookID, recipeID}
+	args = append(args, recipeFields...)
+	args = append(args, common...)
+	_, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateRecipe(), args)
+
+	require.NoError(t, err)
+
+	// create execution
+	args = []string{cookbookID, recipeID,"1", "[]"} // empty list for item-ids since there is no item input
+	args = append(args, common...)
+	out, err2 := clitestutil.ExecTestCLICmd(ctx, cli.CmdExecuteRecipe(), args)
+	require.NoError(t, err2)
+	var resp sdk.TxResponse
+	require.NoError(t, ctx.JSONMarshaler.UnmarshalJSON(out.Bytes(), &resp))
+	require.Equal(t, uint32(0), resp.Code)
+
+	var executionsResponse types.QueryListExecutionsByRecipeResponse
+	args = []string{cookbookID, recipeID, "--output=json"} // empty list for item-ids since there is no item input
+	out,err = clitestutil.ExecTestCLICmd(ctx,cli.CmdListExecutionsByRecipe(),args)
+	require.NoError(t, err)
+	require.NoError(t, ctx.JSONMarshaler.UnmarshalJSON(out.Bytes(), &executionsResponse))
+
+	theExec := executionsResponse.CompletedExecutions[0]
+
+	//TODO see the blocks simulation thing (there is a delay that must be done)
+	t.Run("Test Complete Execution Early, existing cookbook", func(t *testing.T) {
+		args = []string{theExec.ID}
+		out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdCompleteExecutionEarly(), args)
+		require.NoError(t, err)
+		var resp types.MsgCompleteExecutionEarlyResponse
+		require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+	})
+
 }
