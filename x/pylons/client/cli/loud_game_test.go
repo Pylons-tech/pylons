1a89a3cfb5fd970cae01deb7d40a2765b717832b
aljo242 <amj92@pitt.edu>
2021-09-17 14:10:26 -0400

    start LOUD test

---
+++ x/pylons/client/cli/loud_game_test.go
@@ -0,0 +1,46 @@
+package cli_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/Pylons-tech/pylons/testutil/network"
+	"github.com/Pylons-tech/pylons/x/pylons/client/cli"
+)
+
+func TestLOUDBasic(t *testing.T) {
+	net := network.New(t)
+	val := net.Validators[0]
+	ctx := val.ClientCtx
+	cookbookID := "cookbookLOUD"
+	// var recipeID string
+
+	cbFields := []string{
+		"Legend of the Undead Dragon",
+		"Cookbook for running pylons recreation of LOUD",
+		"Pylons Inc",
+		"v0.0.1",
+		"alex@shmeeload.xyz",
+		"{\"denom\": \"pylons\", \"amount\": \"1\"}",
+		"true",
+	}
+
+	common := []string{
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(net.Config.BondDenom, sdk.NewInt(10))).String()),
+	}
+
+	// create cookbook
+	args := []string{cookbookID}
+	args = append(args, cbFields...)
+	args = append(args, common...)
+	_, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateCookbook(), args)
+	require.NoError(t, err)
+}
