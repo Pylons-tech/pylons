13bdf18bd50483f41ee69b6c4df9b8722148038a
NazymBerdaliyeva <nazym.berdalyeva@gmail.com>
2021-09-23 15:10:17 +0600

    setting i18n and l10n in app and creating constants

--- flutter/lib/pages/routing_page.dart
+++ flutter/lib/pages/routing_page.dart
@@ -1,5 +1,6 @@
 import 'package:cosmos_ui_components/cosmos_ui_components.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:pylons_wallet/constants/constants.dart';
 import 'package:pylons_wallet/pages/dashboard/dashboard_main.dart';
 import 'package:pylons_wallet/pages/presenting_onboard_page.dart';
@@ -26,28 +27,31 @@ class _RoutingPageState extends State<RoutingPage> {
     //TODO: Create an option for the user to change to a different wallet.
     if (store.loadWalletsFailure.value == null) {
       //Loads the last used wallet.
-        var prefs = await SharedPreferences.getInstance();
-        var currentWallet = prefs.getString("pylons:current_wallet");
-        if (currentWallet==null){
-          Navigator.of(context).push(MaterialPageRoute(builder: (_) => PresentingOnboardPage()));
-        }else{
-          PylonsApp.currentWallet = PylonsApp.walletsStore.wallets.value.firstWhere((wallet) => wallet.name==currentWallet);
-          Navigator.of(context).push(MaterialPageRoute(builder: (_) => const Dashboard()));
-        }
-
+      final prefs = await SharedPreferences.getInstance();
+      final currentWallet = prefs.getString("pylons:current_wallet");
+      if (currentWallet == null) {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (_) => PresentingOnboardPage()));
+      } else {
+        PylonsApp.currentWallet = PylonsApp.walletsStore.wallets.value
+            .firstWhere((wallet) => wallet.name == currentWallet);
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (_) => const Dashboard()));
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    var i18n = AppLocalizations.of(context);
     return Scaffold(
       backgroundColor: kBGColor,
       body: ContentStateSwitcher(
         isLoading: PylonsApp.walletsStore.areWalletsLoading.value,
         isError: PylonsApp.walletsStore.loadWalletsFailure.value != null,
-        errorChild: const CosmosErrorView(
-          title: "Something went wrong",
-          message: "We had problems retrieving wallets from secure storage.",
+        errorChild: CosmosErrorView(
+          title: i18n!.somethingWrong,
+          message: i18n.walletRetrievingErrMsg,
         ),
         contentChild: const SizedBox(),
       ),
--- flutter/lib/pylons_app.dart
+++ flutter/lib/pylons_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:pylons_wallet/pages/routing_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pylons_wallet/components/pylons_app_theme.dart';
+import 'package:pylons_wallet/pages/routing_page.dart';
 import 'package:pylons_wallet/stores/wallets_store.dart';
 import 'package:pylons_wallet/utils/base_env.dart';
 import 'package:transaction_signing_gateway/gateway/transaction_signing_gateway.dart';
@@ -18,19 +20,24 @@ class PylonsApp extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', ''),
-        Locale('ru', ''),
-      ],
-      title: 'Pylons Wallet',
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en', ''),
+          Locale('ru', ''),
+        ],
+      onGenerateTitle: (context) {
+        return AppLocalizations.of(context)!.appTitle;
+      },
       theme: PylonsAppTheme.buildAppTheme(),
-      home: const RoutingPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const RoutingPage(),
+      },
     );
   }
 }
