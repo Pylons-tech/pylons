4ab9f348f4ad2e9c8974238edeefb50a4039e07b
Rafael de Andrade Sousa <rafael.andrade@tendermint.com>
2021-09-24 16:40:11 -0400

    Keplr integration with vuejs

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -40,6 +40,61 @@ export default {
 	async created() {
 		await this.$store.dispatch('common/env/init')
 		this.initialized = true
+		if (!window.keplr) {
+			console.log("Keplr not installed")
+		}else{
+			await window.keplr.experimentalSuggestChain({
+				chainId: 'pylons',
+				chainName: 'Pylons',
+				rpc: 'http://localhost:26657',
+				rest: 'http://localhost:1317',
+				bip44: {
+					coinType: 118
+				},
+				bech32Config: {
+					bech32PrefixAccAddr: 'cosmos',
+					bech32PrefixAccPub: 'cosmos' + 'pub',
+					bech32PrefixValAddr: 'cosmos' + 'valoper',
+					bech32PrefixValPub: 'cosmos' + 'valoperpub',
+					bech32PrefixConsAddr: 'cosmos' + 'valcons',
+					bech32PrefixConsPub: 'cosmos' + 'valconspub'
+				},
+				currencies: [
+					{
+						coinDenom: 'ATOM',
+						coinMinimalDenom: 'uatom',
+						coinDecimals: 6,
+						coinGeckoId: 'cosmos'
+					},
+					{
+						coinDenom: 'BEDROCK',
+						coinMinimalDenom: 'ubedrock',
+						coinDecimals: 6,
+						coinGeckoId: 'cosmos'
+					},
+				],
+				feeCurrencies: [
+					{
+						coinDenom: 'BEDROCK',
+						coinMinimalDenom: 'ubedrock',
+						coinDecimals: 6,
+						coinGeckoId: 'cosmos'
+					},
+				],
+				stakeCurrency: {
+					coinDenom: 'BEDROCK',
+					coinMinimalDenom: 'ubedrock',
+					coinDecimals: 6,
+					coinGeckoId: 'cosmos'
+				},
+				coinType: 118,
+				gasPriceStep: {
+					low: 0.01,
+					average: 0.025,
+					high: 0.03
+				}
+			})
+		}
 	},
 	errorCaptured(err) {
 		console.log(err)
