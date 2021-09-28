0fd5d0426845a29a47c704133e0cd19f29c0d776
Rafael de Andrade Sousa <rafael.andrade@tendermint.com>
2021-09-28 16:30:55 -0400

    Bootstraping a vanilla vue project

--- vue/README.md
+++ vue/README.md
@@ -1,27 +1,24 @@
-App UI
-
-This is the [vuejs](https://vuejs.org/) user interface for your app.
-
-The vue.js framework for building your user interface helps with rapid app development. The framework provides UI components including a login and an HTML-based template.
+# vue
 
 ## Project setup
-
 ```
-npm install
+yarn install
 ```
 
 ### Compiles and hot-reloads for development
-
 ```
-npm run serve
+yarn serve
 ```
 
 ### Compiles and minifies for production
-
 ```
-npm run build
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
 ```
 
 ### Customize configuration
-
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,49 +1,26 @@
 <template>
-	<div v-if="initialized">
-		<SpWallet ref="wallet" v-on:dropdown-opened="$refs.menu.closeDropdown()" />
-		<SpLayout>
-			<template v-slot:sidebar>
-				<Sidebar />
-			</template>
-			<template v-slot:content>
-				<router-view />
-			</template>
-		</SpLayout>
-	</div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<style>
-body {
-	margin: 0;
-}
-</style>
-
 <script>
-import './scss/app.scss'
-import '@starport/vue/lib/starport-vue.css'
-import Sidebar from './components/Sidebar'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-	components: {
-		Sidebar
-	},
-	data() {
-		return {
-			initialized: false
-		}
-	},
-	computed: {
-		hasWallet() {
-			return this.$store.hasModule(['common', 'wallet'])
-		}
-	},
-	async created() {
-		await this.$store.dispatch('common/env/init')
-		this.initialized = true
-	},
-	errorCaptured(err) {
-		console.log(err)
-		return false
-	}
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
