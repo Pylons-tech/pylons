ede139c7a06196e7fb6b528a0c37337ba0534f43
Rafael de Andrade Sousa <rafael.andrade@tendermint.com>
2021-10-06 10:39:36 -0400

    Finished initial components for the vue / web / kepler wallet signing application

--- vue/src/components/Recipe.vue
+++ vue/src/components/Recipe.vue
@@ -1,7 +1,16 @@
 <!-- Based in https://cnpmjs.org/package/monaco-editor-vue3 -->
 <template>
 	<div class="card">
-		<MonacoEditor ref='editor' theme="vs" :options="options" language="javascript" v-model="code" @editorDidMount='editorDidMount'  :width="1000" :height="800" />
+		<MonacoEditor
+			ref="editor"
+			theme="vs"
+			:options="options"
+			language="javascript"
+			v-model="code"
+			@editorDidMount="editorDidMount"
+			:width="1000"
+			:height="800"
+		/>
 	</div>
 	<div class="card">
 		<Button label="Submit JSON" @click="submitJSON" />
@@ -19,24 +28,32 @@ export default {
 		Button
 	},
 	methods: {
-		editorDidMount(editor){
-			editor.onDidChangeModelContent( e=> {
+		editorDidMount(editor) {
+			editor.onDidChangeModelContent((e) => {
 				this.setNewValue(editor.getValue())
 			})
 		},
-		setNewValue(value){
+		setNewValue(value) {
 			this.code = value
 		},
-		submitJSON() {
+		async submitJSON() {
 			const recipeObj = Recipe.fromJSON(JSON.parse(this.code))
 			console.log(recipeObj)
 			recipeObj.creator = this.$store.getters['common/wallet/address']
-			this.$store.dispatch('Pylonstech.pylons.pylons/sendMsgCreateRecipe', {
+			await this.$store.dispatch('Pylonstech.pylons.pylons/sendMsgCreateRecipe', {
 				value: recipeObj,
 				fee: [],
 				memo: ''
 			})
-		},
+			console.log(
+				(await this.$store.dispatch('Pylonstech.pylons.pylons/QueryListRecipesByCookbook', {
+					params:{
+						CookbookID : recipeObj.cookbookID
+					}
+				})).Recipes
+			)
+			this.$store.state.ListExecutionsByRecipe
+		}
 	},
 	data() {
 		return {
