ed1b3fc937a6da44c67b75d96ee9a4c67b9ed9b2
aljo242 <alex.johnson@tendermint.com>
2021-12-08 13:26:23 -0500

    clean up

--- x/pylons/types/recipe_item_params.go
+++ x/pylons/types/recipe_item_params.go
@@ -147,42 +147,7 @@ func (dp DoubleInputParam) Has(input sdk.Dec) bool {
 	return input.GTE(dp.MinValue) && input.LTE(dp.MaxValue)
 }
 
-// Actualize creates a (key, value) list from ParamList
-func (dpm DoubleInputParamList) Actualize() []DoubleKeyValue {
-	m := make([]DoubleKeyValue, len(dpm))
-	for i, param := range dpm {
-		m[i] = DoubleKeyValue{
-			Key:   param.Key,
-			Value: param.MinValue.Add(param.MaxValue).QuoInt64(2),
-		}
-	}
-	return m
-}
-
 // Has validate if input is between min max range
 func (lp LongInputParam) Has(input int) bool {
 	return int64(input) >= lp.MinValue && int64(input) <= lp.MaxValue
-}
-
-// Actualize generate a value from range
-func (lpm LongInputParamList) Actualize() []LongKeyValue {
-	// We don't have the ability to do random numbers in a verifiable way rn, so don't worry about it
-	m := make([]LongKeyValue, len(lpm))
-	for i, param := range lpm {
-		m[i] = LongKeyValue{
-			Key:   param.Key,
-			Value: (param.MinValue + param.MaxValue) / 2,
-		}
-	}
-	return m
-}
-
-// Actualize actualize string from StringInputParamList
-func (lpm StringInputParamList) Actualize() []StringKeyValue {
-	// We don't have the ability to do random numbers in a verifiable way rn, so don't worry about it
-	m := make([]StringKeyValue, len(lpm))
-	for i, param := range lpm {
-		m[i] = StringKeyValue(param)
-	}
-	return m
 }
