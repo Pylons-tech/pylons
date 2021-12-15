8ae477c24862dfa26d85931b5087617819a223d6
Rafael de Andrade Sousa <rafael.andrade@tendermint.com>
2021-12-15 11:56:45 -0500

    Automate the min field size requirements

--- x/pylons/client/cli/tx_cookbook.go
+++ x/pylons/client/cli/tx_cookbook.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"fmt"
+
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/spf13/cobra"
@@ -15,15 +17,13 @@ import (
 )
 
 func CmdCreateCookbook() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "create-cookbook [id] [name] [description] [developer] [version] [support-email] [enabled]",
-		Short: "create new cookbook",
-		Long: `
+
+	longText := fmt.Sprintf(`
 Create a new cookbook using the following arguments :
 
 * id : a unique identifier to your cookbook. Only letters, numbers and underscore (should not be the first character) allowed 
-* name: a human readable name for your cookbook, with a minimum of 3 characters size 
-* description : A more detailed description of your cookbook. Minimum 8 chars long
+* name: a human readable name for your cookbook, with a minimum of %d characters long 
+* description : A more detailed description of your cookbook. Minimum %d chars long
 * developer : name of the developer
 * version : the version of the cookbook in semVer format, ex.: v0.0.0
 * support-email : a valid email
@@ -31,7 +31,11 @@ Create a new cookbook using the following arguments :
 
 Note that the --from flag is mandatory, as indicates the key to be used to sign the transaction. 
 
-		`,
+		`, types.DefaultMinFieldLength, types.DefaultMinFieldLength)
+	cmd := &cobra.Command{
+		Use:   "create-cookbook [id] [name] [description] [developer] [version] [support-email] [enabled]",
+		Short: "create new cookbook",
+		Long:  longText,
 		Example: `
 pylonsd tx pylons create-cookbook "loud123456" "Legend of the Undead Dragon" "Cookbook for running pylons recreation of LOUD" "Pylons Inc" v0.3.1 test@pylons.com true --from joe
 			`,
