47d75c719b2833f39d4deec23a836817422b5c12
Justyn Temme <justyn@Justyns-MacBook-Air.local>
2021-12-28 23:27:50 -0600

    run pylonsd node instead of starport

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,4 +3,4 @@ export privkey=dklo39sidlw
 cd /app
 mv /config/* ~/.pylons/config/
 (echo 12345678; echo 12345678) | pylonsd keys import validator_1 /vault/secrets/validator_1/privkey
-starport chain serve
+pylonsd start
