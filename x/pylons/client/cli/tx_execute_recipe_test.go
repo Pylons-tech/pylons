1437de7252bc36f07bb42781f7d62cb2355f1c28
blackpanthar <conwuliri@gmail.com>
2022-01-26 20:45:12 +0100

    Add TestExecuteRecipeItemInputOutput Test

--- x/pylons/client/cli/tx_execute_recipe_test.go
+++ x/pylons/client/cli/tx_execute_recipe_test.go
@@ -654,6 +654,201 @@ func TestExecuteDisableRecipe(t *testing.T) {
 	require.Equal(t, sdkerrors.ErrInvalidRequest.ABCICode(), resp.Code)
 }
 
+func TestExecuteRecipeItemInputOutput(t *testing.T) {
+	net := network.New(t)
+	val := net.Validators[0]
+	ctx := val.ClientCtx
+	cookbookID := "testCookbookID"
+	recipeID := "testRecipeID"
+	recipeID2 := "testRecipeID2"
+
+	cbFields := []string{
+		"testCookbookName",
+		"DescriptionDescriptionDescription",
+		"Developer",
+		"v0.0.1",
+		"test@email.com",
+		"true",
+	}
+	itemInputs, err := json.Marshal([]types.ItemInput{
+		{
+			ID: "itemInputID",
+			Doubles: []types.DoubleInputParam{
+				{
+					Key:  "main",
+					MinValue: sdk.NewDec(50),
+					MaxValue: sdk.NewDec(100),
+				},
+			},
+			Longs: nil,
+			Strings: []types.StringInputParam{
+				{
+					Key:     "testInput",
+					Value:   "testVal",
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+
+	//itemInputIDs, err := json.Marshal([]string{types.EncodeItemID(0)})
+
+	entries, err := json.Marshal(types.EntriesList{
+		CoinOutputs: nil,
+		ItemOutputs: []types.ItemOutput{
+			{
+				ID: "testID" ,
+				Doubles: []types.DoubleParam{
+					{
+						Key:  "Mass",
+						WeightRanges: []types.DoubleWeightRange{
+							{
+								Lower:  sdk.NewDec(50),
+								Upper:  sdk.NewDec(100),
+								Weight: 1,
+							},
+						},
+						Program: "",
+					},
+				},
+				Longs: nil,
+				Strings: []types.StringParam{
+					{
+						Key:     "testKey",
+						Value:   "testValue",
+						Program: "",
+					},
+				},
+				MutableStrings: nil,
+				TransferFee:    []sdk.Coin{sdk.NewCoin("pylons", sdk.OneInt())},
+				TradePercentage: sdk.SmallestDec(),
+				Quantity:       0,
+				AmountMinted:   0,
+			},
+		},
+		ItemModifyOutputs: nil,
+	})
+	require.NoError(t, err)
+
+	itemOutputs, err := json.Marshal([]types.WeightedOutputs{
+		{
+			EntryIDs: []string{"testID"},
+			Weight:   1,
+		},
+	})
+	require.NoError(t, err)
+
+	// test json unmarshalling of strings
+	argsEntries, err := cast.ToStringE(string(entries))
+	require.NoError(t, err)
+	jsonArgsEntries := types.EntriesList{}
+	err = json.Unmarshal([]byte(argsEntries), &jsonArgsEntries)
+	require.NoError(t, err)
+	argsOutputs, err := cast.ToStringE(string(itemOutputs))
+	require.NoError(t, err)
+	jsonArgsOutputs := make([]types.WeightedOutputs, 0)
+	err = json.Unmarshal([]byte(argsOutputs), &jsonArgsOutputs)
+	require.NoError(t, err)
+
+	recipeFields := []string{
+		"testRecipeName",
+		"DescriptionDescriptionDescriptionDescription",
+		"v0.0.1",
+		"[]",
+		"[]",
+		string(entries),
+		string(itemOutputs),
+		"0",
+		"{\"denom\": \"upylon\", \"amount\": \"1\"}",
+		"true",
+		"extraInfo",
+	}
+
+
+	recipeFields2 := []string{
+		"testRecipeName",
+		"DescriptionDescriptionDescriptionDescription",
+		"v0.0.1",
+		"[]",
+		string(itemInputs),
+		string(entries),
+		string(itemOutputs),
+		"0",
+		"{\"denom\": \"upylon\", \"amount\": \"1\"}",
+		"true",
+		"extraInfo",
+	}
+
+	common := []string{
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(net.Config.BondDenom, sdk.NewInt(10))).String()),
+	}
+
+	// create cookbook
+	args := []string{cookbookID}
+	args = append(args, cbFields...)
+	args = append(args, common...)
+	_, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateCookbook(), args)
+	require.NoError(t, err)
+
+	// create recipe1
+	args = []string{cookbookID, recipeID}
+	args = append(args, recipeFields...)
+	args = append(args, common...)
+	_, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateRecipe(), args)
+	require.NoError(t, err)
+
+	// create recipe2
+	args = []string{cookbookID, recipeID2}
+	args = append(args, recipeFields2...)
+	args = append(args, common...)
+	_, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateRecipe(), args)
+	require.NoError(t, err)
+
+
+	// create execution
+
+	args = []string{cookbookID, recipeID, "0", "[]", "[]"} // empty list for item-ids since there is no item input
+	args = append(args, common...)
+	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdExecuteRecipe(), args)
+	require.NoError(t, err)
+	var resp sdk.TxResponse
+	require.NoError(t, ctx.Codec.UnmarshalJSON(out.Bytes(), &resp))
+	require.Equal(t, uint32(0), resp.Code)
+
+	// simulate waiting for later block heights
+	height, err := net.LatestHeight()
+	targetHeight := height + 1
+	// build execID from the execution height
+	execID := strconv.Itoa(int(height+0)) + "-" + strconv.Itoa(0)
+	require.NoError(t, err)
+	_, err = net.WaitForHeightWithTimeout(targetHeight, 60*time.Second)
+	require.NoError(t, err)
+
+	// check the execution
+	args = []string{execID}
+	out, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdShowExecution(), args)
+	require.NoError(t, err)
+	var execResp types.QueryGetExecutionResponse
+	require.NoError(t, ctx.Codec.UnmarshalJSON(out.Bytes(), &execResp))
+	// verify completed
+	require.Equal(t, true, execResp.Completed)
+
+	// check the item, itemID is i
+	itemID := types.EncodeItemID(uint64(0))
+	args = []string{cookbookID, itemID}
+	out, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdShowItem(), args)
+	require.NoError(t, err)
+	var itemResp types.QueryGetItemResponse
+	require.NoError(t, ctx.Codec.UnmarshalJSON(out.Bytes(), &itemResp))
+	require.Equal(t, cookbookID, itemResp.Item.CookbookID)
+	require.Equal(t, height, itemResp.Item.LastUpdate)
+
+
+}
+
 func TestExecuteRecipeMutableStringField(t *testing.T) {
 	net := network.New(t)
 	val := net.Validators[0]
