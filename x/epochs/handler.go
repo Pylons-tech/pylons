836b873e147e33e96f1a91f3160460b97c9965b7
vuong177 <nguyenvuong1122000@gmail.com>
2022-06-16 19:33:51 +0700

    remove swith case in epoch handler

--- x/epochs/handler.go
+++ x/epochs/handler.go
@@ -13,10 +13,7 @@ import (
 // NewHandler returns a handler for epochs module messages.
 func NewHandler(k keeper.Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
-		switch msg := msg.(type) {
-		default:
-			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
-			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
-		}
+		errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
 	}
 }
