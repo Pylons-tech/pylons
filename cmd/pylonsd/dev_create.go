099d78101589bd69d53fd7d620d0d7ac6f0f369c
sine/cosine/tangent <zfshmn@gmail.com>
2022-06-21 15:27:26 -0700

    melting, sorry

--- cmd/pylonsd/dev_create.go
+++ cmd/pylonsd/dev_create.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/Pylons-tech/pylons/x/pylons/types"
+)
+
+func CmdDevCreate() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create [account] [path]",
+		Short: "Creates and executes creation transactions Pylons recipe or cookbook files in the provided path, using credentials of provided account",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			account := args[0]
+			path := args[1]
+			ForFiles(path, perCookbook_Create, perRecipe_Create) // this can't work as-is, we need to use account credentials to generate anonymous functions
+		},
+	}
+	return cmd
+}
+
+func perCookbook_Create(path string, _ types.Cookbook) {
+	fmt.Fprintln(Out, path, "is a valid cookbook")
+}
+
+func perRecipe_Create(path string, _ types.Recipe) {
+	fmt.Fprintln(Out, path, "is a valid recipe")
+}
