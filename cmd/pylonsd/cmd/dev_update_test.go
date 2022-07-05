d0e93995fa17ae55a8419aaaa4b613f284dc2ec0
sine/cosine/tangent <zfshmn@gmail.com>
2022-07-05 15:18:34 -0700

    There's no way this should work but I need to figure out how to set up the recipes in advance

--- cmd/pylonsd/cmd/dev_update_test.go
+++ cmd/pylonsd/cmd/dev_update_test.go
@@ -1,1 +1,39 @@
-package cmd_test
+package cmd
+
+import (
+	"testing"
+
+	util "github.com/Pylons-tech/pylons/testutil/cli"
+	"github.com/Pylons-tech/pylons/testutil/network"
+	"github.com/Pylons-tech/pylons/x/pylons/client/cli"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdate(t *testing.T) {
+	preTestValidate(t)
+
+	net := network.New(t)
+	val := net.Validators[0]
+	ctx := val.ClientCtx
+	cli.ForceSkipConfirm = true
+
+	// we don't need to test the bad cb/rcp cases b/c they fail validation
+
+	t.Run("Cookbook", func(t *testing.T) {
+		_, err := util.GenerateAddressWithAccount(ctx, t, net)
+		require.NoError(t, err)
+		args := []string{"NewUser0", goodPLC}
+		cmd := DevUpdate()
+		_, err = clitestutil.ExecTestCLICmd(ctx, cmd, args)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Recipe", func(t *testing.T) {
+		args := []string{"NewUser0", goodPLR}
+		cmd := DevUpdate()
+		_, err := clitestutil.ExecTestCLICmd(ctx, cmd, args)
+		assert.Nil(t, err)
+	})
+}
