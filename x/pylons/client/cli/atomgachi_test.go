31159cb47c52c352bebaeadcbead1f235c2d345b
Jacob Gadikian <jacobgadikian@gmail.com>
2022-07-14 06:27:46 +0700

    restore a few

---
+++ x/pylons/client/cli/atomgachi_test.go
@@ -0,0 +1,180 @@
+package cli_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/Pylons-tech/pylons/app"
+	util "github.com/Pylons-tech/pylons/testutil/cli"
+	"github.com/Pylons-tech/pylons/testutil/network"
+	"github.com/Pylons-tech/pylons/x/pylons/client/cli"
+	"github.com/Pylons-tech/pylons/x/pylons/types"
+)
+
+const (
+	cookbookIDAtomgachi = "atomgachiV2"
+)
+
+type atomgachiBasicSim struct {
+	net                  *network.Network
+	ctx                  client.Context
+	basicTradePercentage sdk.Dec
+	err                  error
+	common               []string
+	commonExec           []string
+	execCount            int
+	itemCount            int
+	mintRecipeID         string
+	mintRecipeID2        string
+}
+
+func TestAtomgachiBasic(t *testing.T) {
+	config := app.DefaultConfig()
+	net := network.New(t, config)
+	val := net.Validators[0]
+	ctx := val.ClientCtx
+	var err error
+	address, err := util.GenerateAddressWithAccount(ctx, t, net)
+	require.NoError(t, err)
+
+	simInfo := &atomgachiBasicSim{
+		net:                  net,
+		ctx:                  ctx,
+		basicTradePercentage: sdk.Dec{},
+		err:                  nil,
+		common:               nil,
+	}
+
+	// checks if the integer has been made correctly from the input string
+	simInfo.basicTradePercentage, err = sdk.NewDecFromStr("0.10")
+	require.NoError(t, err)
+
+	simInfo.common = util.CommonArgs(val.Address.String(), net)
+	simInfo.commonExec = util.CommonArgs(address, net)
+
+	createAtomgachiCookbook(t, simInfo)
+	createMintRecipe(t, simInfo)
+	mint(t, simInfo)
+}
+
+func createAtomgachiCookbook(t *testing.T, simInfo *atomgachiBasicSim) {
+	cbFields := []string{
+		"Atomgachi Test Cookbook",
+		"Cookbook for testing demo atomgachi transactions",
+		"Pylons Inc",
+		"v0.0.1",
+		"alex@shmeeload.xyz",
+		"true",
+	}
+
+	// create Atomgachi cookbook
+	args := []string{cookbookIDAtomgachi}
+	args = append(args, cbFields...)
+	args = append(args, simInfo.common...)
+	_, err := clitestutil.ExecTestCLICmd(simInfo.ctx, cli.CmdCreateCookbook(), args)
+	require.NoError(t, err)
+}
+
+func createMintRecipe(t *testing.T, simInfo *atomgachiBasicSim) {
+	coinInputs, err := json.Marshal([]types.CoinInput{})
+	require.NoError(t, err)
+
+	entries, err := json.Marshal(types.EntriesList{
+		CoinOutputs: nil,
+		ItemOutputs: []types.ItemOutput{{
+			Id: "atomgachiTestCreate",
+			Strings: []types.StringParam{
+				{
+					Key:     "name",
+					Value:   "testObject",
+					Program: "",
+				},
+			},
+			MutableStrings:  nil,
+			TransferFee:     nil,
+			TradePercentage: simInfo.basicTradePercentage,
+			Tradeable:       true,
+		}},
+		ItemModifyOutputs: nil,
+	})
+	require.NoError(t, err)
+
+	itemOutputs, err := json.Marshal([]types.WeightedOutputs{
+		{
+			EntryIds: []string{"atomgachiTestCreate"},
+			Weight:   1,
+		},
+	})
+
+	// Get Character Recipe
+	simInfo.mintRecipeID = "atomgachiTestCreate"
+	mintNFTRecipe := []string{
+		"Atom with it's Number here",
+		"mijollae is testing the demo process",
+		"v0.0.1",
+		string(coinInputs),
+		"[]",
+		string(entries),
+		string(itemOutputs),
+		"0",
+		"{\"denom\": \"upylon\", \"amount\": \"12\"}",
+		"true",
+		"extraInfo",
+	}
+
+	// create recipe
+	args := []string{cookbookIDAtomgachi, simInfo.mintRecipeID}
+	args = append(args, mintNFTRecipe...)
+	fmt.Println(args)
+	args = append(args, simInfo.common...)
+	_, err = clitestutil.ExecTestCLICmd(simInfo.ctx, cli.CmdCreateRecipe(), args)
+	require.NoError(t, err)
+}
+
+func mint(t *testing.T, simInfo *atomgachiBasicSim) {
+	// execute recipe to mint
+	args := []string{cookbookIDAtomgachi, simInfo.mintRecipeID, "0", "[]", "[]"} // empty list for item-ids since there is no item input
+	args = append(args, simInfo.commonExec...)
+	out, err := clitestutil.ExecTestCLICmd(simInfo.ctx, cli.CmdExecuteRecipe(), args)
+	require.NoError(t, err)
+	var resp sdk.TxResponse
+	require.NoError(t, simInfo.ctx.Codec.UnmarshalJSON(out.Bytes(), &resp))
+	require.Equal(t, uint32(0), resp.Code)
+
+	// simulate waiting for later block heights
+	height, err := simInfo.net.LatestHeight()
+	targetHeight := height + 1
+	// build execID from the execution height
+	execID := strconv.Itoa(int(height+0)) + "-" + strconv.Itoa(simInfo.execCount)
+	simInfo.execCount++
+	require.NoError(t, err)
+	_, err = simInfo.net.WaitForHeightWithTimeout(targetHeight, 60*time.Second)
+	require.NoError(t, err)
+
+	// check the execution
+	args = []string{execID}
+	out, err = clitestutil.ExecTestCLICmd(simInfo.ctx, cli.CmdShowExecution(), args)
+	require.NoError(t, err)
+	var execResp types.QueryGetExecutionResponse
+	require.NoError(t, simInfo.ctx.Codec.UnmarshalJSON(out.Bytes(), &execResp))
+	// verify completed
+	require.Equal(t, true, execResp.Completed)
+
+	// check the item, itemID is i
+	nftID := types.EncodeItemID(uint64(simInfo.itemCount))
+	simInfo.itemCount++
+	args = []string{cookbookIDAtomgachi, nftID}
+	out, err = clitestutil.ExecTestCLICmd(simInfo.ctx, cli.CmdShowItem(), args)
+	require.NoError(t, err)
+	var itemResp types.QueryGetItemResponse
+	require.NoError(t, simInfo.ctx.Codec.UnmarshalJSON(out.Bytes(), &itemResp))
+	require.Equal(t, cookbookIDAtomgachi, itemResp.Item.CookbookId)
+}
---
+++ x/pylons/client/cli/query_get_recipe_history_test.go
@@ -0,0 +1,186 @@
+package cli_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"cosmossdk.io/math"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/spf13/cast"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	util "github.com/Pylons-tech/pylons/testutil/cli"
+	"github.com/Pylons-tech/pylons/x/pylons/client/cli"
+	"github.com/Pylons-tech/pylons/x/pylons/types"
+)
+
+func TestGetRecipeHistory(t *testing.T) {
+	net, objs, _ := util.NetworkWithRecipeObjectsHistory(t, 2)
+	val := net.Validators[0]
+	ctx := net.Validators[0].ClientCtx
+	address, err := util.GenerateAddressWithAccount(ctx, t, net)
+	require.NoError(t, err)
+	common := util.CommonArgs(address, net)
+	tc := []struct {
+		desc       string
+		cookbookID string
+		recipeID   string
+		args       []string
+		err        error
+		obj        []types.RecipeHistory
+	}{
+		{
+			desc:       "not found",
+			cookbookID: "not found",
+			recipeID:   "not found",
+			err:        status.Error(codes.NotFound, "not found"),
+			obj:        []types.RecipeHistory{},
+		},
+		{
+			desc:       "found",
+			cookbookID: objs[0].CookbookId,
+			recipeID:   objs[0].Id,
+			args:       common,
+			obj: []types.RecipeHistory{
+				{
+					CookbookId: objs[0].CookbookId,
+					RecipeId:   objs[0].Id,
+					Sender:     address,
+					SenderName: "user",
+					Receiver:   net.Validators[0].Address.String(),
+					Amount:     "",
+				},
+			},
+		},
+	}
+	args := []string{tc[0].cookbookID, tc[0].recipeID}
+	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdGetRecipeHistory(), args)
+	if tc[0].err != nil {
+		stat, ok := status.FromError(tc[0].err)
+		require.True(t, ok)
+		require.ErrorIs(t, stat.Err(), tc[0].err)
+	} else {
+		require.NoError(t, err)
+		var resp types.QueryGetRecipeHistoryResponse
+		require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+		require.NotNil(t, resp.History)
+		require.Equal(t, len(tc[0].obj), len(resp.History))
+	}
+	cbFields := []string{
+		"testCookbookName",
+		"Cookbook for testing demo easel transactions",
+		"Pylons Inc",
+		"v0.0.1",
+		"alex@shmeeload.xyz",
+		"true",
+	}
+
+	// create a cookbook
+	args = []string{tc[1].cookbookID}
+	args = append(args, cbFields...)
+	args = append(args, util.CommonArgs(val.Address.String(), net)...)
+	_, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateCookbook(), args)
+	require.NoError(t, err)
+	// create a recipe
+	tradePercentage, err := sdk.NewDecFromStr("0.01")
+	require.NoError(t, err)
+	entries, err := json.Marshal(types.EntriesList{
+		CoinOutputs: nil,
+		ItemOutputs: []types.ItemOutput{
+			{
+				Id: "testID",
+				Doubles: []types.DoubleParam{
+					{
+						Key: "Mass",
+						WeightRanges: []types.DoubleWeightRange{
+							{
+								Lower:  sdk.NewDec(50),
+								Upper:  sdk.NewDec(100),
+								Weight: 1,
+							},
+						},
+						Program: "",
+					},
+				},
+				Longs: nil,
+				Strings: []types.StringParam{
+					{
+						Key:     "testKey",
+						Value:   "testValue",
+						Program: "",
+					},
+				},
+				MutableStrings:  nil,
+				TransferFee:     []sdk.Coin{sdk.NewCoin("upylon", math.OneInt())},
+				TradePercentage: tradePercentage,
+				Quantity:        0,
+				AmountMinted:    0,
+			},
+		},
+		ItemModifyOutputs: nil,
+	})
+	require.NoError(t, err)
+
+	itemOutputs, err := json.Marshal([]types.WeightedOutputs{
+		{
+			EntryIds: []string{"testID"},
+			Weight:   1,
+		},
+	})
+	require.NoError(t, err)
+
+	// test json unmarshalling of strings
+	argsEntries, err := cast.ToStringE(string(entries))
+	require.NoError(t, err)
+	jsonArgsEntries := types.EntriesList{}
+	err = json.Unmarshal([]byte(argsEntries), &jsonArgsEntries)
+	require.NoError(t, err)
+	argsOutputs, err := cast.ToStringE(string(itemOutputs))
+	require.NoError(t, err)
+	jsonArgsOutputs := make([]types.WeightedOutputs, 0)
+	err = json.Unmarshal([]byte(argsOutputs), &jsonArgsOutputs)
+	require.NoError(t, err)
+	recipeFields := []string{
+		"testRecipeName",
+		"DescriptionDescriptionDescriptionDescription",
+		"v0.0.1",
+		"[]",
+		"[]",
+		string(entries),
+		string(itemOutputs),
+		"0",
+		"{\"denom\": \"upylon\", \"amount\": \"1\"}",
+		"true",
+		"extraInfo",
+	}
+	args = []string{tc[1].cookbookID, tc[1].recipeID}
+	args = append(args, recipeFields...)
+	args = append(args, util.CommonArgs(val.Address.String(), net)...)
+	_, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateRecipe(), args)
+	require.NoError(t, err)
+
+	args = []string{tc[1].cookbookID, tc[1].recipeID, "0", "[]", "[]"} // empty list for item-ids since there is no item input
+	args = append(args, tc[1].args...)
+	out, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdExecuteRecipe(), args)
+	require.NoError(t, err)
+	var resp sdk.TxResponse
+	require.NoError(t, ctx.Codec.UnmarshalJSON(out.Bytes(), &resp))
+	require.Equal(t, uint32(0), resp.Code)
+	args = []string{tc[1].cookbookID, tc[1].recipeID}
+	out, err = clitestutil.ExecTestCLICmd(ctx, cli.CmdGetRecipeHistory(), args)
+	if tc[0].err != nil {
+		stat, ok := status.FromError(tc[1].err)
+		require.True(t, ok)
+		require.ErrorIs(t, stat.Err(), tc[1].err)
+	} else {
+		require.NoError(t, err)
+		var resp types.QueryGetRecipeHistoryResponse
+		require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+		require.NotNil(t, resp.History)
+		require.Equal(t, len(tc[0].obj), len(resp.History))
+		require.Equal(t, tc[1].obj[0].Sender, resp.History[0].Sender)
+	}
+}
