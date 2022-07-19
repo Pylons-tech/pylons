02891391401351f15ab698e42e583f568ad35486
vuong <nguyenvuong1122000@gmail.com>
2022-07-19 20:18:45 +0700

    fix testnet

--- cmd/pylonsd/cmd/testnet.go
+++ cmd/pylonsd/cmd/testnet.go
@@ -480,7 +480,7 @@ func writeFile(name string, dir string, contents []byte) error {
 
 // startTestnet starts an in-process testnet
 func startTestnet(cmd *cobra.Command, args startArgs) error {
-	networkConfig := network.DefaultConfig()
+	networkConfig := pylonsConfig.DefaultConfig()
 
 	// Default networkConfig.ChainID is random, and we should only override it if chainID provided
 	// is non-empty
