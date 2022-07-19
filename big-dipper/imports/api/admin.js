21d69083fd6b73e63bce26b5d98c81d96eb6fd06
aliirns <sli.imran@rnssol.com>
2022-07-19 18:02:17 +0500

    firebase.json is now placed with other config files in Big-Dipper Folder

--- big-dipper/imports/api/admin.js
+++ big-dipper/imports/api/admin.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase.json");
+var serviceAccount = require("../../firebase.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
