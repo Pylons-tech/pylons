3f2ecd985a0a2affd556e54ea63c82f7abb89088
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2022-07-27 01:46:43 +0700

    add migrate scripts for iap_order

---
+++ x/pylons/migrations/v046/convert.go
@@ -0,0 +1,22 @@
+package v046
+
+import (
+
+	v1 "github.com/Pylons-tech/pylons/x/pylons/types/v1"
+	"github.com/Pylons-tech/pylons/x/pylons/types"
+)
+
+// ConvertToLegacyProposal takes a new proposal and attempts to convert it to the
+// legacy proposal format. This conversion is best effort. New proposal types that
+// don't have a legacy message will return a "nil" content
+
+func convertToNewAppleInAppPurchaseOrder(oldProp v1.OldAppleInAppPurchaseOrder) (types.AppleInAppPurchaseOrder, error) {
+
+	return types.AppleInAppPurchaseOrder{
+		Quantity:       oldProp.Quantity,
+		ProductId: oldProp.ProductID,
+		PurchaseId:   oldProp.PurchaseID,
+		PurchaseDate: oldProp.PurchaseDate,
+		Creator:      oldProp.Creator,
+	}, nil
+}
