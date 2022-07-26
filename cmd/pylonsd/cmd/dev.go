787837d7c7c7353e9abf4f9de1b96743e1706fe1
sine/cosine/tangent <zfshmn@gmail.com>
2022-07-26 14:30:33 -0700

    Needs tests, but this is the entire gadget system core in some form at least

--- cmd/pylonsd/cmd/dev.go
+++ cmd/pylonsd/cmd/dev.go
@@ -25,15 +25,17 @@ const (
 )
 
 func forFile(path string, perCookbook func(path string, cookbook types.Cookbook), perRecipe func(path string, recipe types.Recipe)) {
+	// this is slow, we should avoid reloading gadgets w/ every file when we can do some rewriting
+	gadgets := LoadGadgetsForPath(path)
 	if filepath.Ext(path) == cookbookExtension {
-		cb, _, err := loadCookbookFromPath(path)
+		cb, _, err := loadCookbookFromPath(path, gadgets)
 		if err != nil {
 			fmt.Fprintln(Out, "File ", path, " is not a cookbook - parsing error:\n", err)
 		} else {
 			perCookbook(path, cb)
 		}
 	} else if filepath.Ext(path) == recipeExtension {
-		rcp, _, err := loadRecipeFromPath(path)
+		rcp, _, err := loadRecipeFromPath(path, gadgets)
 		if err != nil {
 			fmt.Fprintln(Out, "File ", path, " is not a recipe - parsing error:\n", err)
 		} else {
@@ -74,36 +76,43 @@ func loadModuleFromPath(modulePath string, currentPath string) string {
 	return string(bytes)
 }
 
-func loadModuleInline(bytes []byte, path string, info os.FileInfo) string {
+func loadModulesInline(bytes []byte, path string, info os.FileInfo, gadgets *[]Gadget) string {
 	json := string(bytes)
 	lines := strings.Split(json, "\n")
 	for i, line := range lines {
-		if strings.Contains(line, includeDirective) {
-			modulePath := strings.TrimSpace(strings.Split(line, includeDirective)[1])
-			lines[i] = loadModuleFromPath(modulePath, strings.TrimSuffix(path, info.Name())) + "\n"
+		lineTrimmed := strings.TrimLeft(line, " ")
+		if lineTrimmed[0] == '#' {
+			if strings.Contains(line, includeDirective) {
+				modulePath := strings.TrimSpace(strings.Split(line, includeDirective)[1])
+				lines[i] = loadModuleFromPath(modulePath, strings.TrimSuffix(path, info.Name())) + "\n"
+			} else {
+				splut := strings.Split(strings.TrimPrefix(lineTrimmed, "#"), " ")
+				gadget := GetGadget(strings.TrimSpace(splut[0]), gadgets)
+				lines[i] = ExpandGadget(gadget, splut[1:])
+			}
 		}
 	}
 	json = strings.Join(lines, "")
 	return json
 }
 
-func loadCookbookFromPath(path string) (types.Cookbook, string, error) {
+func loadCookbookFromPath(path string, gadgets *[]Gadget) (types.Cookbook, string, error) {
 	bytes, _ := os.ReadFile(path)
 	info, _ := os.Stat(path)
 	var cb types.Cookbook
 
-	json := loadModuleInline(bytes, path, info)
+	json := loadModulesInline(bytes, path, info, gadgets)
 	err := jsonpb.UnmarshalString(json, &cb)
 
 	return cb, json, err
 }
 
-func loadRecipeFromPath(path string) (types.Recipe, string, error) {
+func loadRecipeFromPath(path string, gadgets *[]Gadget) (types.Recipe, string, error) {
 	bytes, _ := os.ReadFile(path)
 	info, _ := os.Stat(path)
 	var rcp types.Recipe
 
-	json := loadModuleInline(bytes, path, info)
+	json := loadModulesInline(bytes, path, info, gadgets)
 	err := jsonpb.UnmarshalString(json, &rcp)
 	return rcp, string(bytes), err
 }
--- cmd/pylonsd/cmd/gadgets.go
+++ cmd/pylonsd/cmd/gadgets.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 )
 
 type Gadget struct {
@@ -20,8 +22,8 @@ var builtinGadgets []Gadget = []Gadget{
 var reservedNames = []string{"include"}
 
 // one iteration
-func loadGadgetsForPath(d string, gadgets *[]Gadget) (string, string, *[]Gadget) {
-	_, err := os.Stat(path.Join(d, gadgetsFilename))
+func loadGadgetsForPath(p string, gadgets *[]Gadget) (string, string, *[]Gadget) {
+	_, err := os.Stat(path.Join(p, gadgetsFilename))
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return "", "", nil
@@ -29,7 +31,7 @@ func loadGadgetsForPath(d string, gadgets *[]Gadget) (string, string, *[]Gadget)
 			panic(err)
 		}
 	} else {
-		bytes, err := os.ReadFile(d)
+		bytes, err := os.ReadFile(p)
 		if err != nil {
 			panic(err)
 		}
@@ -37,42 +39,105 @@ func loadGadgetsForPath(d string, gadgets *[]Gadget) (string, string, *[]Gadget)
 		g := append(parse, *gadgets...)
 		gadgets = &g
 	}
-	dir, file := path.Split(d)
+	dir, file := path.Split(p)
 	return dir, file, gadgets
 }
 
+func parseGadget(header string, json string, gadgets *[]Gadget) *Gadget {
+	splut := strings.Split(strings.TrimPrefix(header, "#"), " ")
+	if len(splut) != 2 {
+		panic(errors.New("Not a valid gadget header: \n" + header))
+	}
+	gadgetName := splut[0]
+	if GetGadget(gadgetName, gadgets) != nil {
+		panic(errors.New("Duplicate gadget name: " + gadgetName))
+	}
+
+	// we will never have enough reserved names to warrant a real search algorithm here
+	for _, s := range reservedNames {
+		if s == gadgetName {
+			panic("Can't register a gadget of reserved name " + gadgetName)
+		}
+	}
+
+	gadgetArgs, err := strconv.Atoi(splut[1])
+	if err != nil {
+		panic(err)
+	}
+	// todo: we should actually validate the json!
+	return &Gadget{name: gadgetName, json: json, parametersCount: gadgetArgs}
+}
+
 func parseGadgets(s string) []Gadget {
-	// todo: what does a gadget file actually look like?
-	// probably header, json, header, json
-	//
-	// #gadget 42
-	// {
-	//	"some_json": "true".
-	//	"some_more_json": "false"
-	// }
-	//
-	// #different_gadget 1
-	// {
-	//	"other_json": "true".
-	//	"some_other_json": "false"
-	// }
+	gadgets := []Gadget{}
+	const winNewline = "\r\n"
+	const normalNewline = "\n"
+	var nl = normalNewline
+	if strings.Contains(s, winNewline) {
+		nl = winNewline
+	}
+	splut := strings.Split(s, nl)
+	if splut[0][0] != '#' {
+		panic(errors.New("pylons.gadgets file does not start with a valid gadget header")) // todo: this should specify which file, but that can wait
+	}
+	gadgetHeader := ""
+	gadgetJson := ""
+	for i, s := range splut {
+		if s[0] == '#' {
+			// this line is a header, so parse out the gadget we've built. unless this is the first gadget.
+			if i != 0 {
+				gadgets = append(gadgets, *parseGadget(gadgetHeader, gadgetJson, &gadgets))
+			}
+			gadgetHeader = s
+			gadgetJson = ""
+		} else {
+			gadgetJson = gadgetJson + s
+		}
+	}
+	// last gadget will never be parsed by the loop
+	gadgets = append(gadgets, *parseGadget(gadgetHeader, gadgetJson, &gadgets))
+	return gadgets
+}
+
+func ExpandGadget(gadget *Gadget, params []string) string {
+	str := gadget.json
+	for i := 0; i < gadget.parametersCount; i++ {
+		strings.ReplaceAll(str, "%"+strconv.Itoa(i), strings.TrimSpace(params[i]))
+	}
+	return str
+}
+
+func GetGadget(name string, gadgets *[]Gadget) *Gadget {
+	// we should actually build a map of gadgets
+	for _, v := range *gadgets {
+		if v.name == name {
+			return &v
+		}
+	}
+	return nil
 }
 
 func LoadGadgetsForPath(p string) *[]Gadget {
-	gadgets := builtinGadgets
+	gadgets := &builtinGadgets
 	info, err := os.Stat(p)
 	if err != nil {
 		panic(err)
 	}
 
-	if info.IsDir() {
-		for true {
-			//f, d, gadgets := loadGadgetsForPath(p, )
-		}
-	} else {
-
+	searchDir := p
+	if !info.IsDir() {
+		searchDir, _ = path.Split(p)
 	}
-	// get pylons.gadgets for the dir we're in, then for each parent dir that has one
-	// read out gadget files, error check them, add them to a gadget list
-	// append builtin gadgets and return
+
+	var dir string
+	// refactor this to not be for/break, it's gross
+	for true {
+		dir, _, gadgets = loadGadgetsForPath(searchDir, gadgets)
+		if dir != "" {
+			searchDir = dir
+		} else {
+			break
+		}
+	}
+	return gadgets
 }
