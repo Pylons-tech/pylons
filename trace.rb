51129b4a8b233dd0cc05538feb35a557c3ac1587
Michael Sofaer <m@Michaels-MacBook-Pro.local>
2022-08-11 16:06:46 -0400

    base64 decode key and value

--- trace.rb
+++ trace.rb
@@ -1,6 +1,8 @@
-require "json";
+require "json"
+require "base64"
 
-ARGF.each_line{|line| j = JSON.parse(line);
-  next if ["staking","slashing"].member?(j["metadata"]["store_name"]) 
+ARGF.each_line{|line| j = JSON.parse line
+  next if ["staking","slashing"].member? j["metadata"]["store_name"]
+  j["key"] = Base64.decode64 j["key"]
   puts j
 }
