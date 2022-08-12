ecbf6e9e62bfe4b478f16b92c1cb071ce7937439
Michael Sofaer <m@Michaels-MacBook-Pro.local>
2022-08-11 21:10:14 -0400

    drop reads

--- trace.rb
+++ trace.rb
@@ -3,6 +3,7 @@ require "base64"
 
 ARGF.each_line{|line| j = JSON.parse line
   next if ["staking","slashing"].member? j["metadata"]["store_name"]
+  next if ["read"].member? j["operation"]
   j["key"] = Base64.decode64 j["key"]
   puts j
 }
