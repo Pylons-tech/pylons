a717a03f7f9f841484b66637cc3cb060300c58b1
Abdullah Jan Khan <abdullah.jan@rnssol.com>
2022-08-18 16:17:11 +0500

    dockerization for decoupled backend/frontend was implemented

--- big-dipper-backend/scripts/build.sh
+++ big-dipper-backend/scripts/build.sh
@@ -2,11 +2,9 @@
 pwd = $(pwd)
 cd app
 echo "Pre Builing node_modules production..."
-meteor npm install
+meteor npm install --allow-superuser
 echo "Builing for production..."
-meteor build output/ --architecture os.linux.x86_64 --server-only --allow-superuser
-cd output/
-tar -xvf *.tar.gz 
-echo "Post Builing node_modules for production..."
-cd bundle/program/server && npm install
+meteor build ../output/ --architecture os.linux.x86_64 --server-only --allow-superuser
+cd ../output
+tar -xf *.tar.gz 
 cd $pwd
