9f8b6fc6643b157d5a4503c13956d8ae9e3f5261
GNaD <cpt.meoz@gmail.com>
2022-08-18 21:35:14 +0700

    fix var name

--- scripts/spam_mitigation.sh
+++ scripts/spam_mitigation.sh
@@ -20,19 +20,19 @@ MY_TESTING_ACCOUNT_2=$(pylonsd keys show $TEST_ACCOUNT_2 -a --keyring-backend $K
 # Send token to testing account
 VALIDATOR=$(pylonsd keys show $KEY -a --keyring-backend $KEYRING)
 pylonsd tx bank send $VALIDATOR $MY_TESTING_ACCOUNT_1 1000000000upylon --keyring-backend $KEYRING --yes
-
-#spam tx
-echo "===> Spam tx"
+echo "===> Wait for setting up env"
 sleep 5
+#spam tx
+echo "===> Begin test spam tx"
 
 counter=1
 while [ $counter -le 10 ]
 do
     echo "=================$counter================="
-    RET=$(pylonsd tx bank send $MY_TESTING_ACCOUNT_1 $MY_TESTING_ACCOUNT_2 1000000upylon --keyring-backend $KEYRING --yes)
+    RES=$(pylonsd tx bank send $MY_TESTING_ACCOUNT_1 $MY_TESTING_ACCOUNT_2 1000000upylon --keyring-backend $KEYRING --yes)
     # get the return code
-    STRINGTEST=(${RET[@]})
-    echo "${STRINGTEST[0]} ${STRINGTEST[1]}"
+    RES_ARRAY=(${RES[@]})
+    echo "${RES_ARRAY[0]} ${RES_ARRAY[1]}"
     ((counter++))
 done
 
