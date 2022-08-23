dfa612834e112e9fc4bcf0017776ca8b09232102
Abdullah Jan Khan <abdullah.jan@rnssol.com>
2022-08-23 13:57:05 +0500

    updated notification code removed redundacy, fix main.js:'

--- big-dipper-backend/imports/api/notifications/server/methods.js
+++ big-dipper-backend/imports/api/notifications/server/methods.js
@@ -5,8 +5,6 @@ import { sanitizeUrl } from '@braintree/sanitize-url'
 import { HTTP } from 'meteor/http'
 import { admin } from '../../admin.js'
 
-import { Notifications } from '../notifications.js'
-
 import { Res } from '../../res.js'
 
 import { Utils } from '../../utils.js'
@@ -14,71 +12,6 @@ import { Utils } from '../../utils.js'
 import { Codec } from '../../codec.js'
 
 import { API } from '../../api.js'
-
-API.addRoute(
-  'notifications/getAllNotifications/:address/:limit/:offset',
-  {
-    authRequired: false
-  },
-  {
-    get: function () {
-      if (
-        Utils.ValidateAddress(this.urlParams.address) &&
-        this.urlParams.limit &&
-        this.urlParams.offset
-      ) {
-        try {
-          const res = getNotifications(
-            this.urlParams.address,
-            this.urlParams.limit,
-            this.urlParams.offset
-          )
-
-          return Res.Success({
-            results: res
-          })
-        } catch (e) {
-          return Res.InValidInput(Codec.BadRequestMessage, null)
-        }
-      }
-
-      return Res.InValidInput(
-        Codec.BadRequestMessage,
-        'requires params /:address/:limit/:offset'
-      )
-    }
-  }
-)
-
-API.addRoute(
-  'notifications/markread',
-  {
-    authRequired: false
-  },
-  {
-    post: function () {
-      const notificationIDs = this.bodyParams.notificationIDs
-      if (notificationIDs && notificationIDs.length > 0) {
-        for (let index = 0; index < notificationIDs.length; index++) {
-          const id = notificationIDs[index]
-          // mark as Read
-          const result = markRead(id)
-          if (result !== 1) {
-            return Res.InValidInput(
-              Codec.InvalidIDMessage,
-              `notificationID ${id} is invalid`
-            )
-          }
-        }
-        return Res.Success('notifications marked as Read')
-      }
-      return Res.InValidInput(
-        Codec.InvalidIDMessage,
-        'notificationIDs list is missing or corrupt'
-      )
-    }
-  }
-)
 
 API.addRoute(
   'notifications/getAllNotifications/:address/:limit/:offset',
--- big-dipper/server/main.js
+++ big-dipper/server/main.js
@@ -273,13 +273,13 @@ aggregateDaily = () => {
 Meteor.startup(async function() {
     if (Meteor.isDevelopment) {
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = 1;
-        import DEFAULTSETTINGSJSON from '../settings.json'
-        Object.keys(DEFAULTSETTINGSJSON).forEach((key) => {
+        const settings = Meteor.settings;
+        Object.keys(settings).forEach((key) => {
             if (Meteor.settings[key] == undefined) {
                 console.warn(`CHECK SETTINGS JSON: ${key} is missing from settings`)
                 Meteor.settings[key] = {};
             }
-            Object.keys(DEFAULTSETTINGSJSON[key]).forEach((param) => {
+            Object.keys(Meteor.settings[key]).forEach((param) => {
                 if (Meteor.settings[key][param] == undefined) {
                     console.warn(`CHECK SETTINGS JSON: ${key}.${param} is missing from settings`)
                     Meteor.settings[key][param] = DEFAULTSETTINGSJSON[key][param]
