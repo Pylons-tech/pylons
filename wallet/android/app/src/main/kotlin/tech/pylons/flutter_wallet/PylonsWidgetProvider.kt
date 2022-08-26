f18fc5145d712cc7b77486dabb592b8c11a52230
Christine Amin <christine.t.amin@gmail.com>
2022-08-26 02:19:48 -0500

    switched to new repo

---
+++ wallet/android/app/src/main/kotlin/tech/pylons/flutter_wallet/PylonsWidgetProvider.kt
@@ -0,0 +1,40 @@
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetBackgroundIntent
+import es.antonborri.home_widget.HomeWidgetLaunchIntent
+import es.antonborri.home_widget.HomeWidgetProvider
+
+class PylonsWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetIds: IntArray
+    ) {
+        appWidgetIds.forEach { appWidgetId ->
+
+
+            val intent = Intent(context, PylonsWidgetService::class.java).apply {
+                putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+                data = Uri.parse(toUri(Intent.URI_INTENT_SCHEME))
+            }
+            val views = RemoteViews(context.packageName, R.layout.pylons_widget_layout).apply {
+            }
+                val pendingIntent = HomeWidgetLaunchIntent.getActivity(
+                    context,
+                    MainActivity::class.java)
+                setOnClickPendingIntent(R.id.widget_container, pendingIntent)
+
+                setRemoteAdapter(R.id.grid_view, intent)
+
+                setEmptyView(R.id.grid_view, R.id.empty_view)
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
+
---
+++ wallet/android/app/src/main/kotlin/tech/pylons/flutter_wallet/PylonsWidgetService.kt
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+class PylonsWidgetService : RemoteViewsService() {
+
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
+        return GridRemoteViewsFactory(this.applicationContext, intent)
+    }
+}
+
+class GridRemoteViewsFactory(
+    private val context: Context,
+    intent: Intent
+) : RemoteViewsService.RemoteViewsFactory {
+
+    private lateinit var widgetItems: List<WidgetItem>
+
+    override fun onCreate() {
+        // In onCreate() you setup any connections / cursors to your data
+        // source. Heavy lifting, for example downloading or creating content
+        // etc, should be deferred to onDataSetChanged() or getViewAt(). Taking
+        // more than 20 seconds in this call will result in an ANR.
+        widgetItems = List(REMOTE_VIEW_COUNT) { index -> WidgetItem("$index!") }
+        ...
+    }
+
+    override fun getViewAt(position: Int): RemoteViews {
+        // Construct a remote views item based on the widget item XML file,
+        // and set the text based on the position.
+        return RemoteViews(context.packageName, R.layout.widget_item).apply {
+            setTextViewText(R.id.widget_item, widgetItems[position].text)
+        }
+    }
+
+
+
+// See the RemoteViewsFactory API reference for the full list of methods to
+// implement.
+
+}
