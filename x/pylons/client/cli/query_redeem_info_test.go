3c9d18dd093dc1df44c68bd58fad4ce5e42d8502
ThanhNhann <thanhnhan98qh@gmail.com>
2022-08-31 16:33:51 +0700

    update code

--- x/pylons/client/cli/query_redeem_info_test.go
+++ x/pylons/client/cli/query_redeem_info_test.go
@@ -15,49 +15,46 @@ import (
 	"github.com/Pylons-tech/pylons/x/pylons/types"
 )
 
-func TestListRedeemInfo(t *testing.T) {
+func TestShowRedeemInfo(t *testing.T) {
 	net, redeemInfos := util.NetworkWithRedeemInfoObjects(t, 2)
 	ctx := net.Validators[0].ClientCtx
 	common := []string{
 		fmt.Sprintf("--%s=json", tmcli.OutputFlag),
 	}
 	for _, tc := range []struct {
-		desc    string
-		address string
-		id      string
-		args    []string
-		err     error
-		obj     types.RedeemInfo
+		desc string
+		id   string
+		args []string
+		err  error
+		obj  types.RedeemInfo
 	}{
 		{
-			desc:    "Found",
-			address: redeemInfos[0].Address,
-			id:      redeemInfos[0].Id,
-			args:    common,
-			obj:     redeemInfos[0],
+			desc: "Found",
+			id:   redeemInfos[0].Id,
+			args: common,
+			obj:  redeemInfos[0],
 		},
 		{
-			desc:    "Not Found",
-			address: "Not Found",
-			id:      "Not Found",
-			args:    common,
-			err:     status.Error(codes.InvalidArgument, "not found"),
+			desc: "Not Found",
+			id:   "Not Found",
+			args: common,
+			err:  status.Error(codes.InvalidArgument, "not found"),
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
-			args := []string{tc.address, tc.id}
+			args := []string{tc.id}
 			args = append(args, tc.args...)
-			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListRedeemInfo(), args)
+			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdShowRedeemInfo(), args)
 			if tc.err != nil {
 				stat, ok := status.FromError(tc.err)
 				require.True(t, ok)
 				require.ErrorIs(t, stat.Err(), tc.err)
 			} else {
 				require.NoError(t, err)
-				var resp types.QueryAllRedeemInfoResponse
+				var resp types.QueryGetRedeemInfoResponse
 				require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-				require.NotNil(t, resp.RedeemInfo[0])
-				require.Equal(t, tc.obj, resp.RedeemInfo[0])
+				require.NotNil(t, resp.RedeemInfo)
+				require.Equal(t, tc.obj, resp.RedeemInfo)
 			}
 		})
 	}
