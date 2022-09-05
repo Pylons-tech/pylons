87102f202485e5f7275f0f4609396a7301401346
faizhashmiRnS <faiz.hashmi@rnssol.com>
2022-09-05 15:17:58 +0500

    Pipeline fail issue fixed

--- easel/lib/utils/extension_util.dart
+++ easel/lib/utils/extension_util.dart
@@ -9,8 +9,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:path/path.dart' as path;
 import 'package:pylons_sdk/pylons_sdk.dart';
 
-import 'constants.dart';
-
 extension ScaffoldHelper on BuildContext? {
   void show({required String message}) {
     if (this == null) {
