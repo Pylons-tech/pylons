ea547aff89268e094c6045111643c7d44c83ead7
Christine Amin <christine.t.amin@gmail.com>
2022-09-07 16:38:49 -0500

    reorganized collectionsApi call

--- wallet/android/app/src/main/kotlin/tech/pylons/flutter_wallet/PylonsWidgetService.kt
+++ wallet/android/app/src/main/kotlin/tech/pylons/flutter_wallet/PylonsWidgetService.kt
@@ -1,54 +1,41 @@
-package tech.pylons.wallet
+package tech.pylons.flutter_wallet
 
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import androidx.core.net.toUri
-import io.flutter.embedding.engine.FlutterEngine
-import java.util.List
-//import tech.pylons.wallet.Pigeon
-import io.flutter.embedding.android.FlutterActivity
-import tech.pylons.flutter_wallet.MainActivity
+import tech.pylons.wallet.Pigeon
+import tech.pylons.wallet.R
 
 
 class PylonsWidgetService : RemoteViewsService() {
     
-    //private var collectionsApi: Pigeon.CollectionsApi = MainActivity
+    private lateinit var collectionsApi: Pigeon.CollectionsApi
 
     override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
-        return GridRemoteViewsFactory(this.applicationContext, intent)//, collectionsApi)
+        return GridRemoteViewsFactory(this.applicationContext, intent, collectionsApi)
     }
 }
 
 class GridRemoteViewsFactory(
     private val context: Context,
     intent: Intent,
-    //collectionsApi: Pigeon.CollectionsApi
+    private val collectionsApi: Pigeon.CollectionsApi
 ) : RemoteViewsService.RemoteViewsFactory {
     private var count: Int = 6
 
-    //private var collectionsApi: Pigeon.CollectionsApi = Pigeon.CollectionsApi(flutterEngine.dartExecutor.binaryMessenger)
-
-    //private lateinit var widgetItems: List
-
-    private var widgetItems = listOf<String>("san-antonio-tx-flowers-on-the-porch-joanne-beecham")
+    private lateinit var widgetItems: List<Pigeon.NFTMessage>
 
     override fun onCreate() {
-        // In onCreate() you setup any connections / cursors to your data
-        // source. Heavy lifting, for example downloading or creating content
-        // etc, should be deferred to onDataSetChanged() or getViewAt(). Taking
-        // more than 20 seconds in this call will result in an ANR.
-        //widgetItems = collectionsApi.getCollections()
-        //widgetItems = List(REMOTE_VIEW_COUNT) { index -> WidgetItem("$index!") }
-        //widgetItems = listOf("san-antonio-tx-flowers-on-the-porch-joanne-beecham")
+        widgetItems = collectionsApi.getCollection()
+
     }
 
     override fun getViewAt(position: Int): RemoteViews {
-        // Construct a remote views item based on the widget item XML file,
-        // and set the text based on the position.
+
         return RemoteViews(context.packageName, R.layout.nft_image).apply {
-            setImageViewUri(R.id.nft, widgetItems[position].toUri())//.imageUrl.toUri())
+            setImageViewUri(R.id.nft, widgetItems[position].imageUrl.toUri())
         }
     }
 
@@ -69,18 +56,9 @@ class GridRemoteViewsFactory(
     }
 
     override fun onDataSetChanged() {
-        // This is triggered when you call AppWidgetManager notifyAppWidgetViewDataChanged
-        // on the collection view corresponding to this factory. You can do heaving lifting in
-        // here, synchronously. For example, if you need to process an image, fetch something
-        // from the network, etc., it is ok to do it here, synchronously. The widget will remain
-        // in its current state while work is being done here, so you don't need to worry about
-        // locking up the widget.
     }
 
     override fun onDestroy() {
-        // In onDestroy() you should tear down anything that was setup for your data source,
-        // eg. cursors, connections, etc.
-        //widgetItems.clear()
     }
 
     override fun getCount(): Int {
