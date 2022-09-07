42354c481327713daed04afafacdecbf8cd73734
Christine Amin <christine.t.amin@gmail.com>
2022-09-07 13:52:49 -0500

    added flutter engine that produces binary message

--- wallet/android/app/src/main/kotlin/tech/pylons/flutter_wallet/MainActivity.kt
+++ wallet/android/app/src/main/kotlin/tech/pylons/flutter_wallet/MainActivity.kt
@@ -1,6 +1,23 @@
 package tech.pylons.flutter_wallet
 
 import io.flutter.embedding.android.FlutterActivity
+import tech.pylons.wallet.Pigeon
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+    lateinit var collectionsApi: Pigeon.CollectionsApi
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl("https://newsapi.org/")
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+
+        //val service: ArticleService = retrofit.create(ArticleService::class.java)
+
+        collectionsApi = Pigeon.CollectionsApi(flutterEngine.dartExecutor.binaryMessenger)
+    }
+
 }
