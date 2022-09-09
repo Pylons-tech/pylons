1ebf4389eaeb7a1ba5e00c326007d932b23f9eca
GNaD <cpt.meoz@gmail.com>
2022-09-09 17:34:44 +0700

    add func mint ubedrock

--- app/upgrade/mainnet/upgrades.go
+++ app/upgrade/mainnet/upgrades.go
@@ -7,6 +7,56 @@ import (
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 )
 
+const (
+	//----------- FAKE ADDRESS---------------//
+	// StakeholderDistribution wallet address
+	StakeholderDistribution = "pylo1csclcc6rzs7z3eyjqvzrrgrfh79hng2gp0y9aq"
+	// Incentive Pools wallet address
+	IncentivePools = "pylo1m8dczugvz5sa8qe6h0ehcd92uqr9hpm55ulp55"
+	// Foundation Discretionary wallet address
+	FoundationDiscretionary = "pylo1d4u3yf6x382medzspuwtnv6jl6q73j0hshjmsr"
+	// Token Presale wallet address
+	TkPreSale = "pylo1wukgguc6z3fxw8zcdzfwkzpqc67xkhul9mra7f"
+	// Token CF Sale wallet address
+	TkCFSale = "pylo1pu0h7cyx8waj3c6rqmagv3ll5qvygyxvtnzyvv"
+	// Token Reg D+S Sale wallet address
+	TkRegDSSale = "pylo1qufsv679033gm7x372hdwc00gzs32f7r4hjjmu"
+	// Company Revenue wallet address
+	CompanyRevenue = "pylo1hpgtmcvtcnnzt5k8zaxgrzd7dtg76575grpcnv"
+	// Engineering Hot Wallet wallet address
+	EngineHotWal = "pylo1zag5vc7nfwa0scyrzfdsjj000xexfeyd956pqh"
+	//----------- FAKE ADDRESS---------------//
+)
+
+var (
+	Accounts = []string{
+		StakeholderDistribution,
+		IncentivePools,
+		FoundationDiscretionary,
+		TkPreSale,
+		TkCFSale,
+		TkRegDSSale,
+		CompanyRevenue,
+		EngineHotWal,
+	}
+
+	TotalUbedrock = math.NewIntFromUint64(1_000_000_000)
+
+	UbedrockDistribute = map[string]math.Int{
+		Accounts[0]: math.NewIntFromUint64(150_000_000),
+		Accounts[1]: math.NewIntFromUint64(150_000_000),
+		Accounts[2]: math.ZeroInt(),
+		Accounts[3]: math.NewIntFromUint64(20_000_000),
+		Accounts[4]: math.NewIntFromUint64(40_000_000),
+		Accounts[5]: math.NewIntFromUint64(20_000_000),
+		Accounts[6]: math.NewIntFromUint64(619_999_000),
+		Accounts[7]: math.NewIntFromUint64(1000),
+	}
+	_ = Accounts
+	_ = TotalUbedrock
+	_ = UbedrockDistribute
+)
+
 // Burn ubedrock
 func BurnUbedrock(ctx sdk.Context, bank *bankkeeper.BaseKeeper) {
 	// Get all account balances
@@ -26,6 +76,28 @@ func BurnUbedrock(ctx sdk.Context, bank *bankkeeper.BaseKeeper) {
 			if err != nil {
 				panic(err)
 			}
+		}
+	}
+}
+
+// Mint ubedrock for 8 security account
+func MintUbedrockForInitialAccount(ctx sdk.Context, bank *bankkeeper.BaseKeeper) {
+	// Mint coin for module
+	err := bank.MintCoins(ctx, types.PaymentsProcessorName, sdk.NewCoins(sdk.NewCoin(types.StakingCoinDenom, TotalUbedrock)))
+	if err != nil {
+		panic(err)
+	}
+
+	// Send coin from module to account
+	for _, acc := range Accounts {
+		err = bank.SendCoinsFromModuleToAccount(
+			ctx,
+			types.PaymentsProcessorName,
+			sdk.MustAccAddressFromBech32(acc),
+			sdk.NewCoins(sdk.NewCoin(types.StakingCoinDenom, UbedrockDistribute[acc])),
+		)
+		if err != nil {
+			panic(err)
 		}
 	}
 }
--- app/upgrade/mainnet/upgrades_test.go
+++ app/upgrade/mainnet/upgrades_test.go
@@ -3,6 +3,7 @@ package mainnet_test
 import (
 	"testing"
 
+	"cosmossdk.io/math"
 	"github.com/Pylons-tech/pylons/app/apptesting"
 	"github.com/Pylons-tech/pylons/app/upgrade/mainnet"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -34,14 +35,33 @@ func (suite *UpgradeTestSuite) TestBurnUbedrock() {
 	}
 	// Get balances in test account
 	balances := suite.App.BankKeeper.GetBalance(suite.Ctx, suite.TestAccs[0], stakingCoinDenom)
-	suite.Require().Equal(balances.Amount, sdk.NewInt(10000000))
+	suite.Require().Equal(balances.Amount, math.NewInt(10000000))
 	// Get ubedrock total supply
 	totalAmount := suite.App.BankKeeper.GetSupply(suite.Ctx, stakingCoinDenom)
-	suite.Require().Equal(totalAmount.Amount, sdk.NewInt(30000000))
+	suite.Require().Equal(totalAmount.Amount, math.NewInt(30000000))
 	// Burn ubedrock
 	bankBaseKeeper, _ := suite.App.BankKeeper.(bankkeeper.BaseKeeper)
 	mainnet.BurnUbedrock(suite.Ctx, &bankBaseKeeper)
 	// Check ubedrock total supply (should equal 0)
 	totalAmount = suite.App.BankKeeper.GetSupply(suite.Ctx, stakingCoinDenom)
-	suite.Require().Equal(totalAmount.Amount, sdk.ZeroInt())
+	suite.Require().Equal(totalAmount.Amount, math.ZeroInt())
+}
+
+func (suite *UpgradeTestSuite) TestMintUbedrockForInitialAccount() {
+	suite.Setup()
+
+	// Burn ubedrock
+	bankBaseKeeper, _ := suite.App.BankKeeper.(bankkeeper.BaseKeeper)
+	mainnet.BurnUbedrock(suite.Ctx, &bankBaseKeeper)
+	totalAmount := suite.App.BankKeeper.GetSupply(suite.Ctx, stakingCoinDenom)
+	suite.Require().Equal(totalAmount.Amount, math.ZeroInt())
+
+	// Mint ubedrock for initial account
+	mainnet.MintUbedrockForInitialAccount(suite.Ctx, &bankBaseKeeper)
+	totalAmount = suite.App.BankKeeper.GetSupply(suite.Ctx, stakingCoinDenom)
+	suite.Require().Equal(totalAmount.Amount, math.NewIntFromUint64(1_000_000_000))
+	for _, acc := range mainnet.Accounts {
+		accAmount := suite.App.BankKeeper.GetBalance(suite.Ctx, sdk.MustAccAddressFromBech32(acc), stakingCoinDenom)
+		suite.Require().Equal(accAmount.Amount, mainnet.UbedrockDistribute[acc])
+	}
 }
