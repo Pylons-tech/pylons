e66b130a71fc553e1b85e360379f10f753c73a25
ThanhNhann <thanhnhan98qh@gmail.com>
2022-09-13 01:15:32 +0700

    #996 fix lint

--- app/upgrades/v4/upgrades.go
+++ app/upgrades/v4/upgrades.go
@@ -4,23 +4,22 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	bankKeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
-	upgrade_types "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	// "github.com/cosmos/iavl/internal/logger"
-
-	"github.com/Pylons-tech/pylons/x/pylons/types"
+	// "github.com/Pylons-tech/pylons/x/pylons/types"
 )
 
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 	bankKeeper bankKeeper.Keeper,
-) upgrade_types.UpgradeHandler {
-	return func(ctx sdk.Context, plan upgrade_types.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		// logger := ctx.Logger()
 
-		if types.IsMainnet(ctx.ChainID()) {
-			// TODO: Logic upgadeHandler
-		}
+		// if types.IsMainnet(ctx.ChainID()) {
+		// TODO: Logic upgadeHandler
+		// }
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
