e2811bd653257ca5119450245cedb17fa2ff8f42
Faisal Naveed <faisal@rnssol.com>
2022-09-16 19:59:20 +0500

    remove dead code

--- big-dipper/imports/ui/easel_buy/EaselBuy.jsx
+++ big-dipper/imports/ui/easel_buy/EaselBuy.jsx
@@ -792,16 +792,6 @@ export default class EaselBuy extends Component {
                         ) : (
                           <></>
                         )}
-                        {/*For later Use*/}
-                        {/* <div className="views">
-                          {" "}
-                          <img
-                            alt="views"
-                            src="/img/eye.svg"
-                            style={{ width: "34px", height: "20px" }}
-                          />
-                          <p>{this.state.nftViews} views</p>
-                        </div> */}
                       </div>
                       {showHideDetails ? (
                         <>
