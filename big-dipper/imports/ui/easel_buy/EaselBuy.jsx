00766ee7b32367e950ad33025a53f7177d30a0e5
abdulsamad <abdual.samad@rnssol.com>
2022-09-19 12:13:56 +0500

    updated

--- big-dipper/imports/ui/easel_buy/EaselBuy.jsx
+++ big-dipper/imports/ui/easel_buy/EaselBuy.jsx
@@ -434,39 +434,6 @@ export default class EaselBuy extends Component {
                             />
                           </p>
                         </div>
-<<<<<<< HEAD
-                        {/* Can Be Use For Later Version */}
-                        {/* {nftType?.toLowerCase() === "audio" ? (
-                          <>
-                            <audio
-                              controls
-                              onClick={handleClick}
-                              onContextMenu={handleClick}
-                              controlsList="nodownload"
-                              style={{
-                                marginTop: "25px",
-                                height: "50px",
-                              }}
-                            >
-                              <source src={src} type="audio/ogg" />
-                              <source src={src} type="audio/mpeg" />
-                              Your browser does not support the audio element.
-                            </audio>
-                          </>
-                        ) : (
-                          <></>
-                        )} */}
-                        {/*For later Use*/}
-                        {/* <div className="views">
-                          <img
-                            alt="views"
-                            src="/img/eye.svg"
-                            style={{ width: "34px", height: "20px" }}
-                          />
-                          <p>{this.state.nftViews} views</p>
-                        </div> */}
-=======
->>>>>>> 235f6efc61c498e2601d4450ac5297648e10c63d
                       </div>
                       {this.state.description?.length > 35 ? (
                         <>
