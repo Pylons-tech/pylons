9cacac3f141eb7c31f3f60cd387c6dc0f9557175
sine/cosine/tangent <zfshmn@gmail.com>
2022-09-22 09:24:12 -0700

    stubs

--- recipeapp/lib/api2.dart
+++ recipeapp/lib/api2.dart
@@ -1,3 +1,17 @@
-class PylonsApi {
+import 'package:pylons_sdk/pylons_sdk.dart' as sdk;
 
+class Cookbook {
+  final sdk.Cookbook native;
+
+  Execution execute_with(List<Item> inputs) {
+
+  }
+}
+
+class Execution {
+  final sdk.Execution native;
+
+  Function(Execution) onSuccess;
+  Function(Execution) onFailure;
+  Function(Execution) onError;
 }
