d6d6b1e08e1049a41fde048090eab6232e8fae45
Christine Amin <christine.t.amin@gmail.com>
2022-10-16 23:25:57 -0500

    fixed broken test

--- wallet/test/widget_testing/components/wallpaper_widget_test.dart
+++ wallet/test/widget_testing/components/wallpaper_widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pylons_wallet/pages/detailed_asset_view/widgets/wallpaper_set_screen.dart';
 import 'package:pylons_wallet/components/buttons/custom_paint_button.dart';
@@ -21,17 +20,4 @@ void main() {
     expect(gestureDetectorWallpaperScreen, findsOneWidget);
   });
 
-  testWidgets('test case for circle progress indicator', (tester) async {
-    await tester.setScreenSize();
-    await tester.testAppForWidgetTesting(WallpaperScreen(
-      nft: MOCK_NFT_FREE.url,
-    ));
-    await tester.pumpAndSettle();
-    final gestureDetectorWallpaperScreen = find.byType(CustomPaintButton);
-    await tester.tap(gestureDetectorWallpaperScreen);
-    final circularProgressIndicator = find.byType(CircularProgressIndicator);
-    await tester.ensureVisible(circularProgressIndicator);
-    await tester.pumpAndSettle(const Duration(seconds: 5));
-    expect(find.byType(CircularProgressIndicator), findsOneWidget);
-  });
 }
