fec457e33e8247426aac28b52995e97ffc96f072
Christine Amin <christine.t.amin@gmail.com>
2022-10-19 16:56:53 -0500

    added test for nft visibility as well as a test for set wallpaper button pressed

--- wallet/test/widget_testing/components/wallpaper_set_screen_test.dart
+++ wallet/test/widget_testing/components/wallpaper_set_screen_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pylons_wallet/pages/detailed_asset_view/widgets/nft_image_asset.dart';
 import 'package:pylons_wallet/pages/detailed_asset_view/widgets/wallpaper_set_screen.dart';
 import 'package:pylons_wallet/components/buttons/custom_paint_button.dart';
 
@@ -8,7 +9,19 @@ import '../extension/size_extension.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('test case for gestures', (tester) async {
+  testWidgets('test case for nft visibility', (tester) async {
+    await tester.setScreenSize();
+    await tester.testAppForWidgetTesting(WallpaperScreen(
+      nft: MOCK_NFT_FREE.url,
+    ));
+    await tester.pumpAndSettle();
+    final gestureDetectorWallpaperScreen = find.byType(NftImageWidget);
+    await tester.ensureVisible(gestureDetectorWallpaperScreen);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+    expect(gestureDetectorWallpaperScreen, findsOneWidget);
+  });
+
+  testWidgets('test case for set wallpaper button press', (tester) async {
     await tester.setScreenSize();
     await tester.testAppForWidgetTesting(WallpaperScreen(
       nft: MOCK_NFT_FREE.url,
@@ -16,6 +29,7 @@ void main() {
     await tester.pumpAndSettle();
     final gestureDetectorWallpaperScreen = find.byType(CustomPaintButton);
     await tester.ensureVisible(gestureDetectorWallpaperScreen);
+    await tester.tap(gestureDetectorWallpaperScreen);
     await tester.pumpAndSettle(const Duration(seconds: 5));
     expect(gestureDetectorWallpaperScreen, findsOneWidget);
   });
