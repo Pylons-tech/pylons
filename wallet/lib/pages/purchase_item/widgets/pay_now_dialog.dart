efe47072a496a1d597fabe83ce02a1aec174206c
samarali92 <samar.ali@rnssol.com>
2022-10-20 14:43:05 +0500

    redesign pay_now_dialog

--- wallet/lib/pages/purchase_item/widgets/pay_now_dialog.dart
+++ wallet/lib/pages/purchase_item/widgets/pay_now_dialog.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:pylons_wallet/components/buttons/custom_paint_button.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -8,7 +7,6 @@ import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pylons_wallet/components/loading.dart';
-import 'package:pylons_wallet/components/pylons_app_theme.dart';
 import 'package:pylons_wallet/main_prod.dart';
 import 'package:pylons_wallet/model/nft.dart';
 import 'package:pylons_wallet/model/stripe_create_payment_intent_request.dart';
@@ -29,12 +27,11 @@ import 'package:pylons_wallet/utils/enums.dart' as enums;
 import 'package:pylons_wallet/utils/enums.dart';
 import 'package:pylons_wallet/utils/route_util.dart';
 import 'package:pylons_wallet/utils/svg_util.dart';
-import 'package:shimmer_animation/shimmer_animation.dart';
 import 'package:provider/provider.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
-TextStyle _titleTextStyle = TextStyle(color: Colors.white, fontSize: 16.sp);
-TextStyle _subtitleTextStyle = TextStyle(color: Colors.white, fontSize: 12.sp);
-TextStyle _rowTitleTextStyle = TextStyle(color: Colors.white, fontWeight: FontWeight.w800, fontSize: 13.sp);
+TextStyle _titleTextStyle = TextStyle(color: Colors.white, fontSize: 30.sp, fontWeight: FontWeight.w700);
+TextStyle _rowTitleTextStyle = TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 13.sp);
 TextStyle _rowSubtitleTextStyle = TextStyle(color: Colors.white, fontSize: 14.sp);
 
 class PayNowDialog {
@@ -83,39 +80,32 @@ class PayNowWidget extends StatefulWidget {
 class _PayNowWidgetState extends State<PayNowWidget> {
   @override
   Widget build(BuildContext context) {
-    final fee = double.parse(widget.nft.price) * 0.1;
-    final price = double.parse(widget.nft.price) - fee;
-
     return Container(
       color: Colors.black.withOpacity(0.7),
-      height: isTablet ? 290.h : 340.h,
+      // height: isTablet ? 390.h : 390.h,
+      height: 390.h,
       margin: isTablet ? EdgeInsets.symmetric(horizontal: 30.w) : EdgeInsets.zero,
       child: Stack(
         children: [
           Positioned(
             right: 0,
-            bottom: 0,
-            child: SizedBox(
-              height: 60,
-              width: 80,
-              child: ClipPath(
-                clipper: RightTriangleClipper(orientation: enums.Orientation.Orientation_NW),
-                child: ColoredBox(
-                  color: AppColors.kDarkRed,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0,
             top: 0,
             child: SizedBox(
               height: 60,
               width: 80,
               child: ClipPath(
-                clipper: RightTriangleClipper(orientation: enums.Orientation.Orientation_SE),
-                child: ColoredBox(
+                clipper: RightTriangleClipper(orientation: enums.Orientation.Orientation_SW),
+                child: Container(
                   color: AppColors.kDarkRed,
+                  alignment: Alignment.topRight,
+                  child: GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: SizedBox(
+                        height: isTablet ?  25.h : 40.h,
+                        width: isTablet ? 25.w : 40.w,
+                        child: const Icon(Icons.cancel_sharp, color: Colors.white,)
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -129,77 +119,46 @@ class _PayNowWidgetState extends State<PayNowWidget> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                ListTile(
-                  leading: SizedBox(
-                    height: 50.h,
-                    width: 50.h,
-                    child: CachedNetworkImage(
-                      imageUrl: widget.nft.url,
-                      width: 1.sw,
-                      errorWidget: (a, b, c) => Center(
-                          child: Text(
-                        "unable_to_fetch_nft_item".tr(),
-                        style: Theme.of(context).textTheme.bodyText1,
-                      )),
-                      height: 1.sh,
-                      fit: BoxFit.fitHeight,
-                      placeholder: (context, _) => Shimmer(
-                        color: PylonsAppTheme.cardBackground,
-                        child: Container(),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    widget.nft.name,
-                    style: _titleTextStyle,
-                    maxLines: 1,
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        "${"created_by".tr()} ${widget.nft.creator}",
-                        style: _subtitleTextStyle,
-                      ),
-                      if (widget.nft.owner.isNotEmpty)
-                        Text(
-                          "${"sold_by".tr()} ${widget.nft.owner}",
-                          style: _subtitleTextStyle,
-                        ),
-                    ],
-                  ),
+                SizedBox(
+                  height: 60.h,
                 ),
+                Padding(
+                    padding: EdgeInsets.only(left: 20.w),
+                    child: Text("checkout".tr(), style: _titleTextStyle)),
                 SizedBox(
                   height: 30.h,
                 ),
                 buildRow(
-                  subtitle: widget.nft.ibcCoins.getAbbrev(),
-                  title: "currency".tr(),
+                  subtitle: widget.nft.name,
+                  title: "NFT",
                 ),
                 SizedBox(
                   height: 3.h,
                 ),
                 buildRow(
-                  subtitle: widget.nft.ibcCoins.getCoinWithDenominationAndSymbol(price.toString(), showDecimal: true),
-                  title: "price".tr(),
-                ),
-                SizedBox(
-                  height: 3.h,
-                ),
-                buildPylonsFeeRow(
-                  subtitle: widget.nft.ibcCoins.getCoinWithDenominationAndSymbol(fee.toString(), showDecimal: true),
-                  title: "pylons_fee".tr(),
+                  subtitle: "#${widget.nft.amountMinted} of ${widget.nft.quantity}",
+                  title: "edition".tr(),
                 ),
                 SizedBox(
                   height: 3.h,
                 ),
                 buildRow(
-                  subtitle: widget.nft.ibcCoins.getCoinWithDenominationAndSymbol(widget.nft.price, showDecimal: true),
-                  title: "total".tr(),
+                  subtitle: widget.nft.type == NftType.TYPE_RECIPE ? widget.nft.creator : widget.nft.owner,
+                  subtitleTextColor: AppColors.kTradeReceiptTextColor,
+                  title: "artist".tr(),
                 ),
                 SizedBox(
                   height: 30.h,
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 20.w),
+                    child: Text('price'.tr(), style: _rowTitleTextStyle,)),
+                SizedBox(
+                  height: 3.h,
+                ),
+                _buildPriceView(),
+                SizedBox(
+                  height: 60.h,
                 ),
                 if (!widget.shouldBuy)
                   Center(
@@ -232,8 +191,7 @@ class _PayNowWidgetState extends State<PayNowWidget> {
       ),
     );
   }
-
-  Widget buildRow({required String title, required String subtitle}) {
+  Widget buildRow({required String title, required String subtitle, Color subtitleTextColor = Colors.white}) {
     return Row(
       children: [
         Expanded(
@@ -247,8 +205,10 @@ class _PayNowWidgetState extends State<PayNowWidget> {
         Expanded(
             child: Text(
           subtitle,
-          style: _rowSubtitleTextStyle,
-        ))
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(color: subtitleTextColor, fontSize: 14.sp),
+        ),),
       ],
     );
   }
@@ -319,6 +279,37 @@ class _PayNowWidgetState extends State<PayNowWidget> {
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildPriceView() {
+    return Padding(
+      padding: EdgeInsets.only(left: 20.w, right: 20.w),
+      child: Row(
+        children: [
+          Expanded(flex: 2, child: Container(
+            height: 40.h,
+            color: AppColors.kDarkRed,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                    width: 15.w,
+                    height: 15.w,
+                    child: widget.nft.ibcCoins.getAssets()),
+                SizedBox(width: 3.w),
+                Text(widget.nft.ibcCoins.getAbbrev(),  style: TextStyle(fontSize: 10.sp, color: Colors.white),),
+              ],
+            ),),),
+          Expanded(flex: 3, child: Container(
+            height: 40.h,
+            color: AppColors.kGreyColor, child: Center(
+            child: AutoSizeText(widget.nft.ibcCoins.getCoinWithProperDenomination(widget.nft.price),
+              maxLines: 1,
+              style: TextStyle(fontSize: 18.sp, color: Colors.white, fontWeight: FontWeight.w700),),
+          ),),),
+        ],
       ),
     );
   }
