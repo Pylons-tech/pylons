b1979c3a486edb36b85cddd13c04b96cae176d0d
Christine Amin <christine.t.amin@gmail.com>
2022-11-08 12:51:14 -0600

    cleaned up code

--- wallet/lib/pages/detailed_asset_view/widgets/pdf_viewer.dart
+++ wallet/lib/pages/detailed_asset_view/widgets/pdf_viewer.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
@@ -24,7 +23,6 @@ class PdfViewer extends StatefulWidget {
 }
 
 class _PdfViewerState extends State<PdfViewer> with WidgetsBindingObserver {
-  //late PDFDocument doc;
   late String doc;
   bool _isLoading = true;
 
@@ -37,33 +35,20 @@ class _PdfViewerState extends State<PdfViewer> with WidgetsBindingObserver {
 
   @override
   void initState() {
-    //initializeDoc();
     createFileOfPdfUrl();
-
     super.initState();
   }
 
-  // Future initializeDoc() async {
-  //   doc = await PDFDocument.fromURL(widget.fileUrl!);
-  //   _isLoading = false;
-  //   setState(() {});
-  // }
-
   Future createFileOfPdfUrl() async {
-    Completer<File> completer = Completer();
-    print("Start download file from internet!");
+    final Completer<File> completer = Completer();
     try {
-      // "https://berlin2017.droidcon.cod.newthinking.net/sites/global.droidcon.cod.newthinking.net/files/media/documents/Flutter%20-%2060FPS%20UI%20of%20the%20future%20%20-%20DroidconDE%2017.pdf";
-      // final url = "https://pdfkit.org/docs/guide.pdf";
       final url = widget.fileUrl!;
       final filename = url.substring(url.lastIndexOf("/") + 1);
-      var request = await HttpClient().getUrl(Uri.parse(url));
-      var response = await request.close();
-      var bytes = await consolidateHttpClientResponseBytes(response);
-      var dir = await getApplicationDocumentsDirectory();
-      print("Download files");
-      print("${dir.path}/$filename");
-      File file = File("${dir.path}/$filename");
+      final request = await HttpClient().getUrl(Uri.parse(url));
+      final response = await request.close();
+      final bytes = await consolidateHttpClientResponseBytes(response);
+      final dir = await getApplicationDocumentsDirectory();
+      final File file = File("${dir.path}/$filename");
 
       await file.writeAsBytes(bytes, flush: true);
       completer.complete(file);
@@ -95,20 +80,8 @@ class _PdfViewerState extends State<PdfViewer> with WidgetsBindingObserver {
       child: Center(
         child: Stack(
           children: [
-            // PDFViewer(
-            //   document: doc,
-            //   showNavigation: false,
-            //   showPicker: false,
-            //   progressIndicator: SizedBox(
-            //     height: 50.0.h,
-            //     child: Image.asset(
-            //       ImageUtil.LOADING_GIF,
-            //     ),
-            //   ),
-            // ),
             PDFView(
               filePath: doc,
-              enableSwipe: true,
               swipeHorizontal: true,
               autoSpacing: false,
               pageFling: false,
@@ -118,17 +91,8 @@ class _PdfViewerState extends State<PdfViewer> with WidgetsBindingObserver {
                   isReady = true;
                 });
               },
-              onError: (error) {
-                print(error.toString());
-              },
-              onPageError: (page, error) {
-                print('$page: ${error.toString()}');
-              },
               onViewCreated: (PDFViewController pdfViewController) {
                 _controller.complete(pdfViewController);
-              },
-              onPageChanged: (int? page, int? total) {
-                print('page change: $page/$total');
               },
             ),
             _buildPdfFullScreenIcon()
