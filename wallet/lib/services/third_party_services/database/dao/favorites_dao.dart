ec940e1f5a748a831f0856937fdb42dc5501d112
jawad <jawad@pylons.tech>
2022-11-09 22:54:36 +0100

    Added favourites table

--- wallet/lib/services/third_party_services/database/dao/favorites_dao.dart
+++ wallet/lib/services/third_party_services/database/dao/favorites_dao.dart
@@ -5,11 +5,14 @@ import '../../../../model/favorites.dart';
 @dao
 abstract class FavoritesDao {
   @Query('SELECT * FROM FavoritesModel ORDER BY dateTime DESC')
-  Future<List<FavoritesModel>> getAllFailuresEntries();
+  Future<List<FavoritesModel>> getAll();
 
   @insert
   Future<int> insertFavorites(FavoritesModel favoritesModel);
 
   @Query('DELETE FROM FavoritesModel WHERE id = :id')
   Future<void> delete(int id);
+
+  @Query('DELETE * FROM FavoritesModel')
+  Future<void> deleteAll(int id);
 }
