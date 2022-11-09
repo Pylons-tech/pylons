fc1978cd5c6d4de5006f3da7946b7875a6c8fe07
Christine Amin <christine.t.amin@gmail.com>
2022-11-09 09:55:25 -0600

    updated var naming

--- easel/lib/widgets/pdf_viewer_full_screen.dart
+++ easel/lib/widgets/pdf_viewer_full_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 import 'package:easel_flutter/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PdfViewerFullScreen extends StatefulWidget {
@@ -12,22 +15,43 @@ class PdfViewerFullScreen extends StatefulWidget {
   State<PdfViewerFullScreen> createState() => _PdfViewerFullScreenState();
 }
 
-class _PdfViewerFullScreenState extends State<PdfViewerFullScreen> {
+class _PdfViewerFullScreenState extends State<PdfViewerFullScreen> with WidgetsBindingObserver {
+
+  final Completer<PDFViewController> _controller =
+  Completer<PDFViewController>();
+  int? pages = 0;
+  bool isReady = false;
+
   @override
   Widget build(BuildContext context) {
-    final doc = ModalRoute.of(context)!.settings.arguments as List<PDFDocument>;
+    final doc = ModalRoute.of(context)!.settings.arguments as List<String>;
     return SafeArea(
       child: Scaffold(
         body: Center(
-            child: PDFViewer(
-          document: doc[0],
-          progressIndicator: SizedBox(
-            height: 50.0.h,
-            child: Image.asset(
-              kLoadingGif,
+            child:
+        //     PDFViewer(
+        //   document: doc[0],
+        //   progressIndicator: SizedBox(
+        //     height: 50.0.h,
+        //     child: Image.asset(
+        //       kLoadingGif,
+        //     ),
+        //   ),
+        // )
+            PDFView(
+              filePath: doc[0],
+              swipeHorizontal: true,
+              onRender: (pgs) {
+                setState(() {
+                  pages = pgs;
+                  isReady = true;
+                });
+              },
+              onViewCreated: (PDFViewController pdfViewController) {
+                _controller.complete(pdfViewController);
+              },
             ),
-          ),
-        )),
+        ),
       ),
     );
   }
