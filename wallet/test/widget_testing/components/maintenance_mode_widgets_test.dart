14f0056e8f4eac31f8f58115210fb60cc131b3ad
Christine Amin <christine.t.amin@gmail.com>
2022-11-10 10:38:13 -0600

    attempting mock generation

--- wallet/test/widget_testing/components/maintenance_mode_widgets_test.dart
+++ wallet/test/widget_testing/components/maintenance_mode_widgets_test.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:pylons_wallet/components/maintenance_mode_widgets.dart';
 import 'package:pylons_wallet/pages/home/home.dart';
 import 'package:pylons_wallet/services/third_party_services/remote_config_service/remote_config_service.dart';
 import '../extension/size_extension.dart';
 
+// class MockRemoteConfigService extends Mock implements RemoteConfigService {
+//   @override
+//   bool getMaintenanceMode() {
+//     return true;
+//   }
+// }
+
+class MockRemoteConfigService extends Mock implements RemoteConfigService {}
+
+@GenerateMocks([MockRemoteConfigService])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  RemoteConfigService get remoteConfigService => GetIt.I.get();
+
+  late RemoteConfigService remoteConfigService;
+  remoteConfigService = MockRemoteConfigService();
+  GetIt.I.registerLazySingleton<RemoteConfigService>(() => remoteConfigService);
+
 
   testWidgets('test case for maintenance mode banner widget', (tester) async {
+    when(remoteConfigService.getMaintenanceMode()).thenAnswer((realInvocation) => true);
     await tester.setScreenSize();
     await tester.testAppForWidgetTesting(const HomeScreen());
     await tester.pumpAndSettle();
