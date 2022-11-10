4f2aa0a540b08d6dcb9e4c53fb95d96c2f0b40e8
Ahmad Hassan <ahmad.hassan@rnssol.com>
2022-11-10 19:00:40 +0500

    Added Splash Screen

--- wallet/lib/pages/routing_page/splash_screen.dart
+++ wallet/lib/pages/routing_page/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:ui';
 import 'package:cosmos_utils/app_info_extractor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pylons_wallet/components/loading.dart';
 import 'package:pylons_wallet/pages/settings/utils/user_info_provider.dart';
@@ -10,7 +12,6 @@ import 'package:pylons_wallet/services/data_stores/local_data_store.dart';
 import 'package:pylons_wallet/services/repository/repository.dart';
 import 'package:pylons_wallet/services/third_party_services/remote_config_service/remote_config_service.dart';
 import 'package:pylons_wallet/stores/wallet_store.dart';
-import 'package:pylons_wallet/utils/constants.dart';
 import 'package:pylons_wallet/utils/dependency_injection/dependency_injection.dart';
 import 'package:pylons_wallet/utils/image_util.dart';
 import 'package:pylons_wallet/utils/route_util.dart';
@@ -23,20 +24,37 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late final Timer timer;
+
   WalletsStore get walletsStore => GetIt.I.get();
 
   RemoteConfigService get remoteConfigService => GetIt.I.get();
 
   UserInfoProvider get userInfoProvider => GetIt.I.get();
 
+  int index = 0;
+
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3), () {
-      checkAppLatestOrNot().then((value) {
-        userInfoProvider.initIPC();
-      });
-    });
+    initTimer();
+  }
+
+  void initTimer() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        if (index > 4) {
+          checkAppLatestOrNot().then((value) {
+            userInfoProvider.initIPC();
+          });
+          return;
+        }
+        setState(() {
+          index++;
+        });
+      },
+    );
   }
 
   Future<void> _loadWallets() async {
@@ -78,13 +96,62 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
+      body: DecoratedBox(
         decoration: BoxDecoration(
-          color: AppColors.kSplashScreenBgColor,
           image: DecorationImage(
-            image: AssetImage(ImageUtil.SPLASH_SCREEN_BG),
+            image: AssetImage(
+              ImageUtil.SPLASH_SCREEN_BG,
+            ),
             fit: BoxFit.fill,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 250.0.h,
+                ),
+                AnimatedSwitcher(
+                  duration: const Duration(seconds: 1),
+                  child: Stack(
+                    key: UniqueKey(),
+                    children: [
+                      Opacity(
+                        opacity: 0.2,
+                        child: Image.asset(
+                          ImageUtil.BG_IMAGES[index],
+                          color: Colors.black,
+                        ),
+                      ),
+                      ClipRect(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(
+                            sigmaX: 40.0,
+                            sigmaY: 30.0,
+                          ),
+                          child: Image.asset(
+                            ImageUtil.BG_IMAGES[index],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                SizedBox(
+                  height: 30.0.h,
+                  child: Image.asset(
+                    ImageUtil.PYLONS_LOGO,
+                  ),
+                ),
+                SizedBox(
+                  height: 55.0.h,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -113,5 +180,11 @@ class _SplashScreenState extends State<SplashScreen> {
     Navigator.of(navigatorKey.currentState!.overlay!.context).pushReplacementNamed(RouteUtil.ROUTE_APP_UPDATE, arguments: remoteConfigVersion);
 
     return false;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel();
   }
 }
