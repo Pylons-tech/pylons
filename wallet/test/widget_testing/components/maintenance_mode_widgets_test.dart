d4721c0ae7fbe84b912b0b051a9e6f937cd20b3f
Christine Amin <christine.t.amin@gmail.com>
2022-11-10 12:43:54 -0600

    reverted from using home screen widget as app widget to test maintenance widgets

--- wallet/test/widget_testing/components/maintenance_mode_widgets_test.dart
+++ wallet/test/widget_testing/components/maintenance_mode_widgets_test.dart
@@ -7,32 +7,47 @@ import 'package:pylons_wallet/components/maintenance_mode_widgets.dart';
 import 'package:pylons_wallet/pages/home/home.dart';
 import 'package:pylons_wallet/services/third_party_services/remote_config_service/remote_config_service.dart';
 import '../extension/size_extension.dart';
-//import '../../mocks/maintenance_mode_widgets_test.mocks.dart';
+import '../../mocks/maintenance_mode_widgets_test.mocks.dart';
 
-class MockRemoteConfigService extends Mock implements RemoteConfigService {
-  @override
-  bool getMaintenanceMode() {
-    return true;
-  }
-}
+// class MockRemoteConfigService extends Mock implements RemoteConfigService {
+//   @override
+//   bool getMaintenanceMode() {
+//     return true;
+//   }
+// }
 
 @GenerateMocks([RemoteConfigService])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  late RemoteConfigService remoteConfigService;
-  remoteConfigService = MockRemoteConfigService();
-  GetIt.I.registerLazySingleton<RemoteConfigService>(() => remoteConfigService);
+  // late RemoteConfigService remoteConfigService;
+  // remoteConfigService = MockRemoteConfigService();
+  // GetIt.I.registerLazySingleton<RemoteConfigService>(() => remoteConfigService);
 
 
   testWidgets('test case for maintenance mode banner widget', (tester) async {
-    when(remoteConfigService.getMaintenanceMode()).thenAnswer((realInvocation) => true);
+    // when(remoteConfigService.getMaintenanceMode()).thenAnswer((realInvocation) => true);
     await tester.setScreenSize();
-    await tester.testAppForWidgetTesting(const HomeScreen());
+    // await tester.testAppForWidgetTesting(const HomeScreen());
+    await tester.testAppForWidgetTesting(const Material(
+        child: MaintenanceModeBannerWidget()));
     await tester.pumpAndSettle();
     final maintenanceModeBannerWidget = find.byType(MaintenanceModeBannerWidget);
     await tester.ensureVisible(maintenanceModeBannerWidget);
     await tester.pumpAndSettle(const Duration(seconds: 5));
     expect(maintenanceModeBannerWidget, findsOneWidget);
+  });
+
+  testWidgets('test case for maintenance mode message widget', (tester) async {
+    // when(remoteConfigService.getMaintenanceMode()).thenAnswer((realInvocation) => true);
+    await tester.setScreenSize();
+    // await tester.testAppForWidgetTesting(const HomeScreen());
+    await tester.testAppForWidgetTesting(const Material(
+        child: MaintenanceModeMessageWidget()));
+    await tester.pumpAndSettle();
+    final maintenanceModeMessageWidget = find.byType(MaintenanceModeMessageWidget);
+    await tester.ensureVisible(maintenanceModeMessageWidget);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+    expect(maintenanceModeMessageWidget, findsOneWidget);
   });
 }
