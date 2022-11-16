07872347c6ce1dee73ad822b3593ad4d4a19e809
jawad <jawad@pylons.tech>
2022-11-16 18:59:54 +0100

    Resolved pipeline issue

--- wallet/lib/pages/routing_page/splash_screen.dart
+++ wallet/lib/pages/routing_page/splash_screen.dart
@@ -5,7 +5,6 @@ import 'package:cosmos_utils/app_info_extractor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
-import 'package:googleapis/firestore/v1.dart';
 import 'package:provider/provider.dart';
 import 'package:pylons_wallet/components/loading.dart';
 import 'package:pylons_wallet/pages/settings/utils/user_info_provider.dart';
