f49e99c22f179a1b37e28ca6d56d05c133c35a6d
abdulsamad <abdual.samad@rnssol.com>
2022-11-23 16:35:03 +0500

    Test cases was added for AfterEpochEnd function

--- x/pylons/keeper/hooks_test.go
+++ x/pylons/keeper/hooks_test.go
@@ -1,3 +1,101 @@
 package keeper_test
 
-// TODO
+import (
+	"github.com/Pylons-tech/pylons/x/pylons/keeper"
+	"github.com/Pylons-tech/pylons/x/pylons/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (suite *IntegrationTestSuite) TestAfterEpochEnd() {
+	k := suite.k
+	sk := suite.stakingKeeper
+	ctx := suite.ctx
+	require := suite.Require()
+	bk := suite.bankKeeper
+	ak := suite.accountKeeper
+
+	srv := keeper.NewMsgServerImpl(k)
+	wctx := sdk.WrapSDKContext(ctx)
+
+	amountToPay := sdk.NewCoins(sdk.NewCoin(types.PylonsCoinDenom, sdk.NewInt(100)))
+	creator := types.GenTestBech32FromString("test")
+	executor := types.GenTestBech32FromString("executor")
+	feeCollectorAddr := ak.GetModuleAddress(types.FeeCollectorName)
+
+	types.UpdateAppCheckFlagTest(types.FlagTrue)
+
+	srv.CreateAccount(wctx, &types.MsgCreateAccount{
+		Creator:  executor,
+		Username: "Executor",
+	})
+
+	types.UpdateAppCheckFlagTest(types.FlagFalse)
+	cookbookMsg := &types.MsgCreateCookbook{
+		Creator:      creator,
+		Id:           "testCookbookID",
+		Name:         "testCookbookName",
+		Description:  "descdescdescdescdescdesc",
+		Version:      "v0.0.1",
+		SupportEmail: "test@email.com",
+		Enabled:      true,
+	}
+	_, err := srv.CreateCookbook(sdk.WrapSDKContext(suite.ctx), cookbookMsg)
+	require.NoError(err)
+	recipeMsg := &types.MsgCreateRecipe{
+		Creator:       creator,
+		CookbookId:    "testCookbookID",
+		Id:            "testRecipeID",
+		Name:          "recipeName",
+		Description:   "descdescdescdescdescdesc",
+		Version:       "v0.0.1",
+		BlockInterval: 10,
+		CostPerBlock:  sdk.Coin{Denom: "test", Amount: sdk.ZeroInt()},
+		CoinInputs:    []types.CoinInput{{Coins: amountToPay}},
+		Enabled:       true,
+	}
+	_, err = srv.CreateRecipe(sdk.WrapSDKContext(suite.ctx), recipeMsg)
+	require.NoError(err)
+
+	// create only one pendingExecution
+	msgExecution := &types.MsgExecuteRecipe{
+		Creator:         executor,
+		CookbookId:      "testCookbookID",
+		RecipeId:        "testRecipeID",
+		CoinInputsIndex: 0,
+		ItemIds:         nil,
+	}
+
+	// give coins to requester
+	suite.FundAccount(suite.ctx, sdk.MustAccAddressFromBech32(executor), amountToPay)
+
+	resp, err := srv.ExecuteRecipe(sdk.WrapSDKContext(suite.ctx), msgExecution)
+	require.NoError(err)
+
+	// manually trigger complete execution - simulate endBlocker
+	pendingExecution := k.GetPendingExecution(ctx, resp.Id)
+	execution, _, _, err := k.CompletePendingExecution(suite.ctx, pendingExecution)
+	require.NoError(err)
+	k.ActualizeExecution(ctx, execution)
+
+	// verify execution completion and that requester has no balance left,
+	// also pay and fee are transfered to cookbook owner and fee collector module
+	_ = bk.SpendableCoins(ctx, sdk.MustAccAddressFromBech32(executor))
+	_ = bk.SpendableCoins(ctx, sdk.MustAccAddressFromBech32(creator))
+	_ = bk.SpendableCoins(ctx, feeCollectorAddr)
+
+	distrPercentages := k.GetRewardsDistributionPercentages(ctx, sk)
+	delegatorsRewards := k.CalculateDelegatorsRewards(ctx, distrPercentages)
+	address := map[string]sdk.Coin{}
+	if delegatorsRewards != nil {
+		for addr, amt := range delegatorsRewards {
+			bal := suite.bankKeeper.GetBalance(ctx, sdk.MustAccAddressFromBech32(addr), types.PylonsCoinDenom)
+			address[addr] = bal.Add(amt[0])
+		}
+		k.SendRewards(ctx, delegatorsRewards)
+		for addr, amt := range address {
+			new := suite.bankKeeper.GetBalance(ctx, sdk.MustAccAddressFromBech32(addr), types.PylonsCoinDenom)
+			require.Equal(amt.Amount.Int64(), new.Amount.Int64())
+		}
+
+	}
+}
