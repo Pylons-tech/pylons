c02c186b37930ba96617c124846f8675c69e35ef
abdulsamad <abdual.samad@rnssol.com>
2022-11-25 18:08:23 +0500

    Test case for after epoch end function with no deligators was added

--- x/pylons/keeper/hooks_test.go
+++ x/pylons/keeper/hooks_test.go
@@ -113,3 +113,110 @@ func (suite *IntegrationTestSuite) TestAfterEpochEnd() {
 
 	}
 }
+
+// Test Case For After Epoch End Fuction With Case No deligators 
+func (suite *IntegrationTestSuite) TestAfterEpochEndNoDeligators() {
+	k := suite.k
+	ctx := suite.ctx
+	require := suite.Require()
+	bk := suite.bankKeeper
+	ak := suite.accountKeeper
+
+	srv := keeper.NewMsgServerImpl(k)
+	wctx := sdk.WrapSDKContext(ctx)
+
+	amountToPay := sdk.NewCoins(sdk.NewCoin(types.PylonsCoinDenom, sdk.NewInt(100)))
+	creator := types.GenTestBech32FromString("test")
+	executor := types.GenTestBech32FromString("executor")
+	feeCollectorAddr := ak.GetModuleAddress(types.FeeCollectorName)
+
+	types.UpdateAppCheckFlagTest(types.FlagTrue)
+
+	srv.CreateAccount(wctx, &types.MsgCreateAccount{
+		Creator:  executor,
+		Username: "Executor",
+	})
+
+	types.UpdateAppCheckFlagTest(types.FlagFalse)
+	cookbookMsg := &types.MsgCreateCookbook{
+		Creator:      creator,
+		Id:           "testCookbookID",
+		Name:         "testCookbookName",
+		Description:  "descdescdescdescdescdesc",
+		Version:      "v0.0.1",
+		SupportEmail: "test@email.com",
+		Enabled:      true,
+	}
+	_, err := srv.CreateCookbook(sdk.WrapSDKContext(suite.ctx), cookbookMsg)
+	require.NoError(err)
+	recipeMsg := &types.MsgCreateRecipe{
+		Creator:       creator,
+		CookbookId:    "testCookbookID",
+		Id:            "testRecipeID",
+		Name:          "recipeName",
+		Description:   "descdescdescdescdescdesc",
+		Version:       "v0.0.1",
+		BlockInterval: 10,
+		CostPerBlock:  sdk.Coin{Denom: "test", Amount: sdk.ZeroInt()},
+		CoinInputs:    []types.CoinInput{{Coins: amountToPay}},
+		Enabled:       true,
+	}
+	_, err = srv.CreateRecipe(sdk.WrapSDKContext(suite.ctx), recipeMsg)
+	require.NoError(err)
+
+	// create only one pendingExecution
+	msgExecution := &types.MsgExecuteRecipe{
+		Creator:         executor,
+		CookbookId:      "testCookbookID",
+		RecipeId:        "testRecipeID",
+		CoinInputsIndex: 0,
+		ItemIds:         nil,
+	}
+
+	// give coins to requester
+	suite.FundAccount(suite.ctx, sdk.MustAccAddressFromBech32(executor), amountToPay)
+
+	resp, err := srv.ExecuteRecipe(sdk.WrapSDKContext(suite.ctx), msgExecution)
+	require.NoError(err)
+
+	// manually trigger complete execution - simulate endBlocker
+	pendingExecution := k.GetPendingExecution(ctx, resp.Id)
+	execution, _, _, err := k.CompletePendingExecution(suite.ctx, pendingExecution)
+	require.NoError(err)
+	k.ActualizeExecution(ctx, execution)
+
+	// verify execution completion and that requester has no balance left,
+	// also pay and fee are transfered to cookbook owner and fee collector module
+	_ = bk.SpendableCoins(ctx, sdk.MustAccAddressFromBech32(executor))
+	_ = bk.SpendableCoins(ctx, sdk.MustAccAddressFromBech32(creator))
+	_ = bk.SpendableCoins(ctx, feeCollectorAddr)
+
+	delegatorsRewards := k.CalculateDelegatorsRewards(ctx, nil)
+	delegatorMap := map[string]sdk.Coins{}
+	balances := sdk.Coins{}
+	if len(delegatorsRewards) == 0 {
+		// looping through delegators to get their old balance
+		for address, amount := range delegatorsRewards {
+			// looping through amount type of sdk.coins to get every amount and denom
+			for _, val := range amount {
+				oldBalance := suite.bankKeeper.GetBalance(ctx, sdk.MustAccAddressFromBech32(address), val.Denom)
+				// Appending old balance in balances so we can compare it later on with updated balance
+				balances = append(balances, oldBalance.Add(val))
+			}
+			delegatorMap[address] = balances
+
+		}
+		// sending rewards to delegators
+		k.SendRewards(ctx, delegatorsRewards)
+		for address, updatedAmount := range delegatorMap {
+			// looping through updated amount type of sdk.coins to get every amount and denom
+			for _, val := range updatedAmount {
+				newBalance := suite.bankKeeper.GetBalance(ctx, sdk.MustAccAddressFromBech32(address), val.Denom)
+				// Comparing updated Amount with new new Blanace both should  be equal
+				require.Equal(val.Amount.Int64(), newBalance.Amount.Int64())
+			}
+
+		}
+
+	}
+}
