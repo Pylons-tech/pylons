5e093858f6ce8e824777b902392a66ba9205984a
abdulsamad <abdual.samad@rnssol.com>
2022-11-28 11:07:15 +0500

    test case for calculate delegator reqward function was added

--- x/pylons/keeper/distribution_test.go
+++ x/pylons/keeper/distribution_test.go
@@ -19,6 +19,7 @@ import (
 	"github.com/Pylons-tech/pylons/app"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 
+	"github.com/Pylons-tech/pylons/x/pylons/keeper"
 	"github.com/Pylons-tech/pylons/x/pylons/types"
 )
 
@@ -242,3 +243,104 @@ func (suite *IntegrationTestSuite) TestGetRewardsDistributionPercentages() {
 	}
 }
 */
+
+// TestCalculateDelegatorsRewards test cases for calculating delegators rewards
+func (suite *IntegrationTestSuite) TestCalculateDelegatorsRewards() {
+	k := suite.k
+	sk := suite.stakingKeeper
+	ctx := suite.ctx
+	require := suite.Require()
+	bk := suite.bankKeeper
+	ak := suite.accountKeeper
+
+	srv := keeper.NewMsgServerImpl(k)
+	wctx := sdk.WrapSDKContext(ctx)
+
+	amountToPay := sdk.NewCoins(sdk.NewCoin(types.PylonsCoinDenom, sdk.NewInt(100)))
+	creator := types.GenTestBech32FromString("test")
+	executor := types.GenTestBech32FromString("executor")
+	feeCollectorAddr := ak.GetModuleAddress(types.FeeCollectorName)
+
+	types.UpdateAppCheckFlagTest(types.FlagTrue)
+
+	srv.CreateAccount(wctx, &types.MsgCreateAccount{
+		Creator:  executor,
+		Username: "Executor",
+	})
+
+	types.UpdateAppCheckFlagTest(types.FlagFalse)
+	cookbookMsg := &types.MsgCreateCookbook{
+		Creator:      creator,
+		Id:           "testCookbookID",
+		Name:         "testCookbookName",
+		Description:  "descdescdescdescdescdesc",
+		Version:      "v0.0.1",
+		SupportEmail: "test@email.com",
+		Enabled:      true,
+	}
+	_, err := srv.CreateCookbook(sdk.WrapSDKContext(suite.ctx), cookbookMsg)
+	require.NoError(err)
+	recipeMsg := &types.MsgCreateRecipe{
+		Creator:       creator,
+		CookbookId:    "testCookbookID",
+		Id:            "testRecipeID",
+		Name:          "recipeName",
+		Description:   "descdescdescdescdescdesc",
+		Version:       "v0.0.1",
+		BlockInterval: 10,
+		CostPerBlock:  sdk.Coin{Denom: "test", Amount: sdk.ZeroInt()},
+		CoinInputs:    []types.CoinInput{{Coins: amountToPay}},
+		Enabled:       true,
+	}
+	_, err = srv.CreateRecipe(sdk.WrapSDKContext(suite.ctx), recipeMsg)
+	require.NoError(err)
+
+	// create only one pendingExecution
+	msgExecution := &types.MsgExecuteRecipe{
+		Creator:         executor,
+		CookbookId:      "testCookbookID",
+		RecipeId:        "testRecipeID",
+		CoinInputsIndex: 0,
+		ItemIds:         nil,
+	}
+
+	// give coins to requester
+	suite.FundAccount(suite.ctx, sdk.MustAccAddressFromBech32(executor), amountToPay)
+
+	resp, err := srv.ExecuteRecipe(sdk.WrapSDKContext(suite.ctx), msgExecution)
+	require.NoError(err)
+
+	// manually trigger complete execution - simulate endBlocker
+	pendingExecution := k.GetPendingExecution(ctx, resp.Id)
+	execution, _, _, err := k.CompletePendingExecution(suite.ctx, pendingExecution)
+	require.NoError(err)
+	k.ActualizeExecution(ctx, execution)
+
+	// verify execution completion and that requester has no balance left,
+	// also pay and fee are transfered to cookbook owner and fee collector module
+	_ = bk.SpendableCoins(ctx, sdk.MustAccAddressFromBech32(executor))
+	_ = bk.SpendableCoins(ctx, sdk.MustAccAddressFromBech32(creator))
+	_ = bk.SpendableCoins(ctx, feeCollectorAddr)
+
+	distrPercentages := k.GetRewardsDistributionPercentages(ctx, sk)
+	rewardsTotalAmount := bk.SpendableCoins(ctx, k.FeeCollectorAddress())
+	if !rewardsTotalAmount.IsZero() {
+		delegatorsRewards := make(map[string]sdk.Coins)
+		for addr, percentage := range distrPercentages {
+			totalAmountsForAddr := sdk.NewCoins()
+			for _, coin := range rewardsTotalAmount {
+				amountForAddr := sdk.NewDecFromInt(coin.Amount).Mul(percentage).TruncateInt()
+				if amountForAddr.IsPositive() {
+					// only add strictly positive amounts
+					totalAmountsForAddr = totalAmountsForAddr.Add(sdk.NewCoin(coin.Denom, amountForAddr))
+				}
+			}
+			if !totalAmountsForAddr.Empty() {
+				delegatorsRewards[addr] = totalAmountsForAddr
+			}
+
+		}
+		// Checking if delegators Rewards are not empty
+		require.NotEqual(len(delegatorsRewards), 0)
+	}
+}
