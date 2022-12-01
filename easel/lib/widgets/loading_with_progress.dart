e06358d35174f1660c6db7a52cf3dcb071ee12c2
Kamran Khan <kamran.khan@rnssol.com>
2022-12-01 23:15:59 +0500

    1- Refactor tests.

--- easel/lib/widgets/loading_with_progress.dart
+++ easel/lib/widgets/loading_with_progress.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
 
+import '../utils/easel_app_theme.dart';
+
 class LoadingProgress {
   LoadingProgress();
 
