3d4bdeb66cb74e8053d0cecfbd91b44e16e27e5e
sine/cosine/tangent <zfshmn@gmail.com>
2023-01-30 16:44:30 -0800

    it's also important to add the component!

--- flame-demo/lib/game.dart
+++ flame-demo/lib/game.dart
@@ -1,13 +1,15 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pylons_flame_demo/doohickey.dart';
 import 'package:pylons_flame_demo/pylons_component.dart';
 
 class PylonsGame extends FlameGame{
   @override
   Future<void> onLoad() async {
-    add(
-      PylonsComponent()
-    );
+    addAll([
+        PylonsComponent(),
+        Doohickey()
+      ]);
   }
 }
