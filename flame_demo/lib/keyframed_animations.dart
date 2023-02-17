b0725c55604e2fd3485cf9bc5b749d157ef391b5
sine/cosine/tangent <zfshmn@gmail.com>
2023-02-17 00:56:03 -0800

    still wip here

--- flame_demo/lib/keyframed_animations.dart
+++ flame_demo/lib/keyframed_animations.dart
@@ -1,22 +1,75 @@
 import 'package:flame/components.dart';
 
 class KeyframedAnimationComponent extends SpriteAnimationComponent {
+  double currentFrameTime = 0;
+  int currentFrameIndex = 0;
+  KeyframedAnimation? currentAnimation;
   final Map<String, KeyframedAnimation> animMap;
-  double currentFrameTime;
-  Keyframe currentKeyframe;
+  String initialAnimationKey;
 
+  KeyframedAnimationComponent(this.animMap, this.initialAnimationKey);
+
+  /// override w/ a function to be called when an animation finishes
+  void onAnimationDone(String animation) {
+
+  }
+
+  /// override w/ a function to be called when an animation loops
+  void onAnimationLoop(String animation) {
+
+  }
 
   @override
   void update(double dt) {
+    if (currentAnimation != null) {
+      currentFrameTime += dt;
 
-    //scale =
+      final Keyframe framedata;
+      final bool animDone;
+      final bool frameDone;
+
+      // Are we done w/ the current keyframe?
+      if (currentFrameTime >= currentAnimation!.frameData[currentFrameIndex].transitionTime) {
+        currentFrameTime = currentAnimation!.frameData[currentFrameIndex].transitionTime; // clamp
+        frameDone = true;
+      } else {
+        frameDone = false;
+      }
+
+      // Interpolate framedata if not last frame of animation; otherwise, just use last frame
+      if (currentFrameIndex <= currentAnimation!.frameData.length) {
+        framedata = currentAnimation!.frameData[currentFrameIndex].lerp(currentAnimation!.frameData[currentFrameIndex + 1], currentFrameTime);
+        animDone = false;
+      } else {
+        framedata = currentAnimation!.frameData[currentFrameIndex];
+        animDone = true;
+      }
+
+      scale = framedata.scale;
+      angle = framedata.angle;
+      animation = framedata.baseAnim;
+
+      if (frameDone) {
+        currentFrameTime = 0;
+        if (animDone) {
+          switch (currentAnimation!.endBehavior) {
+            case AnimationEndBehavior.playOnce:
+              onAnimationDone(currentAnimation!.name);
+              break;
+            case AnimationEndBehavior.loop:
+              onAnimationLoop(currentAnimation!.name);
+              currentFrameIndex = 0;
+              break;
+          }
+        } else {
+          // Move to next frame
+          currentFrameIndex++;
+        }
+      }
+    }
   }
 
   void playAnimation(String animation) {
-
-  }
-
-  void playKeyframe(Keyframe keyframe) {
 
   }
 
@@ -33,11 +86,18 @@ class KeyframedAnimationComponent extends SpriteAnimationComponent {
   }
 }
 
+enum AnimationEndBehavior {
+  playOnce,
+  loop
+}
+
 class KeyframedAnimation {
+  /// Must be the same string used for the animation's key in the animMap.
   final String name;
   final List<Keyframe> frameData;
+  final AnimationEndBehavior endBehavior;
 
-  KeyframedAnimation(this.name, this.frameData);
+  KeyframedAnimation(this.name, this.frameData, this.endBehavior);
 }
 
 class Keyframe {
