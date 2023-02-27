4b09584779422ab86c2fc1f440de65835aefaf4c
Joe Abbey <joe.abbey@gmail.com>
2023-02-27 15:12:40 -0500

    Emergency Fix of bonded_tokens_pool

--- app/app.go
+++ app/app.go
@@ -683,6 +683,36 @@ func (app *PylonsApp) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) 
 	// 		panic(err)
 	// 	}
 	// }
+
+	// This should only run once, and afterwards the bonded_tokens_pool's balance will be automatically kept in order
+	if ctx.BlockHeight() == 1000001 {
+		ctx.Logger().Info(fmt.Sprintf("Attempting to correct bonded_tokens_pool"))
+
+		bk := app.BankKeeper
+		bonded_tokens_pool_address := sdk.MustAccAddressFromBech32("pylo1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3vp7zan")
+		balance := bk.GetBalance(ctx, bonded_tokens_pool_address, "ubedrock")
+		ctx.Logger().Info(fmt.Sprintf("bonded_tokens_pools has %v", balance))
+
+		// Account currently has 1 BEDROCK
+		// There is currently 15202 BEDROCK Total Power Delegated
+		// Unclear how this became broken, but let's mint the delta and send it to the pool
+		amount := sdk.NewCoins(sdk.NewCoin("ubedrock", sdk.NewInt(15201000000)))
+		err := bk.MintCoins(ctx, "mint", amount)
+		if err != nil {
+			panic(err)
+		}
+
+		err = bk.SendCoinsFromModuleToModule(ctx, "mint", "bonded_tokens_pool", amount)
+		if err != nil {
+			panic(err)
+		}
+
+		// Double check that the balance is expected
+		balance = bk.GetBalance(ctx, bonded_tokens_pool_address, "ubedrock")
+		ctx.Logger().Info(fmt.Sprintf("bonded_tokens_pools now has %v\n", balance))
+	}
+
+
 	return app.mm.BeginBlock(ctx, req)
 }
 
