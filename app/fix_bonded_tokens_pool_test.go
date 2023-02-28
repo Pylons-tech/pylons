df5db65717611192cd58cec0256bd9a67fb44d7e
Joe Abbey <joe.abbey@gmail.com>
2023-02-28 13:59:51 -0500

    Adding additional checks to make sure test makes sense

--- app/fix_bonded_tokens_pool_test.go
+++ app/fix_bonded_tokens_pool_test.go
@@ -38,21 +38,32 @@ func (suite *UpgradeTestSuite) TestFixBondedTokensPool() {
 		suite.FundAcc(acc, defaultAcctFundsBedrockCoin)
 	}
 
+	// Fund the Master wallet as well so we show that the burn ignored it's balance
 	multisigAddress := sdk.MustAccAddressFromBech32(app.MasterWallet)
 	suite.FundAcc(multisigAddress, defaultAcctFundsBedrockCoin)
 
+	// The master wallet should now have 10
+	masterWalletBalance := suite.App.BankKeeper.GetBalance(suite.Ctx, multisigAddress, params.StakingBaseCoinUnit)
+	suite.Require().Equal(masterWalletBalance.Amount, math.NewInt(10_000_000))
+
+	// Check the current bonded tokens pool balance, there should be 1
 	bondedTokensPoolAddress := authtypes.NewModuleAddress(stakingtypes.BondedPoolName)
 	bondedTokensPoolBefore := suite.App.BankKeeper.GetBalance(suite.Ctx, bondedTokensPoolAddress, params.StakingBaseCoinUnit)
+	suite.Require().Equal(bondedTokensPoolBefore.Amount, math.NewInt(1_000_000))
 
 	// Get ubedrock total supply
 	totalAmount := suite.App.BankKeeper.GetSupply(suite.Ctx, stakingCoinDenom)
 	suite.Require().Equal(totalAmount.Amount, math.NewInt(41_000_000))
 
+	// Get the total power
+	totalPower := suite.App.StakingKeeper.GetLastTotalPower(suite.Ctx)
+	suite.Require().Equal(totalPower, math.NewInt(1))
+
 	// Burn ubedrock
 	bankBaseKeeper, _ := suite.App.BankKeeper.(bankkeeper.BaseKeeper)
 	v1.BurnToken(suite.Ctx, &suite.App.AccountKeeper, &bankBaseKeeper, &suite.App.StakingKeeper)
 
-	// Check ubedrock total supply (should equal 0)
+	// Check ubedrock total supply should equal 10 to account for the "Master Wallet"
 	totalAmount = suite.App.BankKeeper.GetSupply(suite.Ctx, stakingCoinDenom)
 	suite.Require().Equal(totalAmount.Amount, math.NewInt(10_000_000))
 
@@ -61,10 +72,19 @@ func (suite *UpgradeTestSuite) TestFixBondedTokensPool() {
 	suite.Require().NotEqual(bondedTokensPoolBefore.Amount, bondedTokensPoolAfter.Amount)
 	suite.Require().Equal(bondedTokensPoolAfter.Amount, math.ZeroInt())
 
+	// The master wallet should still have 10
+	masterWalletBalance = suite.App.BankKeeper.GetBalance(suite.Ctx, multisigAddress, params.StakingBaseCoinUnit)
+	suite.Require().Equal(masterWalletBalance.Amount, math.NewInt(10_000_000))
+
 	// Let's now test the fix
 	suite.App.FixBondedTokensPool(suite.Ctx)
 
 	// The bonded_tokens_pool should now be restored.
 	bondedTokensPoolFixed := suite.App.BankKeeper.GetBalance(suite.Ctx, bondedTokensPoolAddress, params.StakingBaseCoinUnit)
 	suite.Require().NotEqual(bondedTokensPoolBefore.Amount, bondedTokensPoolFixed.Amount)
+
+	// The master wallet should now have 9
+	masterWalletBalance = suite.App.BankKeeper.GetBalance(suite.Ctx, multisigAddress, params.StakingBaseCoinUnit)
+	suite.Require().Equal(masterWalletBalance.Amount, math.NewInt(9_999_999))
+
 }
