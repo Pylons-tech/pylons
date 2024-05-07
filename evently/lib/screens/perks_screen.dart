8f833cbd9f6053b91247de9ff513722c93d769dd
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 17:35:13 +0500

    feat: add perk ui

--- evently/lib/screens/perks_screen.dart
+++ evently/lib/screens/perks_screen.dart
@@ -2,12 +2,17 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:evently/generated/locale_keys.g.dart';
 import 'package:evently/screens/custom_widgets/step_labels.dart';
 import 'package:evently/screens/custom_widgets/steps_indicator.dart';
+import 'package:evently/utils/constants.dart';
 import 'package:evently/utils/evently_app_theme.dart';
+import 'package:evently/utils/screen_responsive.dart';
 import 'package:evently/utils/space_utils.dart';
 import 'package:evently/viewmodels/create_event_viewmodel.dart';
 import 'package:evently/widgets/clipped_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class PerksScreen extends StatefulWidget {
@@ -36,12 +41,12 @@ class _PerksScreenState extends State<PerksScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const VerticalSpace(20),
                   MyStepsIndicator(currentStep: createEventViewModel.currentStep),
                   const VerticalSpace(5),
                   StepLabels(currentPage: createEventViewModel.currentPage, currentStep: createEventViewModel.currentStep),
-                  const VerticalSpace(10),
                   const VerticalSpace(20),
                   Stack(
                     alignment: Alignment.center,
@@ -76,9 +81,94 @@ class _PerksScreenState extends State<PerksScreen> {
                     ],
                   ),
                   const VerticalSpace(20),
-
-
-
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          LocaleKeys.add_perk.tr(),
+                          style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: DecoratedBox(
+                            decoration: const BoxDecoration(
+                              color: EventlyAppTheme.kpurpleDark,
+                            ),
+                            child: Icon(Icons.add, size: 16.h, color: EventlyAppTheme.kWhite),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const VerticalSpace(10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    child: Text(
+                      LocaleKeys.there_no_perks_created.tr(),
+                      style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w700, color: EventlyAppTheme.kDarkGrey02),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        ScreenResponsive(
+                          mobileScreen: (context) => Image.asset(
+                            PngUtils.kLightPurpleSingleLine,
+                            height: 40.h,
+                            width: 1.sw,
+                            fit: BoxFit.fill,
+                          ),
+                          tabletScreen: (context) => Image.asset(
+                            PngUtils.kLightPurpleSingleLine,
+                            height: 32.h,
+                            width: 1.sw,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.w),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset(SVGUtils.kMinus),
+                              HorizontalSpace(10.w),
+                              Text(
+                                LocaleKeys.free_shirt.tr(),
+                                style: TextStyle(color: EventlyAppTheme.kWhite, fontSize: 15.sp, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const VerticalSpace(10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50.w),
+                    child: Text(
+                      LocaleKeys.perk_icon.tr(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13.sp,
+                      ),
+                    ),
+                  ),
+                  const VerticalSpace(6),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50.w),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(padding: const EdgeInsets.all(7), color: EventlyAppTheme.kPurple03, child: SvgPicture.asset(SVGUtils.kShirt)),
+                        Container(padding: const EdgeInsets.all(7), color: Colors.transparent, child: SvgPicture.asset(SVGUtils.kGift)),
+                        Container(padding: const EdgeInsets.all(7), color: Colors.transparent, child: SvgPicture.asset(SVGUtils.kDrinks)),
+                      ],
+                    ),
+                  )
                 ],
               ),
               Padding(
