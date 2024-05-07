d2651b6b26878d285f9b792fb9df76615f7ab1a8
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 10:37:52 +0500

    feat: basic app architecture following easel app

--- evently/lib/main.dart
+++ evently/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:evently/utils/evently_app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -19,6 +20,8 @@ void main() {
   );
 }
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -26,14 +29,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        minTextAdapt: true,
-        builder: (BuildContext context, child) => MaterialApp(
-              title: 'Evently',
-              theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-                useMaterial3: true,
-              ),
-              home: const Placeholder(),
-            ));
+      minTextAdapt: true,
+      builder: (BuildContext context, child) => MaterialApp(
+        builder: (context, widget) {
+          ScreenUtil.init(context);
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaler: TextScaler.noScaling),
+            child: widget!,
+          );
+        },
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        title: 'Evently',
+        navigatorKey: navigatorKey,
+        theme: EventlyAppTheme.theme(context),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const Placeholder(),
+        },
+      ),
+    );
   }
 }
