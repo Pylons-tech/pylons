7e5f5d8a40862d89fd74aaefa92eb7b418ce0ded
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 11:13:08 +0500

    feat: event hub screen

---
+++ evently/lib/screens/event_hub/event_hub_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventHubScreen extends StatefulWidget {
+  const EventHubScreen({super.key});
+
+  @override
+  State<EventHubScreen> createState() => _EventHubScreenState();
+}
+
+class _EventHubScreenState extends State<EventHubScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
