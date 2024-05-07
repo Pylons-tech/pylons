af862850851b82327d9dedddf7f78f4d5be889f0
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 10:43:15 +0500

    feat: routes configurations

--- evently/lib/main.dart
+++ evently/lib/main.dart
@@ -3,7 +3,10 @@ import 'package:evently/utils/evently_app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
   runApp(
     EasyLocalization(
       supportedLocales: const [
---
+++ evently/lib/utils/route_util.dart
@@ -0,0 +1,5 @@
+class RouteUtil {
+  RouteUtil();
+
+  static String kRouteEventHub = "/eventHub";
+}
