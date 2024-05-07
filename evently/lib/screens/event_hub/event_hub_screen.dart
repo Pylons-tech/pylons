4856baacc82ed7cd25f217fdea684d09bc019ffa
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 11:25:47 +0500

    feat: event hub screen

--- evently/lib/screens/event_hub/event_hub_screen.dart
+++ evently/lib/screens/event_hub/event_hub_screen.dart
@@ -1,4 +1,6 @@
+import 'package:evently/utils/evently_app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EventHubScreen extends StatefulWidget {
   const EventHubScreen({super.key});
@@ -10,6 +12,24 @@ class EventHubScreen extends StatefulWidget {
 class _EventHubScreenState extends State<EventHubScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  ColoredBox(
+      color: EventlyAppTheme.kWhite,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: EventlyAppTheme.kBgWhite,
+          body: Padding(
+            padding: EdgeInsets.only(top: 20.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+
 }
