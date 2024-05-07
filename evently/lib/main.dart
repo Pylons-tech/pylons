0f996ab1ed243503660ceb723c05c6ffa2853572
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 10:32:47 +0500

    feat: screen utils init

--- evently/lib/main.dart
+++ evently/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(
@@ -24,13 +25,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Evently',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Placeholder(),
-    );
+    return ScreenUtilInit(
+        minTextAdapt: true,
+        builder: (BuildContext context, child) => MaterialApp(
+              title: 'Evently',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              home: const Placeholder(),
+            ));
   }
 }
