7d6b46755fc27c3a56791a2a15fbd7c7bae5789a
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 12:38:42 +0500

    feat: GetIt via injectable and build runner

---
+++ evently/lib/utils/di/register_modules.dart
@@ -0,0 +1,12 @@
+// coverage: false
+// coverage:ignore-file
+// Injectable is a convenient code generator for get_it.
+// All you have to do now is annotate your injectable classes with @injectable and let the generator do the work.
+// This class is use to generate code to register objects on app start
+import 'package:injectable/injectable.dart';
+
+
+@module
+abstract class RegisterModule {
+
+}
---
+++ evently/lib/viewmodels/create_event_viewmodel.dart
@@ -0,0 +1,12 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+
+
+@lazySingleton
+class CreateEventViewModel extends ChangeNotifier {
+
+
+}
