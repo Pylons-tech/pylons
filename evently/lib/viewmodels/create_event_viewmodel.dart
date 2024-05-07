c464c09702afb20198e5b010b851d2697ceb3f33
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 12:45:16 +0500

    feat: translation for create event screen

--- evently/lib/viewmodels/create_event_viewmodel.dart
+++ evently/lib/viewmodels/create_event_viewmodel.dart
@@ -1,12 +1,37 @@
-
-
-
+import 'package:evently/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 
-
 @lazySingleton
 class CreateEventViewModel extends ChangeNotifier {
+  CreateEventViewModel();
 
+  late ValueNotifier<int> currentPage;
+  late ValueNotifier<int> currentStep;
+  late PageController pageController;
 
+  String? from;
+  final List<String> pageTitles = <String>['LocaleKeys.upload.tr()', ' LocaleKeys.nft_detail_text.tr()', ' LocaleKeys.nft_pricing.tr()', ''];
+
+  void init({required VoidCallback setTextField}) {
+    currentPage = ValueNotifier(0);
+    currentStep = ValueNotifier(0);
+    pageController = PageController();
+  }
+
+  Future<void> nextPage() async {
+    await pageController.nextPage(duration: const Duration(milliseconds: kPageAnimationTimeInMillis), curve: Curves.easeIn);
+    notifyListeners();
+  }
+
+  Future<void> previousPage() async {
+    from = kDraft;
+    await pageController.previousPage(duration: const Duration(milliseconds: kPageAnimationTimeInMillis), curve: Curves.easeIn);
+
+    notifyListeners();
+  }
+
+  void disposeControllers() {
+    pageController.dispose();
+  }
 }
