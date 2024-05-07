0e3836ec061c849fd4dd2ed8a1073ce032e5ac11
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 11:13:08 +0500

    feat: event hub screen

--- evently/lib/main.dart
+++ evently/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:evently/screens/event_hub/event_hub_screen.dart';
 import 'package:evently/screens/splash_screen.dart';
 import 'package:evently/utils/constants.dart';
 import 'package:evently/utils/evently_app_theme.dart';
@@ -58,7 +59,7 @@ class MyApp extends StatelessWidget {
         initialRoute: '/',
         routes: {
           '/': (context) => const SplashScreen(),
-          RouteUtil.kRouteEventHub: (context) => const Placeholder(),
+          RouteUtil.kRouteEventHub: (context) => const EventHubScreen(),
         },
       ),
     );
