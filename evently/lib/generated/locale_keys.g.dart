1ba9c6935c62b36777193334b9ab404ba2db3cdf
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 12:04:06 +0500

    feat: event hub basic with styling

--- evently/lib/generated/locale_keys.g.dart
+++ evently/lib/generated/locale_keys.g.dart
@@ -1,5 +1,12 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
 abstract class  LocaleKeys {
+  static const eventhub = 'eventhub';
+  static const welcome_event = 'welcome_event';
+  static const draft = 'draft';
+  static const for_sale = 'for_sale';
+  static const history = 'history';
+  static const no_nft_created = 'no_nft_created';
+  static const create_event = 'create_event';
 
 }
