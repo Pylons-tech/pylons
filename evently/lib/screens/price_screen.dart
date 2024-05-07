c6c1028a7184b6bf64f1b21cc57c4d91ce3e5902
AhsanRns <ahsan+1@rnssol.com>
2024-05-07 17:58:26 +0500

    feat: price screen

---
+++ evently/lib/screens/price_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatefulWidget {
+  const PriceScreen({super.key});
+
+  @override
+  State<PriceScreen> createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
