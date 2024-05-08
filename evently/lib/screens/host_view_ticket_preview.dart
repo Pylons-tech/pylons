8d0cac010ff0f646951aeab6826c2740178956c7
AhsanRns <ahsan+1@rnssol.com>
2024-05-08 11:42:32 +0500

    feat: host ticket preview

---
+++ evently/lib/screens/host_view_ticket_preview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HostTicketPreview extends StatefulWidget {
+  const HostTicketPreview({super.key});
+
+  @override
+  State<HostTicketPreview> createState() => _HostTicketPreviewState();
+}
+
+class _HostTicketPreviewState extends State<HostTicketPreview> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
