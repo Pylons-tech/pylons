9bde0e1ec19d1cff4f88687dad9e5133d0662779
AhsanRns <ahsan+1@rnssol.com>
2024-05-10 16:48:06 +0500

    feat: shared preference init

--- evently/lib/utils/di/di.dart
+++ evently/lib/utils/di/di.dart
@@ -4,10 +4,14 @@
 import 'package:evently/utils/di/di.config.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // Service Locator for  project
 final sl = GetIt.I;
 
 // initialization of Service locator
 @InjectableInit()
-void configureDependencies() => sl.init();
+void configureDependencies() {
+  sl.registerSingletonAsync<SharedPreferences>(() => SharedPreferences.getInstance());
+  sl.init();
+}
