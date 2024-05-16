2d144ef7bb0f2267942958cbab8ac58ea30d495b
AhsanAli13503 <ahsan+1@rnssol.com>
2024-05-16 15:25:30 +0500

    feat: event sale list on the creator hub

--- evently/lib/screens/event_hub/event_hub_view_model.dart
+++ evently/lib/screens/event_hub/event_hub_view_model.dart
@@ -4,9 +4,27 @@ import 'package:injectable/injectable.dart';
 
 @lazySingleton
 class EventHubViewModel extends ChangeNotifier {
-  final List<Events> _eventPublishedList = [];
+  List<Events> _eventPublishedList = [];
 
   List<Events> get eventPublishedList => _eventPublishedList;
 
-  void updatePublishedEventList({required Events nft}) {}
+  List<Events> _eventForSaleList = [];
+
+  List<Events> get eventForSaleList => _eventForSaleList;
+
+  set setEventForSaleList(List<Events> nftForSale) {
+    _eventForSaleList = nftForSale;
+    notifyListeners();
+  }
+
+  set setEventPublishList(List<Events> events) {
+    _eventPublishedList = events;
+    notifyListeners();
+  }
+
+  void updatePublishedEventList({required Events events}) {
+    _eventPublishedList.add(events);
+    _eventForSaleList.add(events);
+    notifyListeners();
+  }
 }
