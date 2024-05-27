9c088da7da55fdf77889dc7be10980710d2300e7
AhsanAli13503 <ahsan+1@rnssol.com>
2024-05-27 17:39:03 +0500

    feat: basic mobile scanner

---
+++ wallet/lib/pages/events/mobile_scanner.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class MobileQrScanner extends StatefulWidget {
+  const MobileQrScanner({super.key});
+
+  @override
+  State<MobileQrScanner> createState() => _MobileQrScannerState();
+}
+
+class _MobileQrScannerState extends State<MobileQrScanner> {
+  Barcode? _barcode;
+
+  Widget _buildBarcode(Barcode? value) {
+    if (value == null) {
+      return const Text(
+        'Scan something!',
+        overflow: TextOverflow.fade,
+        style: TextStyle(color: Colors.white),
+      );
+    }
+
+    return Text(
+      value.displayValue ?? 'No display value.',
+      overflow: TextOverflow.fade,
+      style: const TextStyle(color: Colors.white),
+    );
+  }
+
+  void _handleBarcode(BarcodeCapture barcodes) {
+    if (mounted) {
+      setState(() {
+        _barcode = barcodes.barcodes.firstOrNull;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Simple scanner')),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          MobileScanner(
+            onDetect: _handleBarcode,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              height: 100,
+              color: Colors.black.withOpacity(0.4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Center(child: _buildBarcode(_barcode))),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
